9f6a2dc4220feb438e31c0d7964274a0b9202f6b
lhoffman <lhoffman@ec9d8473-2c57-4e18-8d51-132f0e30f32e>
2007-08-08 23:28:58 +0000

    fix star card keys
    git-svn-id: svn+ssh://rubyforge.org/var/svn/wagn/trunk@93 ec9d8473-2c57-4e18-8d51-132f0e30f32e

---
+++ db/migrate/088_fix_star_card_keys.rb
@@ -0,0 +1,8 @@
+class FixStarCardKeys < ActiveRecord::Migration
+  def self.up
+    Card.find_by_sql("select * from cards where name LIKE '%*%'").each do |c| c.update_attributes! :key=>c.name.to_key end  
+  end
+
+  def self.down
+  end
+end

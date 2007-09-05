6acd7f2554dcb9183902343b119c21ab3f735acf
ethanmcc <ethanmcc@ec9d8473-2c57-4e18-8d51-132f0e30f32e>
2007-09-05 00:49:28 +0000

    transclusion css and permission spec merger
    git-svn-id: svn+ssh://rubyforge.org/var/svn/wagn/trunk@143 ec9d8473-2c57-4e18-8d51-132f0e30f32e

--- spec/models/card/permissions_spec.rb
+++ spec/models/card/permissions_spec.rb
@@ -35,6 +35,56 @@ describe Card, "new permissions" do
 
 end
   
+
+describe Card, "updating permissions" do
+  before do
+    User.as :admin
+    @anon = Role.find_by_codename 'anon'
+    @auth = Role.find_by_codename 'auth'
+    @perms = [:read,:edit,:comment,:delete].map{|t| ::Permission.new(:task=>t.to_s, :party=>@anon)}
+    @c = Card.find_by_name 'X'
+    @c.permissions=@perms
+    @c.save!
+  end
+  
+  it "should give permissions to auth after setting permissions" do
+    @c.permissions.find_by_task('read').party.should== @anon
+  end
+  
+  it "should set the reader in the process" do
+    @c.reader.should== @anon
+  end
+  it "should retain these permissions after a hard reload" do
+    @c = Card.find_by_name 'X'
+    @c.permissions.find_by_task('read').party.should== @anon
+  end
+end
+
+
+describe Card, "hacky permission api" do
+  before do
+    User.as :admin
+    @c = Card.find_by_name 'X'
+    @r2 = Role.find_by_codename 'r2'
+    @c.permit(:read, @r2)
+  end
+  
+  it "should give permissions after setting permissions" do
+     @c.permissions.find_by_task('read').party.should== @r2
+   end
+  it "should immediately be ok to read" do
+    @c.ok? :read
+  end
+  it "should still work after reload" do
+    @c = Card.find_by_name 'X'
+    @c.ok? :read
+  end
+  it "should update reader" do
+    @c.reader == @r2
+  end
+end  
+  
+  
 =begin
 describe Card, "permissions" do      
   it "should let anonymous users view basic cards" do

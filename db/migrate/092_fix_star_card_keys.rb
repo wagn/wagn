50e59cc5f074c4c61317c25bdefc6444ce0934ad
ethanmcc <ethanmcc@ec9d8473-2c57-4e18-8d51-132f0e30f32e>
2007-09-30 19:01:18 +0000

    just trying to get hooze live now
    git-svn-id: svn+ssh://rubyforge.org/var/svn/wagn/trunk@265 ec9d8473-2c57-4e18-8d51-132f0e30f32e

--- db/migrate/092_fix_star_card_keys.rb
+++ db/migrate/092_fix_star_card_keys.rb
@@ -1,6 +1,7 @@
 class FixStarCardKeys < ActiveRecord::Migration
   def self.up
-    Card.find_by_sql("select * from cards where name LIKE '%*%'").each do |c| c.update_attributes! :key=>c.name.to_key end  
+    # fixme later.
+   # Card.find_by_sql("select * from cards where name LIKE '%*%'").each do |c| c.update_attributes! :key=>c.name.to_key end  
   end
 
   def self.down

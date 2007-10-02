26adeefb96708f0f7fb689a09cb9df0608964ff9
lhoffman <lhoffman@ec9d8473-2c57-4e18-8d51-132f0e30f32e>
2007-10-02 00:48:43 +0000

    tweak copy script
    git-svn-id: svn+ssh://rubyforge.org/var/svn/wagn/trunk@271 ec9d8473-2c57-4e18-8d51-132f0e30f32e

--- script/copy_for_upgrade.rb
+++ script/copy_for_upgrade.rb
@@ -10,5 +10,5 @@ from_dir, to_dir = ARGV
 ["config/database.yml", "config/wagn.rb", "public/image/*",
    "public/images/*", "public/file/*", "public/stylesheets/local.css"].each do |path|
      path, src, dest = Pathname.new(path), Pathname.new(from_dir), Pathname.new(to_dir)
-     puts `cp #{src+path} #{dest+path.dirname}`
+     puts `cp -r #{src+path} #{dest+path.dirname}`
   end

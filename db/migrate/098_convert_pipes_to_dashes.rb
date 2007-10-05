8ddffd5c5d80371ca50b52c13ebf85fe1f61964b
ethanmcc <ethanmcc@ec9d8473-2c57-4e18-8d51-132f0e30f32e>
2007-10-05 23:26:29 +0000

    fixed migrations in mysql / sqlite
    git-svn-id: svn+ssh://rubyforge.org/var/svn/wagn/trunk@311 ec9d8473-2c57-4e18-8d51-132f0e30f32e

--- db/migrate/098_convert_pipes_to_dashes.rb
+++ db/migrate/098_convert_pipes_to_dashes.rb
@@ -1,18 +1,11 @@
 class ConvertPipesToDashes < ActiveRecord::Migration
   def self.up
 
-=begin
-    #FIXME -- I can't find a way to match a stinking pipe.
-    # the following might work in postgres, in which case we'd be ok for the real need, but it brea    
-    cards = [] #Card.find_by_wql("cards with name ~ '|'")
-    cards.each_with_index do |c,i| 
-      puts "#{i}: #{c.name}"
-      c.name = c.name.gsub('|', '-')
-      c.save!
-    end        
-=end
 
-    # this is the code i used to convert meyer
+    # this is code we used to convert one postgres database.  Since we don't yet have a great
+    # way to do consistent pattern matching in a safe, database-agnostic way (maybe in 0.9?),
+    # we're leaving this out of the main migration.
+=begin
 
     cards = Card.find_by_sql(
     "SELECT DISTINCT  t0.* FROM cards t0  WHERE (t0.trash='f' AND t0.name LIKE '%'||E'|'||'%')"
@@ -23,6 +16,7 @@ class ConvertPipesToDashes < ActiveRecord::Migration
       c.confirm_rename=true; c.update_link_ins=true; begin c.save!; rescue Exception=>e; puts e.message; end; 
     end
 
+=end
 
   end
 

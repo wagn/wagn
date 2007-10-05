72efe009e0c7f3853519e97dd0a57416d8e21a67
ethanmcc <ethanmcc@ec9d8473-2c57-4e18-8d51-132f0e30f32e>
2007-10-05 06:08:30 +0000

    migration fix -- permisions tweak
    git-svn-id: svn+ssh://rubyforge.org/var/svn/wagn/trunk@309 ec9d8473-2c57-4e18-8d51-132f0e30f32e

--- db/migrate/099_fix_missing_create_permissions.rb
+++ db/migrate/099_fix_missing_create_permissions.rb
@@ -1,7 +1,9 @@
 class FixMissingCreatePermissions < ActiveRecord::Migration
-  def self.up   
-    Card::Cardtype.find(:all).reject {|x| x.who_can(:create) }.each do |ct| 
-      ct.permit(:create, Role[:auth]); ct.save! 
+  def self.up
+    User.as :admin do
+      Card::Cardtype.find(:all).reject {|x| x.who_can(:create) }.each do |ct| 
+        ct.permit(:create, Role[:auth]); ct.save! 
+      end
     end
   end
 

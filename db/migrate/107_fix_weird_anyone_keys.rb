7e3d13fe09f97766bece18cebae102f5842cd7cd
lhoffman <lhoffman@ec9d8473-2c57-4e18-8d51-132f0e30f32e>
2007-11-28 19:02:08 +0000

    fix bizarro migration failure
    git-svn-id: svn+ssh://rubyforge.org/var/svn/wagn/trunk@395 ec9d8473-2c57-4e18-8d51-132f0e30f32e

--- db/migrate/107_fix_weird_anyone_keys.rb
+++ db/migrate/107_fix_weird_anyone_keys.rb
@@ -1,10 +1,16 @@
 class FixWeirdAnyoneKeys < ActiveRecord::Migration
-  def self.up
+  def self.up 
+    Card::Role.reset_column_information
+    Card.reset_column_information 
+    User.as :admin
     ['Anyone', 'Anyone Signed In'].each do |name|
       c = Card.find_by_name_and_trash(name, false)
       if c
-        c.key = c.name.to_key
-        c.save!
+        c.key = c.name.to_key 
+        puts "Set #{name} to #{c.key}"
+        c.save!   
+      else
+        raise("Failed to update '#{name}'")
       end
     end
   end

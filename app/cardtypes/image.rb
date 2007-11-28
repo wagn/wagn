a63a729fea9485e7947e5d0af0010ca5f18c74ec
ethanmcc <ethanmcc@ec9d8473-2c57-4e18-8d51-132f0e30f32e>
2007-11-28 23:03:10 +0000

    css and image fixes
    git-svn-id: svn+ssh://rubyforge.org/var/svn/wagn/trunk@399 ec9d8473-2c57-4e18-8d51-132f0e30f32e

--- app/cardtypes/image.rb
+++ app/cardtypes/image.rb
@@ -1,9 +1,19 @@
 module Card
 	class Image < Base
+	  def src
+	    "/image/#{content}?#{updated_at.to_i}"
+    end
+    
+    def image_exists?
+      File.exists?("#{RAILS_ROOT}/public/image/#{content}")
+      return "#{RAILS_ROOT}/public/image/#{content}"
+    end
+=begin
     def content_for_rendering
-      src = "/image/#{content}?#{rand}"
+      src = "/image/#{content}?#{updated_at.to_i}"
       %{<a href="#{src}"><img title="#{name}" src="#{src}" /></a>}
     end
+=end
     
 	end
 end

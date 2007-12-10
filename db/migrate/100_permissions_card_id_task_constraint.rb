55cc75dc560a78affdc8b7a16976676e79526006
lhoffman <lhoffman@ec9d8473-2c57-4e18-8d51-132f0e30f32e>
2007-12-10 04:00:45 +0000

    alternative suggestion for when migration 100 breaks
    git-svn-id: svn+ssh://rubyforge.org/var/svn/wagn/trunk@440 ec9d8473-2c57-4e18-8d51-132f0e30f32e

--- db/migrate/100_permissions_card_id_task_constraint.rb
+++ db/migrate/100_permissions_card_id_task_constraint.rb
@@ -2,9 +2,19 @@ class PermissionsCardIdTaskConstraint < ActiveRecord::Migration
   def self.up 
     User.as :admin do
       Card::Cardtype.find(:all).each do |ct|
-        ## this part is to get rid of duplicates
-        ct.permit(:create, ct.who_can(:create))
+        ## this part is to get rid of duplicates  
+        ct.permit(:create, ct.who_can(:create))        
         ct.save!  
+        
+        # IF THIS THROWS ERRORS, TRY THIS WORKAROUND:
+=begin
+        perm = ct.who_can(:create)
+        Permission.find_all_by_task_and_card_id('create', ct.id).plot(:destroy)
+        ct.permit(:create,perm)
+        ct.save!
+
+=end        
+        
       end
     end
     add_unique_index :permissions, :task, :card_id

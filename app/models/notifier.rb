71860d7df4e96380c048a887b7b2fb5f4dadb191
ethanmcc <ethanmcc@ec9d8473-2c57-4e18-8d51-132f0e30f32e>
2008-06-10 18:50:42 +0000

    fixes to new account notification
    git-svn-id: svn+ssh://rubyforge.org/var/svn/wagn/trunk@641 ec9d8473-2c57-4e18-8d51-132f0e30f32e

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -7,9 +7,13 @@ class Notifier < ActionMailer::Base
     from       "#{from_name} <#{from_user.email}>"
     sent_on    Time.now
     subject    subject
-    body  :username => (user.email    or raise Wagn::Oops.new("Oops didnn't have user email")),
+    body  :email    => (user.email    or raise Wagn::Oops.new("Oops didnn't have user email")),
           :password => (user.password or raise Wagn::Oops.new("Oops didn't have user password")),
-          :url      => "#{System.base_url}/account/login",
+          
+          :card_url => "#{System.base_url}/wagn/#{Cardname.escape(user.card.name)}",
+          :pw_url   => "#{System.base_url}/card/options/#{Cardname.escape(user.card.name)}",
+          
+          :login_url=> "#{System.base_url}/account/login",
           :message  => message.clone
   end                 
   

a4c8cc50b35ed5d16191192262e5451146d7819e
lhoffman <lhoffman@ec9d8473-2c57-4e18-8d51-132f0e30f32e>
2008-06-12 22:09:55 +0000

    fix migration to work from scratch with pruned card table
    git-svn-id: svn+ssh://rubyforge.org/var/svn/wagn/trunk@650 ec9d8473-2c57-4e18-8d51-132f0e30f32e

--- db/migrate/120_add_toggle_and_phrase.rb
+++ db/migrate/120_add_toggle_and_phrase.rb
@@ -1,5 +1,8 @@
 class AddToggleAndPhrase < ActiveRecord::Migration
-  def self.up
+  def self.up     
+    Card::Base.reset_column_information
+    Card::Basic.reset_column_information
+    Card::Cardtype.reset_column_information
     if User.as :admin
       Card::Cardtype.create(:name=>'Toggle')
       Card::Cardtype.create(:name=>'Phrase')

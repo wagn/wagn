b12a77ae1f8219ac037287bb3c4d04e65e04903d
lhoffman <lhoffman@ec9d8473-2c57-4e18-8d51-132f0e30f32e>
2008-07-11 18:45:54 +0000

    comments on wagnbot & link revising
    git-svn-id: svn+ssh://rubyforge.org/var/svn/wagn/trunk@669 ec9d8473-2c57-4e18-8d51-132f0e30f32e

--- lib/wag_bot.rb
+++ lib/wag_bot.rb
@@ -1,11 +1,24 @@
-# kindofahack... worksthough
-User.class_eval "cattr_accessor 'wagbot'"
+=begin
 
-module WagBot
+It looks like the only thing the "WagBot" is currently used for is as author of the "automatic"
+revisions that may happen when a card is renamed.  (cards linking to it are revised to update the links)
+
+I'm not clear on the rationale.  possibilities include:
+1. we want links updated even on cards that the original user wouldn't have permission to edit, but the Wagbot does.
+2. we want to flag the "automatic" updates somehow so as not to show them in recent changes or otherwise filter them.
+
+I guess if we didn't have the Wagbot we'd have to figure out what to do in the case where a card name is changed and links exist
+that the current user doesn't have permission to edit.  we could temporarily raise their permissions level.  Something about the Wagbot construct seems ugly to me.  maybe just the name.
+
+=end
+
+module WagBot 
   def self.instance
-    User.wagbot ||=  (User.find_by_login('wagbot') or User.find_by_login('hoozebot')).extend(WagBot)
+    (User.find_by_login('wagbot') or User.find_by_login('hoozebot')).extend(WagBot)
   end
-  
+
+  # FIXME: I think this method should be somewhere else, maybe card::base, and then in the 
+  # update-links-on-rename do User.as(wagbot){ card.revise_links(old, new) }  --LWH
   def revise_card_links( card, oldlink, newlink )
     content_with_revised_links = Renderer.instance.process(card, nil) do |wiki_content|
       wiki_content.find_chunks(Chunk::Link).each do |chunk|

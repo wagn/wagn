43fdc92ac63d6aeca8ad5dc0d99db1fdbf33c90c
lhoffman <lhoffman@ec9d8473-2c57-4e18-8d51-132f0e30f32e>
2008-07-24 23:23:10 +0000

    fix number on tinymce config migration
    git-svn-id: svn+ssh://rubyforge.org/var/svn/wagn/trunk@697 ec9d8473-2c57-4e18-8d51-132f0e30f32e

--- db/migrate/114_sidebar_to_transclusion.rb
+++ db/migrate/114_sidebar_to_transclusion.rb
@@ -2,7 +2,7 @@ class SidebarToTransclusion < ActiveRecord::Migration
   def self.up
     User.as(:admin) do
       if s = Card['*sidebar']   
-        s.content = Card.search( :plus=>"*sidebar" ).map{|c| "{{#{c.name|open}}}" }.join("\n")
+        s.content = Card.search( :plus=>"*sidebar" ).map{|c| "{{#{c.name}|open}}" }.join("\n")
         s.save!
       end
     end

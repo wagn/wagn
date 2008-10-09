7459f8501f3cdd864c4b1810d57ec079a59fa28f
lhoffman <lhoffman@ec9d8473-2c57-4e18-8d51-132f0e30f32e>
2008-10-09 21:21:35 +0000

    further rspec updates
    git-svn-id: svn+ssh://rubyforge.org/var/svn/wagn/trunk@762 ec9d8473-2c57-4e18-8d51-132f0e30f32e

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,7 +12,6 @@ Spec::Runner.configure do |config|
   config.use_transactional_fixtures = true
   config.use_instantiated_fixtures  = false
   config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
-  
   config.mock_with :flexmock
 
   # == Fixtures

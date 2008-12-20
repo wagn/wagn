3b6d2efd03c1b4b79dfa494d561a9f961696441a
Lewis Hoffman <lewis@grasscommons.org>
2008-12-20 09:35:22 -0600

    update cards when fixing HTML cardtype

--- db/migrate/20081216171432_fix_html_cardtype.rb
+++ db/migrate/20081216171432_fix_html_cardtype.rb
@@ -1,10 +1,11 @@
 class FixHtmlCardtype < ActiveRecord::Migration
-  def self.up
+  def self.up    
     if c = Cardtype.find_by_class_name("HTML1")
       unless Cardtype.find_by_class_name("HTML")
         c.class_name = "HTML"
         c.save!
-      end
+      end   
+      execute %{ update cards set type='HTML' where type='HTML1' }
     end
   end
 

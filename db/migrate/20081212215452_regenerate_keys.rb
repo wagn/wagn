2b78907f01ad3266cb221c68630b5b679677bd65
Lewis Hoffman <lewis@grasscommons.org>
2009-02-27 12:21:12 -0600

    fix handling of failed key generation in migration

--- db/migrate/20081212215452_regenerate_keys.rb
+++ db/migrate/20081212215452_regenerate_keys.rb
@@ -7,10 +7,15 @@ class RegenerateKeys < ActiveRecord::Migration
         old_date = card.updated_at
         puts "Setting key: #{card.name} -> #{card.key}"
         begin
-          card.save!
-          execute %{ update cards set updated_at = #{quote(old_date)} where id=#{card.id}}
-          #card.update_attribute("updated_at", 
-        rescue
+          transaction do 
+            if card.save!
+              puts "card.save successful"
+            end
+            execute %{ update cards set updated_at = #{quote(old_date)} where id=#{card.id}}
+            #card.update_attribute("updated_at", 
+          end
+        rescue Exception=>e
+          puts "rescued exception, creating #{card.name}+broken key"
           c2 = Card.find(card.id)
           if c2.key != card.key
             Card.create :name=>"#{card.name}+broken key", :content=>"failed to change key from #{c2.key} to #{card.key}"

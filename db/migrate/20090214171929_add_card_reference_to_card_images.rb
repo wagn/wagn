114cb29d3cf3e5f81a54a77674279b68f11df9af
Lewis Hoffman <lewis@grasscommons.org>
2009-03-26 19:16:33 -0500

    remove obsoleted migration that keeps breaking

--- db/migrate/20090214171929_add_card_reference_to_card_images.rb
+++
@@ -1,9 +0,0 @@
-class AddCardReferenceToCardImages < ActiveRecord::Migration
-  def self.up
-    add_column :card_images, :card_id, :integer
-  end
-
-  def self.down
-    remove_column :card_images, :card_id
-  end
-end

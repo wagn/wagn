5ed20684c1f5e72d48a236f3f2778ca7d956cdd3
Gerry Gleason <gerry@wiley.private.com>
2009-04-17 11:15:51 -0500

    Accidental uncomment reverted

--- app/views/card/R.sv
+++
@@ -1,5 +0,0 @@
-xml.instruct! :xml, :version => "1.0"
-
-xml.status do
-  xml.result 'success'
-end
--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ ActionController::Routing::Routes.draw do |map|
   map.connect 'me',               :controller => 'card', :action=>'mine'
  
   map.connect ':controller/:action/:id/:attribute' 
-  map.connect '/card/new/:cardtype', :controller=>'card', :action=>'new'
+  #map.connect '/card/new/:cardtype', :controller=>'card', :action=>'new'
   
   map.connect ':controller/:action/:id.:format',  :requirements=>{ :id=>/.*/, :format=>FORMAT_PATTERN  }
   map.connect ':controller/:action/:id',  :requirements=>{ :id=>/.*/ }

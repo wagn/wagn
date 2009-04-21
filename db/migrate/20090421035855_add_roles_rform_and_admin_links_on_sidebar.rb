04a32c2544de32c32798fdb019adc289e641e4f6
Ethan McCutchen <ethan@cutch.local>
2009-04-20 22:25:17 -0600

    new migration to fix *roles+*rform and admin links

---
+++ db/migrate/20090421035855_add_roles_rform_and_admin_links_on_sidebar.rb
@@ -0,0 +1,21 @@
+class AddRolesRformAndAdminLinksOnSidebar < ActiveRecord::Migration
+  def self.up
+    User.as :wagbot
+    
+    if c=Card['*roles+*rform']
+      c.extension_type='HardTemplate'
+      c.save
+    end
+    if c=Card['Administrator links']
+      c.permit(:read, Role[:admin])
+      c.save
+    end
+    if c=Card['*sidebar']
+      c.content = c.content+"<p>&nbsp;</p><p>{{Administrator links|open}}</p>"
+      c.save
+    end
+  end
+
+  def self.down
+  end
+end

b590870e85c0b70a433969660a26613a2336e838
Ethan McCutchen <ethan@cutch.local>
2009-04-22 16:58:18 -0600

    fixed permissions on settings deletions

--- db/migrate/20090421172134_delete_setting_cards.rb
+++ db/migrate/20090421172134_delete_setting_cards.rb
@@ -1,5 +1,6 @@
 class DeleteSettingCards < ActiveRecord::Migration
   def self.up
+    User.as :wagbot
     if setting_card = Card['Setting'] and setting_card.type=='Cardtype'
       Card::Setting.find(:all).each do |card|
         card.extension = nil

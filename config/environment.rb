e76cd8ba11e25913e44b28c347a00d1e10009215
Lewis Hoffman <lewis@grasscommons.org>
2009-04-23 14:06:29 -0500

    add gem configuration to environment; require uuid as gem

--- config/environment.rb
+++ config/environment.rb
@@ -4,15 +4,18 @@
 # you don't control web/app server and can't set it the proper way
 # ENV['RAILS_ENV'] ||= 'production'
 
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
   
 # needs to be loaded for all files, before migrations, etc.
 #require "lib/wagn"         
-                          
+#ActiveRecord::Base.logger.info("after boot, before config")
+
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence those specified here
   #RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION  
+
   # Skip frameworks you're not going to use
   config.frameworks -= [ :action_web_service ]
 
@@ -37,15 +40,22 @@ Rails::Initializer.run do |config|
   # See Rails::Configuration for more options   
   
   #config.gem "rspec-rails", :lib => "spec"          
+  config.gem "uuid"
+  config.gem "json-pure"
 
   # FIXME: should we also set :secret ?
-  require 'yaml'
-  db = YAML.load_file('config/database.yml')
+  require 'yaml'   
+  require 'erb'     
+  database_configuration_file = 'config/database.yml'
+  db = YAML::load(ERB.new(IO.read(database_configuration_file)).result)
   config.action_controller.session = {
     :session_key => db[RAILS_ENV]['session_key'],
     :secret      => db[RAILS_ENV]['secret']
   }  
 end
+   
+ActiveRecord::Base.connection.reconnect!          
+ActiveRecord::Base.logger.info("after config")
 
 
 #ExceptionNotifier.exception_recipients = %w(someone@somewhere.org)

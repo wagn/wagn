caa5018f6a85c9d145c1c50b588e6d22021128cc
Lewis Hoffman <lewis@grasscommons.org>
2009-07-21 13:45:52 -0500

    add missing  *watchers to bootstrap & test data

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090618184743) do
+ActiveRecord::Schema.define(:version => 20090721173947) do
 
   create_table "card_files", :force => true do |t|
     t.string   "filename"

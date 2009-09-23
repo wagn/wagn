d9a1d0fb7577ac7c04844105794ecbc2b1c5b6b5
Lewis Hoffman <lewis@grasscommons.org>
2009-09-23 17:59:04 -0500

    fix wicked trash-rename-recreate bug

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,7 +18,7 @@ config.action_controller.perform_caching             = true
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
 
-config.log_level = :info
+config.log_level = :debug
 
 config.gem "rspec-rails", :version => "~>1.2.6", :lib => false
 config.gem "webrat", :version => "~>0.4.4", :lib => false

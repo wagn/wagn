f121a0c54d42cf9bb5877b8ef3485b9a4fe09534
Lewis Hoffman <lewis@grasscommons.org>
2009-11-09 19:14:27 -0800

    update test data for pattern specs

--- test/fixtures/shared_data.rb
+++ test/fixtures/shared_data.rb
@@ -32,6 +32,7 @@ class SharedData
       request_card = Card::InvitationRequest.create! :name=>"Sample InvitationRequest" #, :email=>"invitation@request.com"  
       Cardtype.find(:all).each do |ct|
         next if ['User','InvitationRequest'].include? ct.codename
+        puts "CODENAME: #{ct.codename}"
         Card.create! :type=>ct.codename, :name=>"Sample #{ct.codename}"
       end
       # data for role_test.rb

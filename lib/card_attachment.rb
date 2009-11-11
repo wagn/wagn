ef31e51e89dda545e2a2a69bb8087d4b3d421328
Lewis Hoffman <lewis@grasscommons.org>
2009-11-11 13:44:37 -0800

    tweaks for card lib refactoring

--- lib/card_attachment.rb
+++ lib/card_attachment.rb
@@ -1,31 +1,30 @@
-module Card
-  module CardAttachment
-    module ActMethods
-      def card_attachment(klass)
-        extend ClassMethods unless (class << self; included_modules; end).include?(ClassMethods)
-        include InstanceMethods unless included_modules.include?(InstanceMethods)
-        
-        cattr_accessor :attachment_model
-        attr_accessor :attachment_id
-    	  after_save :update_attachment
-    	                                  
-    	  self.attachment_model = klass
+module CardAttachment
+  module ActMethods
+    def card_attachment(klass)
+      extend ClassMethods unless (class << self; included_modules; end).include?(ClassMethods)
+      include InstanceMethods unless included_modules.include?(InstanceMethods)
+      
+      cattr_accessor :attachment_model
+      attr_accessor :attachment_id
+  	  after_save :update_attachment
+  	                                  
+  	  self.attachment_model = klass
+    end
+  end
+
+  module ClassMethods
+  end
+
+  module InstanceMethods
+    def update_attachment
+      if attachment_id and !attachment_id.blank?
+        attachment_model.find( attachment_id ).update_attribute :revision_id, current_revision_id
       end
     end
 
-    module ClassMethods
-    end
-
-    module InstanceMethods
-      def update_attachment
-        if attachment_id and !attachment_id.blank?
-          attachment_model.find( attachment_id ).update_attribute :revision_id, current_revision_id
-        end
-      end
-
-      def attachment
-        attachment_model.find_by_revision_id( current_revision_id )
-      end
+    def attachment
+      attachment_model.find_by_revision_id( current_revision_id )
     end
   end
 end
+

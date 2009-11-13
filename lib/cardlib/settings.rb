5022f97c585cbf7cd7914f0288fc1c3aabdf0ff4
Lewis Hoffman <lewis@grasscommons.org>
2009-11-13 12:57:48 -0800

    move cardlib from models to lib

--- lib/cardlib/settings.rb
+++ lib/cardlib/settings.rb
@@ -1,9 +1,9 @@
 module Cardlib
   module Settings
     def setting setting_name
-      Pattern.keys_for_card( self ).each do |key|
+      Wagn::Pattern.keys_for_card( self ).each do |key|
         if pattern_card = Card.find_by_pattern_spec_key( key )
-          if setting_card = CachedCard.get( "#{pattern_card.name}+*#{setting}" ) 
+          if setting_card = CachedCard.get( "#{pattern_card.name}+*#{setting_name}" ) 
             return setting_card.content
           end
         end

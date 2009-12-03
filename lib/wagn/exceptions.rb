d4d5855ecbbb53ccb412ae2c71b498da64a68093
Ethan McCutchen <ethan@cutch.(none)>
2009-12-03 14:02:17 -0700

    refactor exceptions to work with application reloading in development

--- lib/wagn/exceptions.rb
+++ lib/wagn/exceptions.rb
@@ -1,42 +1,10 @@
 module Wagn
   module Exceptions
-  end
-  
-  class Error < StandardError
-  end
-  
-  class NotFound < Error
-  end
-  
-  class PermissionDenied < Error
-  end
-  
-  class Oops < Error
-  end
-
-  class RecursiveTransclude < Error
-  end     
-  
-  class WqlError < Error
+    Error               = Class.new StandardError
+    NotFound            = Class.new Error
+    PermissionDenied    = Class.new Error
+    Oops                = Class.new Error
+    RecursiveTransclude = Class.new Error
+    WqlError            = Class.new Error
   end
 end
-
-# FIXME: this is here because errors defined in permissions break without it? kinda dumb
-module Card    
-  class CardError < Wagn::Error   
-    attr_reader :card
-    def initialize(card)
-      @card = card
-      super build_message 
-    end   
-    
-    def get_card
-      @card
-    end 
-    
-    def build_message
-      "#{@card.name} has errors: #{@card.errors.full_messages.join(', ')}"
-    end
-  end
-end
-  

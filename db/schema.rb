72b3ad8fde7a8e7e16bcfcec6be095ffcf4872d5
Ethan McCutchen <ethan@cutch.(none)>
2009-12-04 15:36:51 -0700

    loose ends: migrated help and defaults and updated noside layout

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091203185655) do
+ActiveRecord::Schema.define(:version => 20091204221210) do
 
   create_table "card_files", :force => true do |t|
     t.string   "filename"

f97f3b7ddb25015dc573b224dae8030f85369855
Lewis Hoffman <lewis@grasscommons.org>
2009-12-17 16:07:00 -0700

    implement fallback settings and refactor add / edit help

--- spec/lib/cardlib/setting_spec.rb
+++ spec/lib/cardlib/setting_spec.rb
@@ -4,7 +4,7 @@ describe Card do
   before do
     User.as(:wagbot)
   end
-  
+
   context "settings" do
     it "retrieves Set based value" do
       Card.create :name => "Book+*type+*add help", :content => "authorize"
@@ -21,6 +21,27 @@ describe Card do
     it "retrieves single values" do
       Card.create :name => "banana+*self+*edit help", :content => "pebbles"
       Card["banana"].setting('edit help').should == "pebbles"
+    end
+  end
+  
+  context "cascading settings" do
+    before do
+      Card.create :name => "*all+*edit help", :content => "edit any kind of card"
+    end
+    
+    it "retrieves default setting" do
+      Card.new( :type => "Book" ).setting('add help').should == "edit any kind of card"
+    end
+    
+    it "retrieves primary setting" do
+      Card.create :name => "*all+*add help", :content => "add any kind of card"
+      Card.new( :type => "Book" ).setting('add help').should == "add any kind of card"
+    end
+    
+    it "retrieves more specific default setting" do
+      Card.create :name => "*all+*add help", :content => "add any kind of card"
+      Card.create :name => "*Book+*type+*edit help", :content => "edit a Book"
+      Card.new( :type => "Book" ).setting('add help').should == "add any kind of card"
     end
   end
 end

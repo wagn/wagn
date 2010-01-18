9a480cd3396e38b1c2e0a4c85ebafdaf51af636a
Lewis Hoffman <lewis@grasscommons.org>
2010-01-18 15:16:37 -0700

    tighten up hooks

--- app/models/card_observer.rb
+++ app/models/card_observer.rb
@@ -1,9 +1,8 @@
 class CardObserver < ActiveRecord::Observer
   observe Card
-  Wagn::Hook
   [:before_save, :before_create, :after_save, :after_create].each do |hookname| 
     define_method( hookname ) do |card|
-      Wagn::Hook::Card.invoke hookname, card
+      Wagn::Hook.invoke hookname, card
     end
   end
 end

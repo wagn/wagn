e3ce7d43500eb560c2f9659a25d74665de233d66
Lewis Hoffman <lewis@grasscommons.org>
2010-01-18 21:46:53 -0700

    move hooks from card_observer to main card class
    in trying to implement flexmail it appeared that callbacks in observer were being called twice.
    switching methods fixed the problem much quicker than I expect diagnosing the observer issue would have been.

--- app/models/card_observer.rb
+++
@@ -1,8 +0,0 @@
-class CardObserver < ActiveRecord::Observer
-  observe Card
-  [:before_save, :before_create, :after_save, :after_create].each do |hookname| 
-    define_method( hookname ) do |card|
-      Wagn::Hook.invoke hookname, card
-    end
-  end
-end

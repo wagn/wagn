b0160184f0830b3f9b0f9b84c7c9dd94f5db6bef
Lewis Hoffman <lewis@grasscommons.org>
2010-03-02 14:35:35 -0700

    add create_or_update and save_all

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,54 @@
+module CardData
+  def extract_plus_data!
+    keys.inject({}) {|h,k| h[k] = delete(k) if k =~ /^\+/; h }
+  end
+end
+
 module Card
+  class << self
+    def create_or_update args
+      if c = Card[ args[:name] ]
+        c.update_attributes args
+      else
+        Card.create! args
+      end
+    end
+    
+    def save_all data, opts = {}
+      options = {
+        :strategy => :create_or_update,  # :find_or_new, :create!, :update
+        :plus_strategy => :create_or_update
+      }.merge( opts )
+
+      data.extend CardData
+      plusses = data.extract_plus_data!
+
+      Card::Base.transaction do
+        base_card = Card.send options[:strategy], data
+
+        plusses.each do |plus_name, plus_data|
+          plus_card_name = base_card.name + plus_name
+          case plus_data
+            when String;  
+              Card.send options[:plus_strategy], :name=> plus_card_name, :content => plus_data
+            when Array;
+              if block_given?
+                plus_data = plus_data..map{|x| yield(plus_name, x) }
+              end
+              card_args = {
+                :name => plus_card_name, 
+                :type => "Pointer",  
+                :content => plus_data.map{|x| "[[#{x}]]" }.join("\n")
+              }
+              Card.send options[:plus_strategy], card_args
+            when Hash;
+              plus_data[:name] ||= plus_card_name
+              Card.send options[:plus_strategy], plus_data
+          end
+        end
+      end
+    end
+
+  end
 end  
 

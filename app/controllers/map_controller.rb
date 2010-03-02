80a34711bc17ba7c9fa7e284c7392e6c2b666864
Vine <vineacorn@gmail.com>
2010-03-01 21:10:15 -0600

    Don't show deleted Patterns; do show orphan Patterns

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,25 +1,35 @@
+# map_controller v0.2
+
+# Changes from v0.1:
+#   Derive from ApplicationController instead of ActionController::Base
+#     so database switching by domain works.
+#   Use more elegant and efficient representations of "content" and "done".
+#   Include check for deleted ("trash") cards.
+#   Use WQL to find unlinked Pattern cards in case "type" in the database
+#     is not the type displayed in the UI.
+
 class MapController < ApplicationController
   layout nil
   
   def show
-    @content = ""
-    done = []
+    content = []
+    done = {}
     cards = Card.all(:include => :current_revision, 
-      :conditions => "name LIKE '%+related patterns'")
+      :conditions => "name LIKE '%+related patterns' AND NOT trash")
     cards.each do |card|
       name = card.name.sub(/\+related patterns$/,'')
       card.current_revision.content.scan(/\[\[([^\]]*)\]\]/) do |related|
-        @content += name+"~->~"+related[0]+"\n"
-        done.push(name);
-        done.push(related[0])
+        content << name+"~->~"+related[0]
+        done[name] = true
+        done[related[0]] = true
       end
     end
-    cards = Card.find_all_by_type("Pattern")
+    cards = Card.search(:type=>'Pattern')
     cards.each do |card|
-      name = card.name
-      if (!done.include?(name))
-        @content += name+"\n"
+      if (!done[card.name])
+        content << card.name
       end
     end
+    @content = content.join("\n")
   end
 end

4155d5a9e5025f1e664fa8c50f37e7354a24d2b8
Lewis Hoffman <lewis@grasscommons.org>
2010-04-14 18:06:14 -0600

    convert permission test to spec.  fix "whole-suite" failures.

---
+++ spec/permission_spec_helper.rb
@@ -0,0 +1,60 @@
+module PermissionSpecHelper
+
+  def assert_hidden_from( user, card, msg='')
+    ::User.as(user) { assert_hidden( card, msg ) }
+  end
+
+  def assert_not_hidden_from( user, card, msg='')
+    ::User.as(user) { assert_not_hidden( card, msg ) }
+  end
+
+  def assert_locked_from( user, card, msg='')
+    ::User.as(user) { assert_locked( card, msg ) }
+  end
+
+  def assert_not_locked_from( user, card, msg='')
+    ::User.as(user) { assert_not_locked( card, msg ) }
+  end
+  
+  def assert_hidden( card, msg='' )
+    assert_equal [], Card.search(:id=>card.id).plot(:name), msg  
+  end
+  
+  def assert_not_hidden( card, msg='' )
+    assert_equal [card.name], Card.search(:id=>card.id).plot(:name), msg
+  end
+  
+  def assert_locked( card, msg='' )
+    assert_equal false, card.ok?(:edit), msg
+  end
+  
+  def assert_not_locked( card, msg='' )
+    assert_equal true, card.ok?(:edit), msg
+  end
+end
+
+ActiveSupport::TestCase.send :include, PermissionSpecHelper
+
+
+module Card
+  class Base
+    def writeable_by(user)
+      ::User.as(user) do
+        ok? :edit
+      end
+    end
+    
+    def readable_by(user)
+      ::User.as(user) do
+        ok? :read
+      end
+    end
+    
+    def appendable_by(user)
+      ::User.as(user) do
+        ok? :append
+      end
+    end 
+  end
+end
+ 

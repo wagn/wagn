d65ce9e7aee64fda87c1a80a6a815a8055f327fa
Lewis Hoffman <lewis@grasscommons.org>
2010-04-16 11:54:41 -0600

    remove cookie class- I can't see that it's serving any purpose.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,34 +9,6 @@ var warn = function(stuff) {
     console.log( stuff );
 }
 
-var Cookie = {
-  set: function(name, value, daysToExpire) {
-    var expire = '';
-    if (daysToExpire != undefined) {
-      var d = new Date();
-      d.setTime(d.getTime() + (86400000 * parseFloat(daysToExpire)));
-      expire = '; expires=' + d.toGMTString();
-    }
-    return (document.cookie = escape(name) + '=' + escape(value || '') + expire);
-  },
-  get: function(name) {
-    var cookie = document.cookie.match(new RegExp('(^|;)\\s*' + escape(name) + '=([^;\\s]*)'));
-    return (cookie ? unescape(cookie[2]) : null);
-  },
-  erase: function(name) {
-    var cookie = Cookie.get(name) || true;
-    Cookie.set(name, '', -1);
-    return cookie;
-  },
-  accept: function() {
-    if (typeof navigator.cookieEnabled == 'boolean') {
-      return navigator.cookieEnabled;
-    }
-    Cookie.set('_test', '1');
-    return (Cookie.erase('_test') === '1');
-  }
-};
-
 Wagn.Messenger = {  
   element: function() { return $('alerts') },
   alert: function( message ) {

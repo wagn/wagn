9d7fb1d8068370291ca933ae2901975c08b3b691
Lewis Hoffman <lewis@grasscommons.org>
2010-04-23 09:21:45 -0700

    *send email setting default migrations

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100422235300) do
+ActiveRecord::Schema.define(:version => 20100423161721) do
 
   create_table "card_files", :force => true do |t|
     t.string   "filename"

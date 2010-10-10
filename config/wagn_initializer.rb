2ba196ca70d84702c7a70f712c3b8fd721da2ec8
Ethan McCutchen <ethan@grasscommons.org>
2010-10-10 10:23:22 -0600

    added htmlentities to initializer

--- config/wagn_initializer.rb
+++ config/wagn_initializer.rb
@@ -7,6 +7,7 @@ module Wagn
         config.frameworks -= [ :action_web_service ]
         config.gem "uuid"
         config.gem "json"
+        config.gem "htmlentities"
         unless ENV['RUN_CODE_RUN']
           config.gem "hoptoad_notifier"
         end

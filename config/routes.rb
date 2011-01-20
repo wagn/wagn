82f26e48856e1f13c6c905043646de89d2b303ab
Ethan McCutchen <ethan@grasscommons.org>
2011-01-20 16:07:50 -0700

    first stab at getting css link tags working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-FORMATS = "html|json|xml|rss|kml" unless defined? FORMATS
+FORMATS = "html|json|xml|rss|kml|css|txt|text" unless defined? FORMATS
 FORMAT_PATTERN = /#{FORMATS}/ unless defined? FORMAT_PATTERN   
 
 # This regexp solves issues with cards with periods in the name.  Also makes it so you don't have to write separate routes for

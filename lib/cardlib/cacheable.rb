6da98d8060d7182e56613655a9e0f65a3ac41dd8
Ethan McCutchen <ethan@grasscommons.org>
2011-03-21 12:39:31 -0600

    limit contextual overrides in flexmail settings

--- lib/cardlib/cacheable.rb
+++ lib/cardlib/cacheable.rb
@@ -1,26 +1,15 @@
 module Cardlib
   module Cacheable
     
-    
     def extended_list context = nil
-      args={:context => (context ? context.name : self.name), :limit=>''}
-      self.item_cards(args).map{|x| x.item_cards(args) }.flatten.map{|y| y.item_names(args) }.flatten
+      context = (context ? context.name : self.name)
+      args={ :limit=>'' }
+      self.item_cards(args.merge(:context=>context)).map{|x| x.item_cards(args) }.flatten.map{|y| y.item_names(args) }.flatten
       # this could go on and on..
     end
     
     def contextual_content context = nil
       Slot.new(context).process_content(Slot.new(self)._render_raw)
     end
-
-
-    
-    # FIXME: limit moved here from pointer card.
-    # This fixes explosion creating pointer cards, but it
-    # should be refactored.    
-    #def limit
-    #  card = System.setting("#{self.name.tag_name}+*max") or return nil
-    #  card.content.strip.to_i
-    #end    
-    
   end
 end

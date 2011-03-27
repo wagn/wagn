99408d8efb37957dfd733efe0401407d65392b07
Ethan McCutchen <ethan@grasscommons.org>
2011-03-26 18:26:47 -0600

    made layout_card more self-documented / clearer

--- app/models/system.rb
+++ app/models/system.rb
@@ -55,27 +55,30 @@ class System < ActiveRecord::Base
     def toggle(val)
       val == '1'
     end
-
+    
     def layout_card(card, cardname)
       User.as(:wagbot) do 
-        case
-          when ( cardname.present? and 
-            lo_card = Card.fetch(cardname, :skip_virtual => true) and
-            lo_card.ok?(:read))         ;
-Rails.logger.info "layout_card cd(#{card}, #{cardname}) #{lo_card}"
-            lo_card
-          when ( (setting_card = (card && card.setting_card('layout') or
-                                   Card.default_setting_card('layout'))) and
-            #throwing lots of warnings, cucumber: setting_card.type == 'Pointer'
-            setting_card.is_a?(Card::Pointer) and
-            layout_name=setting_card.first and !layout_name.nil? and
-            lo_card = Card.fetch(layout_name, :skip_virtual => true) and
-            lo_card.ok?(:read) )          ;
-Rails.logger.info "layout_card st(#{card}, #{cardname}) #{lo_card}"; lo_card
-          when block_given?             ;
-            yield cardname
-          end
+        layout_from_url(cardname) or 
+        layout_from_setting(card) or
+        (block_given? and yield(cardname))
       end
+    end
+    
+    def layout_from_url(cardname)
+      return nil unless cardname.present? and 
+        lo_card = Card.fetch(cardname, :skip_virtual => true) and
+        lo_card.ok?(:read)
+      lo_card
+    end
+    
+    def layout_from_setting(card)
+      return unless setting_card = ((card && card.setting_card('layout')) or Card.default_setting_card('layout'))
+      return unless setting_card.is_a?(Card::Pointer) and  # type check throwing lots of warnings under cucumber: setting_card.type == 'Pointer'        and
+        layout_name=setting_card.item_names.first     and
+        !layout_name.nil?                             and
+        lo_card = Card.fetch(layout_name, :skip_virtual => true)    and
+        lo_card.ok?(:read)
+      lo_card
     end
     
     def image_setting(name)

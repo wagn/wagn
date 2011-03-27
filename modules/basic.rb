bd2ff7557b7d0b3c6fc0191198ce5c96295ce3db
Gerry Gleason <gerrg@inbox.com>
2011-03-27 17:51:11 -0500

    merge from ethan

--- modules/basic.rb
+++ modules/basic.rb
@@ -77,7 +77,12 @@ class Renderer
   <span id="navbox_background">
     <a id="navbox_image" title="Search" onClick="navboxOnSubmit($('navbox_form'))">&nbsp;</a>
     <input type="text" name="navbox" value="#{ params[:_keyword] || '' }" id="navbox_field" autocomplete="off" />
-     #{ navbox_complete_field('navbox_field') }
+    #{ #navbox_complete_field('navbox_field')
+      content_tag("div", "", :id => "navbox_field_auto_complete", :class => "auto_complete") +
+      auto_complete_field('navbox_field', {
+        :url =>"/card/auto_complete_for_navbox/",
+        :after_update_element => "navboxAfterUpdate" }.update({}))
+    }
   </span>
 </form>
     }

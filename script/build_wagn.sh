9f9c358f32ef830ca4939fdf9ff5ca8da658df86
Ethan McCutchen <ethan@grasscommons.org>
2011-05-09 12:49:29 -0600

    further tweaking ci build script

--- script/build_wagn.sh
+++ script/build_wagn.sh
@@ -1,47 +1,26 @@
+#!/bin/bash
+
 bundle install
 
-# will not re-create databases from scratch if config/wagn.rb is in place 
 if [ ! -f config/wagn.rb ]; then
+  export WAGN_CI_MODE=scratch
   cp config/cruise.wagn.rb config/wagn.rb
-
-  git update-index --assume-unchanged .gitignore #config/environment.rb
-  git config --global alias.co checkout
-
-
-  for db_config in config/cruise.*.database.yml; do
-    cp $db_config config/database.yml
-    #if [ -f config/cruise.schema.rb ]; then
-    #  cp config/cruise.schema.rb db/schema.rb
-    #fi
-
-    rake wagn:create --trace
-
-    env RAILS_ENV=test rake db:create
-
-    # setup for tests
-    env RELOAD_TEST_DATA=true rake db:test:prepare
-  done
+else
+  echo -e "USING PRE-EXISTING DATABASE\n to regenerate, delete config/wagn.rb"
 fi
 
 for db_config in config/cruise.*.database.yml; do
-  cp $db_config config/database.yml
-  echo -e "\n\n"
-  echo DATABASE CONFIGURATION: $db_config
-  echo -e "\n\n"
-
-  rake testspec
-#  echo -n "Starting test/* at: "; date
-#  if ! env RAILS_ENV=test rake test --trace 2>&1     |./script/test_filter | tee tst.out; then
-#    exit $?
-#  fi
-
-#  echo -n "Starting spec at: "; date
-#  if ! env RAILS_ENV=test rake spec --trace 2>&1     |./script/test_filter | tee spc.out; then
-#    exit $?
-#  fi
-
-#  echo -n "Starting cucumber at: "; date
-#  if ! env RAILS_ENV=test rake cucumber --trace 2>&1 |./script/test_filter | tee cuc.out; then
-#    exit $?
-#  fi
+  echo -e "~~~~~~\nDATABASE CONFIGURATION: $db_config\n~~~~~~~"
+  
+  if [ $WAGN_CI_MODE = scratch ]; then
+    cp $db_config config/database.yml
+    echo "creating wagn database"
+    rake wagn:create --trace
+#    env RAILS_ENV=test rake db:create
+    echo "reloading test data"
+    env RELOAD_TEST_DATA=true rake db:test:prepare
+  fi
+  
+  rake testspec  
 done
+

356c9576c28457337a39703fa6061767f50e4b83
Ethan McCutchen <ethan@grasscommons.org>
2011-05-10 14:07:45 -0600

    specs for email message (and reorganized pack spec helper)

---
+++ spec/packs/default/email_pack_spec.rb
@@ -0,0 +1,9 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+require File.dirname(__FILE__) + '/../pack_spec_helper'
+
+describe EmailHtmlRenderer do
+  it "should render full urls" do
+    System.base_url = 'http://www.fake.com'
+    render_content('[[B]]', :format=>'email_html').should == '<a class="known-card" href="http://www.fake.com/wagn/B">B</a>'
+  end
+end

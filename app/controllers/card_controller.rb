9966488ae8d5af5c0ac46948d2db8545a2a0e49d
Ethan McCutchen <ethan@grasscommons.org>
2011-05-12 21:02:32 -0600

    fixed pointer issue with "add another"

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -373,7 +373,7 @@ class CardController < ApplicationController
     load_card if params[:id]
     @card ||= Card.new(:type=>'Pointer', :skip_defaults=>true)
     #render :partial=>'types/pointer/field', :locals=>params.merge({:link=>:add,:card=>@card})
-    render(:text => Renderer.new(@card).render(:field, :link=>:add, :index=>params[:index]) )
+    render(:text => Renderer.new(@card, :context=>params[:eid]).render(:field, :link=>:add, :index=>params[:index]) )
   end
 
 end

fa7574c2f61bfd55a71b4a7a36ad1f9b33c476c8
Ethan McCutchen <ethan@grasscommons.org>
2011-08-16 14:27:42 -0600

    migrate new content for *all plus, *star, and *rstar

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110815225422) do
+ActiveRecord::Schema.define(:version => 20110816201444) do
 
   create_table "card_files", :force => true do |t|
     t.string   "filename"

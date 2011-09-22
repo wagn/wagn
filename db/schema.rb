aacc6f6c7fede2af41b7eac85e0fa661044c42ac
Gerry Gleason <gerrg@inbox.com>
2011-09-22 05:56:59 -0500

    ...

--- db/schema.rb
+++ db/schema.rb
@@ -82,6 +82,11 @@ ActiveRecord::Schema.define(:version => 20110816201444) do
 
   add_index "cardtypes", ["class_name"], :name => "cardtypes_class_name_uniq", :unique => true
 
+  create_table "codename", :force => true do |t|
+    t.integer "card_id",  :null => false
+    t.string  "codename", :null => false
+  end
+
   create_table "db_files", :force => true do |t|
     t.binary "data"
   end

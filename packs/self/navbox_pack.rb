99f78d47d4e154159ddb85cca516cf15579082d9
Ethan McCutchen <ethan@grasscommons.org>
2011-09-27 16:48:09 -0600

    remove unused navbox method

--- packs/self/navbox_pack.rb
+++ packs/self/navbox_pack.rb
@@ -1,6 +1,5 @@
 class Wagn::Renderer
   define_view(:raw, :name=>'*navbox') do
-#Rails.logger.debug("Builtin *navbox")
     #ENGLISH
     %{
 <form id="navbox_form" action="/search" onsubmit="return navboxOnSubmit(this)">
@@ -18,13 +17,4 @@ class Wagn::Renderer
     }
   end
   alias_view(:raw, {:name=>'*navbox'}, :naked)
-
-
-  private
-  def navbox_complete_field(fieldname, card_id='')
-    content_tag("div", "", :id => "#{fieldname}_auto_complete", :class => "auto_complete") +
-    auto_complete_field(fieldname, { :url =>"/card/auto_complete_for_navbox/#{card_id.to_s}",
-      :after_update_element => "navboxAfterUpdate"
-     }.update({}))
-  end
 end

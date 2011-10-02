b1b333a56c3f66a94d8d2ad7115c944b65bcecca
Gerry Gleason <gerrg@inbox.com>
2011-10-02 14:20:10 -0500

    Make card_attachment consistent with mainline

--- lib/wagn/model/card_attachment.rb
+++ lib/wagn/model/card_attachment.rb
@@ -1,21 +1,7 @@
 module Wagn::Model::CardAttachment
-  def self.included(base)
-    super
-    #Rails.logger.debug "add act methods for attachements #{base} #{self}"
-    base.extend(CardMethods)
-  end
-
-  mattr_accessor :attachment_model
-
-  module CardMethods
-
-    def card_attachment(klass)
-      include InstanceMethods unless included_modules.include?(InstanceMethods)
-    
-      attr_accessor :attachment_id
-      #after_save :update_attachment moved to Card (with null update to override)
-
-      Wagn::Model::CardAttachment.attachment_model = klass
+  def update_attachment
+    if attachment_id and !attachment_id.blank?
+      attachment_model.find( attachment_id ).update_attribute :revision_id, current_revision_id
     end
   end
 

e7018a0d21d2d9f1ffe07a79ddf99d941abb0fb6
Ethan McCutchen <ethan@grasscommons.org>
2011-10-07 16:32:36 -0600

    fixed routing bugs

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ ID_REQS = { :id => /([^\.]*(\.(?!(#{FORMATS})))?)*/, :format=>FORMAT_PATTERN }
 
 
 Wagn::Application.routes.draw do
-  match 'rest/:id.:format' => 'rest_card#method', :constraints => { :id => /.*/ }, :via => [:get, :post, :put, :delete]
+  match 'rest/:id(.:format)' => 'rest_card#method', :constraints => { :id => /.*/ }, :via => [:get, :post, :put, :delete]
   # these file requests should only get here if the file isn't present.
   # if we get a request for a file we don't have, don't waste any time on it.
   #FAST 404s
@@ -15,11 +15,11 @@ Wagn::Application.routes.draw do
   match 'file/:foo.:format' => 'application#render_fast_404'
   match 'images/:foo/:bar' => 'application#render_fast_404', :constraints => { :bar => /.*/ }
   
-  match 'wagn/:id.:format' => 'card#show', :constraints => ID_REQS
+  match 'wagn/:id(.:format)' => 'card#show', :constraints => ID_REQS
   
   match 'recent' => 'card#show', :id => '*recent', :view => 'content'
-  match 'recent.:format' => 'card#show', :id => '*recent', :view => 'content', :format => FORMAT_PATTERN
-  match 'search/:_keyword.:format' => 'card#show', :id => '*search', :view => 'content', 
+  match 'recent(.:format)' => 'card#show', :id => '*recent', :view => 'content', :format => FORMAT_PATTERN
+  match 'search/:_keyword(.:format)' => 'card#show', :id => '*search', :view => 'content', 
     :constraints => { :_keyword => /([^\.]*(\.(?!(#{FORMATS})))?)*/, :format => FORMAT_PATTERN }
     
   match 'new/:type' => 'card#new'
@@ -29,8 +29,8 @@ Wagn::Application.routes.draw do
   resource :card_files
   
   match ':controller/:action/:id/:attribute' #=> '#index'
-  match ':controller/:action/:id.:format', :constraints=> ID_REQS
-  match ':controller/:action.:format', :format => FORMAT_PATTERN
+  match ':controller/:action/:id(.:format)', :constraints=> ID_REQS
+  match ':controller/:action(.:format)', :format => FORMAT_PATTERN
   match ':controller(/:action)' => '#index'
   
 # these were translated by rake rails:upgrade:routes but seemed dubious to me. attempted corrections above
@@ -42,7 +42,7 @@ Wagn::Application.routes.draw do
 #  match ':controller' => '#index'
   match '' => 'card#index'
   
-  match ':id.:format' => 'card#show', :constraints => ID_REQS
+  match ':id(.:format)' => 'card#show', :constraints => ID_REQS
   match '*id' => 'application#render_404'
 end
 

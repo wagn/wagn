67735d9b8ac804f34e4bb4836272886bb3fb2d34
Gerry Gleason <gerrg@inbox.com>
2011-12-09 09:03:59 -0600

    cleaning up, getting ready to integrate with develop

--- packs/type/etherpad_pack.rb
+++ packs/type/etherpad_pack.rb
@@ -4,7 +4,7 @@ class Wagn::Renderer::Html
   define_view(:current_naked) do |args| _render_naked end
 
   define_view(:current, :fallback=>:raw, :type=>'Etherpad') do |args|
-    Rails.logger.debug "current_pad view #{card}, #{card.inspect}"
+    #Rails.logger.debug "current_pad view #{card}, #{card.inspect}"
     card.include_set_modules
     card.get_pad_content
   end
@@ -18,24 +18,28 @@ class Wagn::Renderer::Html
   end
 
   # edit views
+=begin not sure anymore why we want/need this
   define_view(:edit, :type=>'Etherpad') do |args|
     @state=:edit
-    self._render_editor
+    wrap(:edit, args) do
+      self._render_editor
+    end
   end
+=end
 
   define_view(:editor, :type=>'Etherpad') do |args|
     eid, raw_id = context, context+'-raw-content'
-    epad_opts = card.pad_options
+    pad_opts = card.pad_options
     %{#{form.hidden_field( :content, :id=>"#{eid}-hidden-content" )}#{
-      text_area_tag :content_to_replace, '...', :style=>'display:none', :id=>"#{eid}-epad"
+      text_area_tag :content_to_replace, '...', :style=>'display:none', :id=>"#{eid}-etherpad"
       }<iframe id="epframe-#{eid}" width="100%" height="500" src="#{
-      epad_opts[:url]}#{card.key
-      }?showControls=#{epad_opts[:showControls]
-      }&showChat=#{epad_opts[:showChat]
-      }&showLineNumbers=#{epad_opts[:showLineNumbers]
-      }&useMonospaceFont=#{epad_opts[:useMonospaceFont]
+      pad_opts[:url]}#{card.key
+      }?showControls=#{pad_opts[:showControls]
+      }&showChat=#{pad_opts[:showChat]
+      }&showLineNumbers=#{pad_opts[:showLineNumbers]
+      }&useMonospaceFont=#{pad_opts[:useMonospaceFont]
       }&userName=#{User.current_user.card.name
-      }&noColors=#{epad_opts[:noColors]}"></iframe>
+      }&noColors=#{pad_opts[:noColors]}"></iframe>
     }
   end
 

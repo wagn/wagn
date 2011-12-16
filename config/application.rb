4608417e95e7a6389b27a1558c00117a05769673
Ethan McCutchen <ethan@grasscommons.org>
2011-12-16 11:52:13 -0700

    generalized log and database custom file locations

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,44 @@ if defined?(Bundler)
 end
 
 module Wagn
+  class Conf
+    class << self
+      def [](key)         @@hash[key.to_sym]            end
+      def []=(key, value) @@hash[key.to_sym]=value      end
+      
+      WAGN_CONFIG_DEFAULTS = { :role_tasks => %w[administrate_users create_accounts assign_user_roles] }    
+      WAGN_CONFIG_FILE = ENV['WAGN_CONFIG_FILE'] || "#{Rails.root}/config/wagn.yml"
+
+      def load
+        @@hash = h = WAGN_CONFIG_DEFAULTS
+
+        h.merge! YAML.load_file( WAGN_CONFIG_FILE ) if File.exists? WAGN_CONFIG_FILE
+        h.symbolize_keys!
+      end
+      
+      def load_after_app
+        h = @@hash
+        if base_u = h[:base_url]
+          h[:base_url] = base_u.gsub!(/\/$/,'')
+          h[:host] = base_u.gsub(/^https?:\/\//,'') unless h[:host]
+        end
+
+        h[:root_path] = begin
+          epath = ENV['RAILS_RELATIVE_URL_ROOT'] 
+          epath && epath != '/' ? epath : ''
+        end
+      
+        h[:upload_base_url] ||= h[:root_path] + '/files'
+        h[:upload_storage_dir] ||= "#{Rails.root}/local/uploads"
+
+        h[:pack_dirs] ||= "#{Rails.root}/lib/packs, #{Rails.root}/local/packs"
+      end
+    end
+  end
+  
+  
+  Wagn::Conf.load
+  
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -44,45 +82,18 @@ module Wagn
     
     config.cache_store = :file_store, "#{Rails.root}/tmp/cache"
     
-    #config.after_initialize do Wagn::Conf.wagn_init end
+    if log_file = Wagn::Conf[:log_file]
+      config.paths['log'] = File.join( log_file )
+    end
+    
+    if db_file = Wagn::Conf[:database_config_file]
+      config.paths['config/database'] = File.join( db_file )
+    end
 
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += Dir["#{config.root}/app/models/"]
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
   end
-
-  class Conf
-    class << self
-
-      def [](key)         @@hash[key.to_sym]            end
-      def []=(key, value) @@hash[key.to_sym]=value      end
-      
-      WAGN_CONFIG_DEFAULTS = { :role_tasks => %w[administrate_users create_accounts assign_user_roles] }    
-      WAGN_CONFIG_FILE = ENV['WAGN_CONFIG_FILE'] || "#{Rails.root}/config/wagn.yml"
-
-      def load
-        @@hash = h = WAGN_CONFIG_DEFAULTS
-
-        h.merge! YAML.load_file( WAGN_CONFIG_FILE ) if File.exists? WAGN_CONFIG_FILE
-        h.symbolize_keys!
-
-        if base_u = h[:base_url]
-          h[:base_url] = base_u.gsub!(/\/$/,'')
-          h[:host] = base_u.gsub(/^https?:\/\//,'') unless h[:host]
-        end
-
-        h[:root_path] = begin
-          epath = ENV['RAILS_RELATIVE_URL_ROOT'] 
-          epath && epath != '/' ? epath : ''
-        end
-      
-        h[:upload_base_url] ||= h[:root_path] + '/files'
-        h[:upload_storage_dir] ||= "#{Rails.root}/local/uploads"
-
-        h[:pack_dirs] ||= "#{Rails.root}/lib/packs, #{Rails.root}/local/packs"
-      end
-    end
-  end
+  
+  Wagn::Conf.load_after_app
 end
-
-Wagn::Conf.load

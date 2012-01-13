8de75447a35eb0d5ff098e931064d3c5c1f8cf35
Gerry Gleason <gerrg@inbox.com>
2012-01-13 11:12:20 -0600

    Added the rest of the 'task' codenames to the migration

--- db/migrate/20120112070200_codename_table.rb
+++ db/migrate/20120112070200_codename_table.rb
@@ -9,19 +9,25 @@ class CodenameTable < ActiveRecord::Migration
 
     codecards = %w{
         *account *accountable *account_link *add_help *alert *all *all_plu
-        *attach *autoname *bcc *captcha *cc *comment *community *content
-        *count *create *created *creator *css *default *delete *edit_help
-        *editing *editor *email *foot *from *head *home *includer *inclusion
-        *incoming *input *invite *last_edited *layout *link *linker *logo
-        *member *missing_link *navbox *now *option *option_label *outgoing
-        *plu_card *plu_part *pluss *read *recent *referred_to_by *refer_to
-        *related *request *right *role *rstar *search *self *send *sidebar
-        *signup *star *subject *table_of_content *tagged *thank *tiny_mce
-        *title *to *type *type_plu_right *update *version *watcher *watching
-        *when_created *when_last_edited anyone_signed_in anyone administrator
-        anonymous wagn_bot Basic Cardtype Date File Html Image AccountRequest
-        Number Phrase PlainText Pointer Role Search Set Setting Toggle User
+        *attach *autoname *bcc *captcha *cc *community *content *count
+        *created *creator *css *default *edit_help *editing *editor *email
+        *foot *from *head *home *includer *inclusion *incoming *input *invite
+        *last_edited *layout *link *linker *logo *member *missing_link
+        *navbox *now *option *option_label *outgoing *plu_card *plu_part
+        *pluss *recent *referred_to_by *refer_to *related *request *right
+        *role *rstar *search *self *send *sidebar *signup *star *subject
+        *table_of_content *tagged *thank *tiny_mce *title *to *type *watching
+        *type_plu_right *version *watcher *when_created *when_last_edited
+
+        anyone_signed_in anyone administrator anonymous wagn_bot
+
+        *administrate_users *assign_user_roles *comment *create
+        *create_accounts *delete *read *update
+
+        Basic Cardtype Date File Html Image AccountRequest Number Phrase
+        PlainText Pointer Role Search Set Setting Toggle User
     }
+
     renames = {
         "AccountRequest"   => "InvitationRequest",
         "wagn_bot"         => "wagbot",
@@ -31,7 +37,8 @@ class CodenameTable < ActiveRecord::Migration
       }
 
     codecards.map { |name|
-         c = Card[name] or raise "Missing codename #{name} card"
+         c = Card[name] || User.as(:wagbot) { r=Card.create!(:name=>name) } or
+             raise "Missing codename #{name} card"
          name = renames[name] if renames[name]
          Wagn::Codename.insert(c.id, name)
        }

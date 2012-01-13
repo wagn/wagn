843479fc2bb364ec0f6d5fe0bfd2e7aa0c724503
Gerry Gleason <gerrg@inbox.com>
2012-01-13 13:15:25 -0600

    Fixtures once again

--- db/migrate/20120112070200_codename_table.rb
+++ db/migrate/20120112070200_codename_table.rb
@@ -9,20 +9,18 @@ class CodenameTable < ActiveRecord::Migration
 
     codecards = %w{
         *account *accountable *account_link *add_help *alert *all *all_plu
-        *attach *autoname *bcc *captcha *cc *community *content *count
-        *created *creator *css *default *edit_help *editing *editor *email
-        *foot *from *head *home *includer *inclusion *incoming *input *invite
-        *last_edited *layout *link *linker *logo *member *missing_link
-        *navbox *now *option *option_label *outgoing *plu_card *plu_part
-        *pluss *recent *referred_to_by *refer_to *related *request *right
-        *role *rstar *search *self *send *sidebar *signup *star *subject
-        *table_of_content *tagged *thank *tiny_mce *title *to *type *watching
-        *type_plu_right *version *watcher *when_created *when_last_edited
+        *attach *autoname *bcc *captcha *cc *comment *community *content *count
+        *create *created *creator *css *default *delete *edit_help *editing
+        *editor *email *foot *from *head *home *includer *inclusion *incoming
+        *input *invite *last_edited *layout *link *linker *logo *member
+        *missing_link *navbox *now *option *option_label *outgoing *plu_card
+        *plu_part *pluss *read *recent *referred_to_by *refer_to *related
+        *request *right *role *rstar *search *self *send *sidebar *signup *star
+        *subject *table_of_content *tagged *thank *tiny_mce *title *to *type
+        *watching *type_plu_right *update *version *watcher *when_created
+        *when_last_edited
 
         anyone_signed_in anyone administrator anonymous wagn_bot
-
-        *administrate_users *assign_user_roles *comment *create
-        *create_accounts *delete *read *update
 
         Basic Cardtype Date File Html Image AccountRequest Number Phrase
         PlainText Pointer Role Search Set Setting Toggle User
@@ -42,10 +40,17 @@ class CodenameTable < ActiveRecord::Migration
          name = renames[name] if renames[name]
          Wagn::Codename.insert(c.id, name)
        }
+
+    Card.reset_column_information
   end
 
 
   def self.down
+    execute %{update cards as c set typecode = code.codename
+                from codename code
+                where c.type_id = code.card_id
+      }
+
     change_column "cards", "typecode", :string, :null=>false
 
     drop_table "codename"

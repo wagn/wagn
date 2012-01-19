25149833695de2ddd2189683b673d34dc0ea5d87
Gerry Gleason <gerrg@inbox.com>
2012-01-19 17:38:27 -0600

    ...

--- db/migrate/20120112070100_add_type_id.rb
+++ db/migrate/20120112070100_add_type_id.rb
@@ -2,12 +2,11 @@ class AddTypeId < ActiveRecord::Migration
   def up
     add_column :cards, :type_id, :integer
 
-    execute %{update cards as c set type_id = code.id
+    execute %{update cards set type_id = code.id
                 from cardtypes ct, cards code
                 where ct.id = code.extension_id
                   and code.extension_type='Cardtype'
-                  and c.typecode=ct.class_name
-    }
+                  and cards.typecode=ct.class_name }
   end
 
   def down

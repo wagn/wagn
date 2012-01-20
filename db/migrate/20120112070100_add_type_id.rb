4b29f0ce578eca86a8cafd200e33619077020ca8
Gerry Gleason <gerrg@inbox.com>
2012-01-19 22:24:09 -0600

    Two version of the sql for mysql and postgres

--- db/migrate/20120112070100_add_type_id.rb
+++ db/migrate/20120112070100_add_type_id.rb
@@ -1,15 +1,24 @@
 class AddTypeId < ActiveRecord::Migration
   def up
+    dbtype = ActiveRecord::Base.configurations[Rails.env]['adapter']
     add_column :cards, :type_id, :integer
     add_column :cardtypes, :card_id, :integer
 
-    execute %{update cardtypes ct, cards c
-                 set ct.card_id = c.id
-               where ct.id = c.extension_id and c.extension_type='Cardtype'}
+    if dbtype.to_s == 'mysql'
+      execute %{update cardtypes ct, cards c
+                   set ct.card_id = c.id
+                 where ct.id = c.extension_id and c.extension_type='Cardtype'}
 
-    execute %{update cards c, cardtypes ct
-                 set c.type_id = ct.card_id
-               where c.typecode = ct.class_name }
+      execute %{update cards c, cardtypes ct
+                   set c.type_id = ct.card_id
+                 where c.typecode = ct.class_name }
+    else
+      execute %{update cardtypes ct set card_id = c.id from cards c
+                 where ct.id = c.extension_id and c.extension_type='Cardtype'}
+
+      execute %{update cards c set type_id = ct.card_id from cardtypes ct
+                 where c.typecode = ct.class_name }
+    end
   end
 
   def down
--- db/migrate/20120116114524_user_card_id.rb
+++ db/migrate/20120116114524_user_card_id.rb
@@ -1,5 +1,7 @@
 class UserCardId < ActiveRecord::Migration
   def up
+    dbtype = ActiveRecord::Base.configurations[Rails.env]['adapter']
+
     add_column :users, :card_id, :integer
     rename_column :cards, :created_by, :creator_id
     rename_column :cards, :updated_by, :updater_id
@@ -9,25 +11,47 @@ class UserCardId < ActiveRecord::Migration
     add_column :cards, :created_by, :integer
     add_column :cards, :updated_by, :integer
     add_column :revisions, :created_by, :integer
+
     execute %{update revisions set created_by = creator_id}
     execute %{update cards set created_by=creator_id, updated_by = updater_id}
 
-    # populate the new column with card_id of the user extension
-    execute %{update users u, cards c set u.card_id = c.id
-               where c.extension_type = 'User' and c.extension_id = u.id }
+    if dbtype.to_s == 'mysql'
+      # populate the new column with card_id of the user extension
+      execute %{update users u, cards c set u.card_id = c.id
+                 where c.extension_type = 'User' and c.extension_id = u.id }
 
-    # Update the fields to use card_ids instead of extension_ids
-    # change the creator_id references from revisions table
-    execute %{update revisions r, cards c set r.creator_id = c.id 
-               where c.extension_id = r.creator_id
-                 and c.extension_type = 'User'}
-    # change the creator_id, and updater_id references cards table
-    execute %{update cards c, users u set c.creator_id = u.card_id
-               where u.id = c.creator_id }
+      # Update the fields to use card_ids instead of extension_ids
+      # change the creator_id references from revisions table
+      execute %{update revisions r, cards c set r.creator_id = c.id 
+                 where c.extension_id = r.creator_id
+                   and c.extension_type = 'User'}
+      # change the creator_id, and updater_id references cards table
+      execute %{update cards c, users u set c.creator_id = u.card_id
+                 where u.id = c.creator_id }
 
-    # change the updater_id cards from revisions table
-    execute %{update cards c, users u set c.updater_id = u.card_id
-               where u.id = c.updater_id }
+      # change the updater_id cards from revisions table
+      execute %{update cards c, users u set c.updater_id = u.card_id
+                 where u.id = c.updater_id }
+
+    else
+      # populate the new column with card_id of the user extension
+      execute %{update users as u set card_id = c.id from cards c
+                 where c.extension_type = 'User' and c.extension_id = u.id }
+
+      # Update the fields to use card_ids instead of extension_ids
+      # change the creator_id references from revisions table
+      execute %{update revisions as r set creator_id = c.id  from cards c
+                 where c.extension_id = r.creator_id
+                   and c.extension_type = 'User'}
+      # change the creator_id, and updater_id references cards table
+      execute %{update cards as c set creator_id = u.card_id from users u
+                 where u.id = c.creator_id }
+
+      # change the updater_id cards from revisions table
+      execute %{update cards as c set updater_id = u.card_id from users u
+                 where u.id = c.updater_id }
+
+    end
 
     change_column :users, :card_id, :integer, :null => false
   end

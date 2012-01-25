809134b072948b8ab95f0dd86704af288f146c13
Gerry Gleason <gerrg@inbox.com>
2012-01-25 09:39:12 -0600

    Added support for Mysql2 in quoting adapter

--- lib/active_record/quoting_and_matching.rb
+++ lib/active_record/quoting_and_matching.rb
@@ -41,6 +41,16 @@ module ActiveRecord
         "REGEXP #{string}"
       end
     end   
+
+    class Mysql2Adapter
+      def quote_interval(string)
+        "interval #{string}"
+      end
+      
+      def match(string)
+        "REGEXP #{string}"
+      end
+    end   
     
 <<<<<<< HEAD
     class SQLiteAdapter < AbstractAdapter

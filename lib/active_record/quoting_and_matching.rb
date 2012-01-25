98037c919b76f9aa128b169e3dd443130ffaec79
Gerry Gleason <gerrg@inbox.com>
2012-01-25 08:33:17 -0600

    Re-adding, but this probably means you need to un-install postgres for mysql to work on 1.9

--- lib/active_record/quoting_and_matching.rb
+++ lib/active_record/quoting_and_matching.rb
@@ -14,7 +14,11 @@ module ActiveRecord
       end
     end
 
+<<<<<<< HEAD
     class PostgreSQLAdapter < AbstractAdapter
+=======
+    class PostgreSQLAdapter
+>>>>>>> 81387a6... Re-adding, but this probably means you need to un-install postgres for mysql to work on 1.9
       def quote_interval(string)
         "interval '#{string}'"
       end
@@ -24,7 +28,11 @@ module ActiveRecord
       end
     end
     
+<<<<<<< HEAD
     class MysqlAdapter < AbstractAdapter
+=======
+    class MysqlAdapter
+>>>>>>> 81387a6... Re-adding, but this probably means you need to un-install postgres for mysql to work on 1.9
       def quote_interval(string)
         "interval #{string}"
       end
@@ -34,7 +42,11 @@ module ActiveRecord
       end
     end   
     
+<<<<<<< HEAD
     class SQLiteAdapter < AbstractAdapter
+=======
+    class SQLiteAdapter
+>>>>>>> 81387a6... Re-adding, but this probably means you need to un-install postgres for mysql to work on 1.9
       def quote_interval(string)
         "interval #{string}"
       end

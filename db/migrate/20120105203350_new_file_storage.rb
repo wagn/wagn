dcd9875356b9c5e36d21de2e6f3ca0bfb8b6935b
Ethan McCutchen <ethan@grasscommons.org>
2012-02-27 21:00:15 -0700

    tuned file migration to work with large data sets

--- db/migrate/20120105203350_new_file_storage.rb
+++ db/migrate/20120105203350_new_file_storage.rb
@@ -4,43 +4,50 @@ class NewFileStorage < ActiveRecord::Migration
   def up
     User.as :wagbot do
       %w{ File Image }.each do |typecode|
-        Card.search( :type => typecode ).each do |card|
-          card.revisions.each do |revision|
-            begin
-              filename = filename_for_revision(revision, typecode)
-              next unless filename
+        count = Card.count_by_wql(:type=>typecode)
+        limit = 100
+        migrated = 0
+        while count > migrated
+          Card.search( :type => typecode, :limit=>limit, :offset=>migrated, :sort=>'id' ).each do |card|
+            migrated += 1
+            card.revisions.each do |revision|
+              begin
+                filename = filename_for_revision(revision, typecode)
+                next unless filename
             
-              card.selected_rev_id = revision.id
-              filename = File.join( Rails.root, 'public', filename ) if filename =~ /^\/card/
+                card.selected_rev_id = revision.id
+                filename = File.join( Rails.root, 'public', filename ) if filename =~ /^\/card/
 
-              file = begin
-                  open filename, 'rb'
-                rescue
-                  open filename.sub( /\.png$/, '.gif' ), 'rb'
-                end
+                file = begin
+                    f = open filename, 'rb'
+                  rescue
+                    f.close if f && f.respond_to?( :close )
+                    open filename.sub( /\.png$/, '.gif' ), 'rb'
+                  end
               
-              data = file.read
-              card.attach = file
-              card.attach.instance_variable_set("@_attach_file_name", filename) # fixes ext in path
-              card.attach_file_name = "#{card.key.gsub('*','X').camelize}#{File.extname(filename)}" # fixes ext in content
+                data = file.read
+                card.attach = file
+                card.attach.instance_variable_set("@_attach_file_name", filename) # fixes ext in path
+                card.attach_file_name = "#{card.key.gsub('*','X').camelize}#{File.extname(filename)}" # fixes ext in content
           
-              revision.update_attribute :content, card.content
-              write_file data, card.attach.path(typecode=='Image' ? :original : '')
+                revision.update_attribute :content, card.content
+                write_file data, card.attach.path(typecode=='Image' ? :original : '')
           
-              if typecode == 'Image'
-                Card::STYLES.each do |style|
-                  next if style == 'original'
-                  f = open filename.sub( /\.\w+$/, "_#{style}\\0" ), 'rb'
-                  write_file f.read, card.attach.path( style )
-                  f.close
+                if typecode == 'Image'
+                  Card::STYLES.each do |style|
+                    next if style == 'original'
+                    f = open filename.sub( /\.\w+$/, "_#{style}\\0" ), 'rb'
+                    write_file f.read, card.attach.path( style )
+                    f.close
+                  end
                 end
-              end
             
-              file.close
-              
-            rescue Exception => e
-              Rails.logger.info "Migration exception: #{e.message}\n  #{e.backtrace*"\n  "}"
-              say "Error converting file for #{card.name}. #{e.message} continuing", :red
+              rescue Exception => e
+                Rails.logger.info "Migration exception: #{e.message}\n  #{e.backtrace*"\n  "}"
+                say "Error converting file for #{card.name}. #{e.message} continuing", :red
+              ensure
+                file.close if file && file.respond_to?( :close )
+              end
             end
           end
         end
@@ -54,8 +61,9 @@ class NewFileStorage < ActiveRecord::Migration
   def filename_for_revision( revision, typecode )
     content = revision.content
     return nil if content !~ /^\s*\</
+    return nil unless match = content.match( /(src|href)=\"([^\"]+)/ )
     
-    filename = content.match(/(src|href)=\"([^\"]+)/)[2]
+    filename = match[2]
     filename.sub!('_medium', '') if typecode == 'Image'
     filename
   end

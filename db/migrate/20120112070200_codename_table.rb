1d4ec2e7297b85d6040ba60bb9aa0b2c4ed2b938
Gerry Gleason <gerrg@inbox.com>
2012-03-30 10:11:04 -0500

    Initialize cache for migrations

--- db/migrate/20120112070200_codename_table.rb
+++ db/migrate/20120112070200_codename_table.rb
@@ -1,42 +1,10 @@
+require 'wagn/cache'
+
 class CodenameTable < ActiveRecord::Migration
-  
-  @@renames = {
-    "AccountRequest"   => "InvitationRequest",
-    "wagn_bot"         => "wagbot",    
-  }
-  
+
   def self.up
-    cardnames = %w{
-      
-      *accountable *add_help *autoname *captcha *comment *content
-      *create *default *delete *edit_help *input *layout *option *option_label
-      *read *send *thank *table_of_content *update
-      
-      *all *all_plus *right *rstar *self *star *type *type_plus_right
-      
-      *account_link  *head *logo *navbox *sidebar *version
-      
-      *css *tiny_mce
-      
-      *attach *bcc *cc *from *subject *to 
-       
-      *account *email *home *invite *now *recent *related *request *role
-      *search *signup *title *watcher *when_created *when_last_edited
+    Wagn::Cache.new_all
 
-      anyone_signed_in anyone administrator anonymous wagn_bot
-
-      Basic Cardtype Date File Html Image AccountRequest Number Phrase
-      PlainText Pointer Role Search Set Setting Toggle User
-    }
-    
-    # left out: *community, *creator, *editor, *editing, *count,   *includer *inclusion *incoming, *last_edited
-    #           *link  *linker *member *missing_link  *outgoing *plu_card *plu_part *pluss *referred_to_by *refer_to
-    #           *tagged *watching 
-    # delete: *alert, *foot, 
-    
-    
-    #omitted: *session and *user
-    
     create_table "card_codenames", :force => true, :id => false do |t|
       t.integer  "card_id", :null => false
       t.string   "codename", :null => false

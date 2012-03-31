213c76c36437bea95af6a223620529ad82479c67
Gerry Gleason <gerrg@inbox.com>
2012-03-30 19:20:25 -0500

    Regenerate fixtures with new test data

--- test/fixtures/shared_data.rb
+++ test/fixtures/shared_data.rb
@@ -167,7 +167,7 @@ class SharedData
     Card.create :name=>'TwwoHeading', :content => "<h1>One Heading</h1>\r\n<p>and some text</p>\r\n<h2>And a Subheading</h2>\r\n<p>and more text</p>"
     Card.create :name=>'ThreeHeading', :content =>"<h1>A Heading</h1>\r\n<p>and text</p>\r\n<h2>And Subhead</h2>\r\n<p>text</p>\r\n<h1>And another top Heading</h1>"
 
-    c=Card.fetch_or_new('Basic+*type').trait_card(:table_of_contents)
+    c=Card.fetch_or_new('Basic+*type+*table of contents')
     c.content='2'
     c.save
 

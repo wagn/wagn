9663f73b63bd36b7ef9ff691b1af740e3ce28f59
Ethan McCutchen <ethan@grasscommons.org>
2012-04-15 21:37:52 -0600

    several name -> codename changes

--- lib/packs/right/template_pack.rb
+++ lib/packs/right/template_pack.rb
@@ -1,6 +1,6 @@
 class Wagn::Renderer 
-  define_view  :core, :right=>'*content'  do |args|
+  define_view  :core, :right=>'content'  do |args|
     self._render_raw 
   end
-  alias_view :core, {:right=>'*content'}, {:right=>'*default'}
+  alias_view :core, {:right=>'content'}, {:right=>'default'}
 end

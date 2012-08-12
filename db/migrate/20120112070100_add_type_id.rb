e588963cfec38cb7a28a105faae36f282bb98d36
Gerry Gleason <gerrg@inbox.com>
2012-08-12 17:17:47 -0500

    split type_id migration into structure and data parts

--- db/migrate/20120112070100_add_type_id.rb
+++ db/migrate/20120112070100_add_type_id.rb
@@ -1,30 +1,7 @@
 class AddTypeId < ActiveRecord::Migration
   def up
-    dbtype = ActiveRecord::Base.configurations[Rails.env]['adapter']
-    begin
-      add_column :cards, :type_id, :integer
-      add_column :cardtypes, :card_id, :integer
-    rescue
-      puts "skipping column adds. presumption is that this has failed to rollback in mysql"
-    end
-
-    
-
-    if dbtype.to_s =~ /mysql/
-      execute %{update cardtypes ct, cards c
-                   set ct.card_id = c.id
-                 where ct.id = c.extension_id and c.extension_type='Cardtype'}
-
-      execute %{update cards c, cardtypes ct
-                   set c.type_id = ct.card_id
-                 where c.typecode = ct.class_name }
-    else
-      execute %{update cardtypes ct set card_id = c.id from cards c
-                 where ct.id = c.extension_id and c.extension_type='Cardtype'}
-
-      execute %{update cards c set type_id = ct.card_id from cardtypes ct
-                 where c.typecode = ct.class_name }
-    end
+    add_column :cards, :type_id, :integer
+    add_column :cardtypes, :card_id, :integer
   end
 
   def down

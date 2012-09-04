48337c90a949d63fa45dffbd931e69f375d4e9f1
Ethan McCutchen <ethan@grasscommons.org>
2012-09-04 10:22:09 -0600

    white space cleanup / comments

--- lib/wagn/set/type/cardtype.rb
+++ lib/wagn/set/type/cardtype.rb
@@ -2,20 +2,18 @@ module Wagn::Set::Type::Cardtype
   include Wagn::Set::Type::Basic
 
   def on_type_change
-    validate_destroy
+    custom_validate_destroy
   end
 
   def validate_type_change
-    validate_destroy
+    custom_validate_destroy
   end
 
   def cards_of_type_exist?
-    Card.find_by_type_id_and_trash( id, false )
+    Session.as_bot { Card.count_by_wql :type_id=>id } > 0 
   end
 
-  private
-
-  def validate_destroy
+  def custom_validate_destroy
     if cards_of_type_exist?
       errors.add :cardtype, "can't be altered because #{name} is a Cardtype and cards of this type still exist"
       false

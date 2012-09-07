90345bc6cf988c57251be60bfb0f1ddb94fccfda
Gerry Gleason <gerrg@inbox.com>
2012-09-07 18:16:05 -0500

    cleanup all_set (remove debugging)

--- lib/all_sets.rb
+++ lib/all_sets.rb
@@ -1,11 +1,6 @@
 module AllSets
   Dir.glob('lib/wagn/set/**/*.rb').each do |f|
-    if f =~ /lib\/(wagn\/set\/.*)\.rb$/
-      klass = $1.split('/').map{|s| s.camelize}*'::' #.inject(Module) {|k,m|
-      #Rails.logger.warn "alls: #{k.inspect}.const_get(#{m.inspect})"
-      #k.const_get(m) }
-      k=class_eval(klass)
-      Rails.logger.warn "all sets: #{klass.inspect}, #{k}"
-    end
+    f =~ /lib\/(wagn\/set\/.*)\.rb$/ and
+      class_eval $1.split('/').map{|s| s.camelize}*'::'
   end
 end

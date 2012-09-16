d7390dea45f6964699e458317496fbcc51b43548
Ethan McCutchen <ethan@grasscommons.org>
2012-09-16 15:50:59 -0600

    trimmed codename migration

--- db/migrate/20120327090000_codename_table.rb
+++ db/migrate/20120327090000_codename_table.rb
@@ -9,27 +9,45 @@ class CodenameTable < ActiveRecord::Migration
       "layout"            => "layout_type"
     }
   CODENAMES = %w{
-      *account *accountable *account_link *add_help *alert *all *all_plus
-      *attach *autoname *bcc *captcha *cc *comment *community *content *count
-      *create *created *creator *css *default *delete *edit_help *editing
-      *editor *email *foot *from *head *home *includer *inclusion *incoming
-      *input *invite *last_edited *layout *link *linker *logo *member
-      *missing_link *navbox *now *option *option_label *outgoing *plus_card
-      *plus_part *plus *read *recent *referred_to_by *refer_to *related
-      *request *right *roles *rstar *search *self *send *session *sidebar
-      *signup *star *subject *table_of_contents *tagged *thanks *tiny_mce
-      *title *to *type *watching *type_plus_right *update *users *version
-      *watchers *when_created *when_last_edited
-
-      *declare *declare_help *sol *pad_options etherpad
+        
+      basic cardtype date file html image account_request number phrase
+      plain_text pointer role search set setting toggle user layout
+    
+      *all *all_plus *star *rstar *type *type_plus_right *right *self
+      
+      *add_help *accountable *autoname *captcha *comment *content *create *default *delete
+      *edit_help *option *option_label *input *layout *read  *send *table_of_contents *thanks *update 
+      
+      *account *users *roles *email
+      
+      *home *title *tiny_mce
+      
+      *account_link *alert *version *foot *head *when_created *when_last_edited *navbox
+      
+      *attach *bcc *cc *from *subject *to 
+      
+      *css *now *recent *related
+      
+      *watchers *search
+      
+      *invite *signup *request 
 
       anyone_signed_in anyone administrator anonymous wagn_bot
 
-      *double_click *favicon
+      *double_click *favicon *logo
 
-      basic cardtype date file html image account_request number phrase
-      plain_text pointer role search set setting toggle user layout
-    } # FIXME: *declare, *sol ... need to be in packs
+    } 
+
+#not referred to in code:
+# *incoming *tagged *community *count *editing *editor *outgoing *plus_part *refer_to *member *sidebar
+# *created *creator *includer   *inclusion  *last_edited *missing_link *session *link *linker 
+# *referred_to_by *plus_card *plus *watching
+
+
+#   need to be in packs    
+#    *declare *declare_help *sol *pad_options etherpad
+    
+    # FIXME: *declare, *sol ... need to be in packs
 
   OPT_CODENAMES = %w{cardtype_a cardtype_b cardtype_c cardtype_d cardtype_e cardtype_f}
 
@@ -37,6 +55,46 @@ class CodenameTable < ActiveRecord::Migration
   @@have_codes = nil
 
   YML_CODE_FILE = 'db/bootstrap/cards.yml'
+  
+  
+  
+  def self.up
+    remove_index "cards", :name=>"card_type_index"
+    change_column "cards", "typecode", :string, :null=>true
+    change_column "cards", "type_id", :integer, :null=>false
+    add_index "cards", ["type_id"], :name=>"card_type_index"
+
+    Card.reset_column_information
+    Wagn::Cache.reset_global 
+
+    @@have_codes = !Wagn::Codename[:wagbot].nil?
+    warn Rails.logger.warn("have_codes #{@@have_codes}")
+    CodenameTable.load_bootcodes unless @@have_codes
+
+    Session.as_bot do
+      CodenameTable::CODENAMES.each { |name| CodenameTable.add_codename name }
+      warn Rails.logger.warn("migr opt test #{Card['cardtype_a']}")
+      if Card['cardtype_a']
+        CodenameTable::OPT_CODENAMES.each { |name| CodenameTable.add_codename name, true }
+      else warn Rails.logger.warn("migr skip optionals") end
+    end
+    Wagn::Codename.reset_cache
+  end
+
+
+  def self.down
+    execute %{update cards as c set typecode = code.codename
+                from codename code
+                where c.type_id = code.card_id
+      }
+
+    remove_index "cards", :name=>"card_type_index"
+    change_column "cards", "type_id", :integer, :null=>true
+    change_column "cards", "typecode", :string, :null=>false
+    add_index "cards", ["typecode"], :name=>"card_type_index"
+  end
+  
+  
   def self.load_bootcodes
     codehash = {}
     # seed the codehash so that we can bootstrap
@@ -47,7 +105,9 @@ class CodenameTable < ActiveRecord::Migration
         code, id = codename.to_sym, p[1]['id'].to_i
         codehash[code.to_sym] = id.to_i; codehash[id.to_i] = code.to_sym
       end
-    else warn Rails.logger.warn("no file? #{YML_CODE_FILE}") end
+    else
+      puts Rails.logger.warn("no file? #{YML_CODE_FILE}")
+    end
 
     #warn Rails.logger.warn("code cache: #{codehash.inspect}\n")
     Wagn::Codename.bootdata(codehash)
@@ -73,53 +133,23 @@ class CodenameTable < ActiveRecord::Migration
   def self.add_codename name, opt=false
     if check_codename(name)
       Rails.logger.warn("migr good code #{name}, #{c=Card[name] and c.id}")
-      return
-    end
-    if card = Card[name] || (!opt && Card.create!(:name=>name))
-      card or raise "Missing codename #{name} card"
+    else
+      card = Card[name]
+      
+      if !card
+        Wagn::Codename.reset_cache
+        return false if opt
+        puts Rails.logger.warn "adding card for codename #{name}"
+        card = Card.create! :name=>name
+        card or raise "Missing codename #{name} card"
+      end
 
       newname = CodenameTable.name2code(name)
       #Card.where(:id=>card.id).update_all(:codename=>nil) # should not be possible, right?
 
       Rails.logger.warn("migr codename for [#{card.id}] #{name}, #{newname}")
       Card.where(:id=>card.id).update_all(:codename=>newname)
-
-    elsif !opt; warn(Rails.logger.warn "missing card for #{name}")
     end
   end
 
-  def self.up
-    remove_index "cards", :name=>"card_type_index"
-    change_column "cards", "typecode", :string, :null=>true
-    change_column "cards", "type_id", :integer, :null=>false
-    add_index "cards", ["type_id"], :name=>"card_type_index"
-
-    Card.reset_column_information
-    Wagn::Cache.reset_global 
-
-    @@have_codes = !Wagn::Codename[:wagbot].nil?
-    warn Rails.logger.warn("have_codes #{@@have_codes}")
-    CodenameTable.load_bootcodes unless @@have_codes
-
-    Session.as_bot do
-      CodenameTable::CODENAMES.each { |name| CodenameTable.add_codename name }
-      warn Rails.logger.warn("migr opt test #{Card['cardtype_a']}")
-      if Card['cardtype_a']
-        CodenameTable::OPT_CODENAMES.each { |name| CodenameTable.add_codename name, true }
-      else warn Rails.logger.warn("migr skip optionals") end
-    end
-  end
-
-
-  def self.down
-    execute %{update cards as c set typecode = code.codename
-                from codename code
-                where c.type_id = code.card_id
-      }
-
-    remove_index "cards", :name=>"card_type_index"
-    change_column "cards", "type_id", :integer, :null=>true
-    change_column "cards", "typecode", :string, :null=>false
-    add_index "cards", ["typecode"], :name=>"card_type_index"
-  end
 end

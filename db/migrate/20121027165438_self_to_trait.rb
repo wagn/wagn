2533a60b5a95fa6c6275c51a3bee0cd9996363e2
Gerry Gleason <gerrg@inbox.com>
2012-10-29 08:25:02 -0500

    migration fixes

--- db/migrate/20121027165438_self_to_trait.rb
+++ db/migrate/20121027165438_self_to_trait.rb
@@ -1,17 +1,21 @@
 class SelfToTrait < ActiveRecord::Migration
   def up
-    Card.where(cards[:name].matches("%+*self+%")).each do |card|
+    Card.where(Card.arel_table[:name].matches("%+*self+%")).each do |card|
       card.name = card.name.sub(/\+\*self\+/,'+')
       prc = card.permission_rule_card :read
       card.save
     end
-    Card.where(cards[:name].matches("%+*self%")).each do |card|
+    Card.where(Card.arel_table[:name].matches("%+*self%")).each do |card|
       card.delete
     end
-    Card.find_all.each do |card|
-      card.read_rule_id = prc.first.id
-      card.read_rule_class = prc.last
-      card.save
+    Card.all.each do |card|
+      prc = card.permission_rule_card :read
+      if card.read_rule_id != prc.first.id or
+          card.read_rule_class != prc.last
+        card.read_rule_id = prc.first.id
+        card.read_rule_class = prc.last
+        card.save
+      end
     end
   end
 

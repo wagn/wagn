31d5e64ea7810fb03e718875edd8ebecd4acaf6c
Gerry Gleason <gerrg@inbox.com>
2012-11-28 08:01:56 -0600

    remove migrations that got added early

--- db/migrate/20121111024904_account_id.rb
+++
@@ -1,9 +0,0 @@
-class AccountId < ActiveRecord::Migration
-  def up
-    add_column :users, :account_id, :integer, :null=>false # add new column to refer to User+*account cards
-  end
-
-  def down
-    remove_column :users, :account_id
-  end
-end

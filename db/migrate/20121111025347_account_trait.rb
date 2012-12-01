71438d3ca67086e8ab336cd5eb10332b02d5b885
Gerry Gleason <gerrg@inbox.com>
2012-11-30 19:45:47 -0600

    fixing migration

--- db/migrate/20121111025347_account_trait.rb
+++ db/migrate/20121111025347_account_trait.rb
@@ -3,10 +3,16 @@ class AccountTrait < ActiveRecord::Migration
     Wagn::Conf[:migration]=true
 
     Account.as_bot do
+      if related_rule = Card[:account].fetch(:trait=>[:right, :content])
+        Rails.logger.warn "delete related rule #{related_rule.inspect}"
+        related_rule.confirm_destroy=true
+        related_rule.destroy!
+      end
+
       User.where(:status=>'active').each do |user|
         #next if user.card_id == Card::WagnBotID || user.card_id == Card::AnonID
-        card = Card.find user.card_id
-        if account = card.fetch(:trait=>:account, :new=>{})
+        if card = Card.where( :id=>user.card_id, :trash=>false ).first and
+             account = card.fetch(:trait=>:account, :new=>{})
           account.save!
           user.account_id = account.id
           user.save!

4a2a64fdd352e547d0f0c33fc26808ec89d1a399
Gerry Gleason <gerrg@inbox.com>
2012-12-12 16:09:57 -0600

    missed a ref_type in a spec

--- spec/models/card/reference_spec.rb
+++ spec/models/card/reference_spec.rb
@@ -107,7 +107,7 @@ describe "Card::Reference" do
     watermelon.name="grapefruit"
     watermelon.save!
     lew.reload.content.should == "likes [[watermelon]] and [[watermelon+seeds|seeds]]"
-    assert_equal [ LINK, LINK ], lew.out_references.map(&:ref_type), "links should be a LINK"
+    assert_equal [ LINK, LINK ], lew.out_references.map(&:link_type), "links should be a LINK"
     assert_equal [ 0, 0 ], lew.out_references.map(&:present), "links should not be present"
   end
 
@@ -190,9 +190,9 @@ describe "Card::Reference" do
     references = new_card.card_references(true)
     references.size.should == 2
     references[0].referee_key.should == 'WantedCard'
-    references[0].ref_type.should == Card::Reference::WANTED_PAGE
+    references[0].link_type.should == Card::Reference::WANTED_PAGE
     references[1].referee_key.should == 'WantedCard2'
-    references[1].ref_type.should == Card::Reference::WANTED_PAGE
+    references[1].link_type.should == Card::Reference::WANTED_PAGE
 
     wanted_card = Card.create(:name=>'WantedCard')
     wanted_card.revise('And here it is!', Time.now, Card['quentin'].to_user), get_renderer)
@@ -202,9 +202,9 @@ describe "Card::Reference" do
     references = new_card.card_references(true)
     references.size.should == 2
     references[0].referee_key.should == 'WantedCard'
-    references[0].ref_type.should == Card::Reference::LINKED_PAGE
+    references[0].link_type.should == Card::Reference::LINKED_PAGE
     references[1].referee_key.should == 'WantedCard2'
-    references[1].ref_type.should == Card::Reference::WANTED_PAGE
+    references[1].link_type.should == Card::Reference::WANTED_PAGE
   end
 =end
   private

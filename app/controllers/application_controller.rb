3728b902e836e21c80879b0ff98feeb48bb033e6
Gerry Gleason <gerrg@inbox.com>
2013-01-13 14:59:40 -0600

    Removing json/xml stuff (to another branch)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -118,14 +118,9 @@ class ApplicationController < ActionController::Base
     case
     when known                # renderers can handle it
       renderer = Wagn::Renderer.new @card, :format=>ext, :controller=>self
-      view ||= params[:view]
-      if ext == 'json'
-        render_object = renderer.render_show( :view => view )
-        render :json=>render_object, :status=>(renderer.error_status || status)
-      else
-        render :text=>renderer.render_show( :view => view ),
-          :status=>(renderer.error_status || status)
-      end
+      render_text = renderer.render_show :view => view || params[:view]
+      render :text=>render_text, :status=> renderer.error_status || status
+
     when show_file            # send_file can handle it
     else                      # dunno how to handle it
       render :text=>"unknown format: #{extension}", :status=>404

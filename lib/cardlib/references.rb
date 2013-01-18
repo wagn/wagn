d1b879223bf82d27f9c477a7235875d9257c5fee
Gerry Gleason <gerrg@inbox.com>
2013-01-18 17:37:14 -0600

    Double included removed

--- lib/cardlib/references.rb
+++ lib/cardlib/references.rb
@@ -139,22 +139,4 @@ Rails.logger.warn "rref? #{was_name} :#{inspect}"
     Card::Reference.update_on_destroy self
     expire_templatee_references
   end
-
-  def self.included(base)
-
-    super
-
-    base.class_eval do
-      # ---------- Reference associations -----------
-      has_many :references, :class_name => :Reference, :foreign_key => :referee_id
-      has_many :includes,   :class_name => :Reference, :foreign_key => :referee_id, :conditions => { :ref_type => 'I' }
-
-      has_many :out_references, :class_name => :Reference, :foreign_key => :referer_id
-      has_many :out_includes,   :class_name => :Reference, :foreign_key => :referer_id, :conditions => { :ref_type => 'I' }
-
-      after_create  :update_references_on_create
-      after_destroy :update_references_on_destroy
-      after_update  :update_references_on_update
-    end
-  end
 end

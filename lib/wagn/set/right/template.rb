331cf7767a7ad622af324358710cb86c6e8fd9d4
Ethan McCutchen <ethan@grasscommons.org>
2013-02-22 10:54:14 -0700

    restored/updated template functionality

--- lib/wagn/set/right/template.rb
+++ lib/wagn/set/right/template.rb
@@ -5,26 +5,26 @@ module Wagn
     format :base
 
     define_view  :core, :right=>'content' do |args|
-      self._render_raw
-      #with_inclusion_mode :template do
-      #  self._final_core args
-      #end
+      #self._render_raw
+      with_inclusion_mode :template do
+        self._final_core args
+      end
     end
     alias_view :core, {:right=>'content'}, {:right=>'default'}
 
     define_view :template_rule, :tags=>:unknown_ok do |args|
       tname = args[:include_name].gsub /\b_(left|right|whole|self|user|main|\d+|L*R?)\b/, ''
       if tname !~ /^\+/
-        "{{#{args[:unmask]}}}"
+        "{{#{args[:include]}}}"
       else
-          tmpl_set_name = parent.card.cardname.left_name
-          set_name =
+        tmpl_set_name = parent.card.cardname.left_name
+        set_name =
           if tmpl_set_class_name = tmpl_set_name.tag_name and Card[tmpl_set_class_name].codename == 'type'
             "#{tmpl_set_name.left_name}#{args[:include_name]}+#{Card[:type_plus_right].name}"
           else
             "#{tname.gsub /^\+/,''}+#{Card[:right].name}"
           end
-        "<strong>{{#{args[:unmask]}}} -- #{set_name}</strong>"
+        "<strong>{{#{args[:include]}}} -- #{set_name}</strong>"
       end
 
     end

5277c1ea69b3323191fc5a99a083d8fc5766ed39
Gerry Gleason <gerrg@inbox.com>
2013-05-02 17:27:24 -0500

    Move status to renerder (from card) to fix remaining merge issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -91,7 +91,7 @@ class ApplicationController < ActionController::Base
 
     renderer = Wagn::Renderer.new card, :controller=>self, :format=>format
     result = renderer.render_show opts
-    status = card.error_status || status
+    status = renderer.error_status || status
     
     if format==:file && status==200
       send_file *result

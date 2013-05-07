c079c3a70c99c98c9d5aa31b51dca711d7065734
Ethan McCutchen <ethan@grasscommons.org>
2013-05-07 09:41:35 -0600

    made #empty_trash get rid of old files

--- app/models/card.rb
+++ app/models/card.rb
@@ -83,6 +83,18 @@ class Card < ActiveRecord::Base
       User.delete_cardless
       Card::Revision.delete_cardless
       Card::Reference.repair_missing_referees
+      Card.delete_trashed_files
+    end
+    
+    def delete_trashed_files #deletes any file not associated with a real card.
+      dir = Wagn::Conf[:attachment_storage_dir]
+      card_ids = Card.connection.select_all( %{ select id from cards where trash is false } ).map( &:values ).flatten
+      file_ids = Dir.entries( dir )[2..-1].map( &:to_i )
+      file_ids.each do |file_id|
+        if !card_ids.member?(file_id) && !Card.exists?(file_id) #does a separate lookup to double check (overkill?)
+          FileUtils.rm_rf("#{dir}/#{file_id}", secure: true)
+        end
+      end
     end
     
     def merge name, attribs={}, opts={}

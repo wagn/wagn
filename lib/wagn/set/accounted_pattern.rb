ff7ec0f4084dc95ce3797515e6f66e30bf7dcf21
Gerry Gleason <gerrg@inbox.com>
2013-06-10 15:15:39 -0500

    ...

--- lib/wagn/set/accounted_pattern.rb
+++ lib/wagn/set/accounted_pattern.rb
@@ -7,11 +7,9 @@ module Wagn::SetPatterns
     register 'accounted', :index=>1, :method_key=>'accounted', :junction_only=>true
     def self.label name;              'Accounted "+" cards'        end
     def self.prototype_args anchor;   {:name=>'*dummy+*account'}   end
-    def self.pattern_applies?  card; 
-r1= !card.new_card?
-r = r1 && (c=card.fetch(:skip_modules=>true,:trait=>:account)) && !c.new_card?
-warn "Accounted appl? #{card.inspect}, cd:#{c.inspect}, r1:#{r1}, r:#{r.inspect}"; r
-end
+    def self.pattern_applies? card; 
+      !card.new_card? and cd = card.fetch(:skip_modules=>true,:trait=>:account) and cd.new_card?
+    end
   end
 
 end

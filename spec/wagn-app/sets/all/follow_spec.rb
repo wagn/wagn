11bdd28bf300421b4383c87314ec855247b3ac89
Ethan McCutchen <ethan@grasscommons.org>
2013-06-17 16:30:39 -0600

    moved old module specs to correct place

--- spec/wagn-app/sets/all/follow_spec.rb
+++ spec/wagn-app/sets/all/follow_spec.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
-require File.expand_path('../spec_helper', File.dirname(__FILE__))
-require File.expand_path('../../test/seed', File.dirname(__FILE__))
+require File.expand_path('../../../spec_helper', File.dirname(__FILE__))
+require File.expand_path('../../../../test/seed', File.dirname(__FILE__))
 FUTURE = SharedData::FUTURE
 
 

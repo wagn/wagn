82f2bb6b9d4102505a7b5c03ffd24d84d3dbb937
Gerry Gleason <gerrg@inbox.com>
2013-06-24 11:14:24 -0500

    First failing test for card_attributes, need event to save attribute cards

---
+++ spec/pack/core/sets/right_account_spec.rb
@@ -0,0 +1,24 @@
+# -*- encoding : utf-8 -*-
+require 'wagn/spec_helper'
+
+describe Card do
+  before do
+    #Account.as(Card::WagnBotID) # FIXME: as without a block is deprecated
+    @account_card = Card['sara'].fetch(:trait=>:account)
+  end
+
+  describe "Read and write card attribute" do
+    it "gets email attribute" do
+      @account_card.email.should == '' #FIXME: migrate data to cards and test for real value
+    end
+
+    it "sets and saves email attribute" do
+      @account_card.email= 'NewSara@user.com'
+      @account_card.email.should == 'NewSara@user.com'
+      Account.as_bot { @account_card.save }
+      Card.cache.reset
+      @account_card = Card['sara'].fetch(:trait=>:account)
+      @account_card.email.should == 'NewSara@user.com'
+    end
+  end
+end

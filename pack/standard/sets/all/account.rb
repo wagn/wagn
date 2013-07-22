9d2aa424381fd4a50ef11020bc9e8ac30f48489c
Ethan McCutchen <ethan@grasscommons.org>
2013-07-22 11:49:42 -0600

    fixed account request acceptance bug

--- pack/standard/sets/all/account.rb
+++ pack/standard/sets/all/account.rb
@@ -98,7 +98,8 @@ format :html do
         <div class="card-header">
           <h1>Invite</h1>
           #{ _render_help :text=>"Accept account request from: #{link_to_page card.name}" if card.known? }
-        </div>        
+        </div>
+        
         #{
 
           form_for :card, :action=>params[:action] do |f|
@@ -112,10 +113,7 @@ format :html do
                       #{ fieldset :email, text_field( :account, :email, :size=>60 ) }
                     }
                   else
-                    %{
-                      #{ hidden_field :card, :key }
-                      #{ hidden_field :account, :email }
-                    }
+                    hidden_field_tag 'card[key]', card.key
                   end
                 }
 

a83ae25b47f2d6f22515fcbd783a71f634685543
Ethan McCutchen <ethan@grasscommons.org>
2013-08-01 16:14:11 -0600

    added stub specs for standard all and standard type

---
+++ spec/pack/standard/sets/all/kml_spec.rb
@@ -0,0 +1,6 @@
+# -*- encoding : utf-8 -*-
+require 'wagn/pack_spec_helper'
+
+describe Card::Set::All::Kml do
+  # SPECSTUB (low priority)
+end
---
+++ spec/pack/standard/sets/type/search_type_spec.rb
@@ -0,0 +1,6 @@
+# -*- encoding : utf-8 -*-
+require 'wagn/pack_spec_helper'
+
+describe Card::Set::Type::SearchType do
+  # SPECSTUB (low priority)
+end

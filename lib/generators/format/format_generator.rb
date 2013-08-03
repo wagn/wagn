65c7d453fbca215fb09964d0eb2ec946c2bf3161
Ethan McCutchen <ethan@grasscommons.org>
2013-08-02 22:15:52 -0600

    used new format generator to generate spec stubs

--- lib/generators/format/format_generator.rb
+++ lib/generators/format/format_generator.rb
@@ -2,7 +2,7 @@ class FormatGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
   
   def create_files
-    template 'format_template.erb',           "pack/standard/sets/#{file_name}.rb"
-    template 'format_spec_template.erb', "spec/pack/standard/sets/#{file_name}_spec.rb"
+    template 'format_template.erb',           "pack/standard/formats/#{file_name}_format.rb"
+    template 'format_spec_template.erb', "spec/pack/standard/formats/#{file_name}_format_spec.rb"
   end
 end
---
+++ spec/pack/standard/formats/xml_format_spec.rb
@@ -0,0 +1,6 @@
+# -*- encoding : utf-8 -*-
+require 'wagn/spec_helper'
+
+describe Card::XmlFormat do
+  # format has no methods
+end

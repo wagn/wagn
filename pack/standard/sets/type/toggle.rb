8e286070441b1fa5b8c8c8c3c4c19d3ed17d845a
Ethan McCutchen <ethan@grasscommons.org>
2013-08-06 12:59:16 -0600

    minor whitespace fixes

--- pack/standard/sets/type/toggle.rb
+++ pack/standard/sets/type/toggle.rb
@@ -1,10 +1,10 @@
 # -*- encoding : utf-8 -*-
 view :core do |args|
   case card.raw_content.to_i
-    when 1; 'yes'
-    when 0; 'no'
-    else  ; '?'
-    end
+  when 1; 'yes'
+  when 0; 'no'
+  else  ; '?'
+  end
 end
 
 view :editor do |args|

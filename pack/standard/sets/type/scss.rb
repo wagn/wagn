8013ed5ee9b9a5037d23211a766ec30c583a7c08
Ethan McCutchen <ethan@grasscommons.org>
2013-08-24 11:58:48 -0600

    added scss content handling

--- pack/standard/sets/type/scss.rb
+++ pack/standard/sets/type/scss.rb
@@ -1,1 +1,24 @@
 # -*- encoding : utf-8 -*-
+
+format :html do
+
+  view :editor, :type=>:plain_text
+  
+  view :core do |args|
+    css = super args
+    ::CodeRay.scan( css, :css ).div
+  end
+  
+end
+
+
+format do
+  view :core do |args|
+    scss = process_content _render_raw
+    begin
+      Sass.compile scss, :style=>:expanded
+    rescue Exception=>e
+      e
+    end
+  end
+end

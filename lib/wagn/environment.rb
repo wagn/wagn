76a2e73b0a03a3afbbc892d10f30cb21ce000f4b
Ethan McCutchen <ethan@grasscommons.org>
2014-01-13 22:46:00 -0700

    new wagn environment handling

---
+++ lib/wagn/environment.rb
@@ -0,0 +1,7 @@
+# -*- encoding : utf-8 -*-
+# Load the rails application
+require 'wagn'
+require File.expand_path('../../config/application', __FILE__)
+
+# Initialize the rails application
+Wagn::Application.initialize!

e542c2a7f2be65e3285dd82bfd2cb24d41570297
Ethan McCutchen <ethan@grasscommons.org>
2014-02-21 14:54:31 -0700

    make closed content use max depth

--- mods/standard/sets/type/search_type.rb
+++ mods/standard/sets/type/search_type.rb
@@ -171,7 +171,7 @@ format :html do
 
 
   view :closed_content do |args|
-    if @depth > 2
+    if @depth > @@max_depth
       "..."
     else
       search_params[:limit] = 10 #not quite right, but prevents massive invisible lists.  

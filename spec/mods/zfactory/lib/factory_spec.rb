5cecf10f3bafd11163bfa44184e873ee193c3c1a
xithan <philipp.kuehl@gmail.com>
2014-05-06 18:31:30 +0200

    the first factory specs

---
+++ spec/mods/zfactory/lib/factory_spec.rb
@@ -0,0 +1,106 @@
+require 'byebug'
+require 'spec_helper'
+
+
+describe Factory do
+  include Wagn::Location
+  
+  before(:all) do
+    @depth = 4
+    @basics = []
+    Card::Auth.as_bot do
+      (2*@depth).times do |i|
+        @basics << Card.fetch( "basic level #{i}", :new => { :type => Card::BasicID } )
+        @basics.last.save
+      end
+    end
+  end
+  context 'new factory card' do
+    it 'creates input card' do
+      Card::Auth.as_bot do
+        #c = Card.create :name => 'a style test',  :type => Card::SkinID 
+        c = Card.fetch 'a style test',  :new => { :type => Card::SkinID }
+        c.type = Card::SkinID
+        c.save
+        c.factory_input_cards.should == Card.fetch( 'a style test+*input' )
+        
+      end
+    end
+    it 'creates output card' do
+      Card::Auth.as_bot do
+        #c = Card.create :name => 'a style test',  :type => Card::SkinID 
+        c = Card.fetch 'a style test',  :new => { :type => Card::SkinID }
+        c.type = Card::SkinID
+        c.save
+        c.factory_input_cards.should == Card.fetch( 'a style test+*input' )
+      end
+    end
+    
+  end
+  
+  describe "factory_input_cards" do
+    before do
+      Card::Auth.as_bot do
+        css = '#box { display: block }'
+        @css_card = Card.create :name=>'my css', :content=> css
+        @skin = Card.create :name=>'my skin', :type => Card::SkinID
+        @skin << @css_card
+      end
+    end
+     
+    context 'for skins' do
+      before do
+        Card::Auth.as_bot do
+          @first_level = last_level = Card.fetch( 'my nested skin', :new => {:type => Card::SkinID} )
+          @depth.times do |i|
+            last_level.content = ""
+            next_level = Card.fetch( "skin level #{i}", :new => {:type => Card::SkinID} )
+            last_level << @basics[i]
+            last_level << next_level
+            last_level << @basics[2*@depth-i-1]
+            last_level.save
+            last_level = next_level
+          end
+          last_level.save
+        end
+      end
+      it "scans all levels" do
+        @first_level.factory_input_cards.map(&:id).sort.should == @basics.map(&:id).sort
+      end
+      it "presevs order" do
+        @first_level.factory_input_cards.map(&:id).should == @basics.map(&:id)
+      end
+    end
+    
+    context 'for pointers' do
+      before do
+        Card::Auth.as_bot do
+          @first_level = last_level = Card.fetch 'my nested pointer', :new => {:type => Card::SkinID}
+          @depth.times do |i|
+            next_level = Card.fetch "pointer level #{i}", :new => {:type => Card::PointerID}
+            last_level.content = ""
+            last_level << @basics[i]
+            last_level << next_level
+            last_level << @basics[2*@depth-i-1]
+            last_level.save
+            last_level = next_level
+          end
+          last_level.save
+        end
+      end
+      
+      it "contains pointer card items" do
+        input = @skin.factory_input_cards
+        input.should == [@css_card]
+      end
+  
+      it "scans all levels" do
+          @first_level.factory_input_cards.map(&:id).sort.should == @basics.map(&:id).sort
+      end
+      
+      it "preserves order" do
+          @first_level.factory_input_cards.map(&:id).should == @basics.map(&:id)
+      end
+    end
+   end
+end

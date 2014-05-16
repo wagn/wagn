779746111d4f396b6bfbfc262d13be257af238c6
xithan <philipp.kuehl@gmail.com>
2014-05-17 00:39:03 +0200

    fixes

--- spec/mods/standard/sets/all/rich_html_spec.rb
+++ spec/mods/standard/sets/all/rich_html_spec.rb
@@ -14,10 +14,10 @@ describe Card::Set::All::RichHtml do
       @card.format.type_field.should match(/<option [^>]*selected/)
       tf=@card.format.type_field(:no_current_type=>true)
       tf.should_not match(/<option [^>]*selected/)
-      tf.scan(/<option /).length.should == 23
+      tf.scan(/<option /).length.should == 25
       tf=@card.format.type_field
       tf.should match(/<option [^>]*selected/)
-      tf.scan(/<option /).length.should == 23
+      tf.scan(/<option /).length.should == 25
     end
     it "should get type list" do
       Card::Auth.as :anonymous do

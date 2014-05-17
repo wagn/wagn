d0872e2d52b44859d0cff69ce2a412c1769915ac
xithan <philipp.kuehl@gmail.com>
2014-05-17 14:59:47 +0200

    removed comment

--- mods/standard/sets/right/style.rb
+++ mods/standard/sets/right/style.rb
@@ -3,14 +3,6 @@ include Machine
 
 store_machine_output :filetype => "css"
     
-# Is this issue still relevant with the new machine approach?    
-#FIXME - the following could be unified with type/file.rb considerably
-
-# note that this was formerly accomplished as a separate File card (eg *all+*style+file).  The issue was that the permanent
-# file regularly caused problems with non-root wagns, and requiring users to re-save the *all+*style rule upon updates
-# to CSS, SCSS, and Skin cards was not popular.
-
-
 format :file do
   view :core do |args|
     if params[:explicit_file] and r = controller.response

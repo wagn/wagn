4eea5d49e18987f9c96a66f1705754da85e97822
xithan <philipp.kuehl@gmail.com>
2014-05-27 02:35:29 +0200

    merged script migrations

--- db/migrate_cards/20140512155840_add_script_cards.rb
+++ db/migrate_cards/20140512155840_add_script_cards.rb
@@ -35,6 +35,21 @@ class AddScriptCards < ActiveRecord::Migration
       Card.create! :name=>"*machine output+#{default_rule_ending}", :type_id=>Card::FileID
       Card.create! :name=>'*machine input', :codename=>:machine_input
       Card.create! :name=>"*machine input+#{default_rule_ending}", :type_id=>Card::PointerID
+      
+      # create default script rule
+      card_type = { 'js' => :java_script, 'coffee' => :coffee_script}
+      scripts        = %w{ jquery tinymce slot     card_menu jquery_helper html5shiv_printshiv  }
+      types          = %w{ js     js      coffee   js        js            js                   }
+      # jquery.mobile  (in jquery_helper) must be after wagn to avoid mobileinit nastiness
+      cardnames = scripts.map { |name| "script: #{name.gsub( '_', ' ' )}" }
+      
+      scripts.each_with_index do |name, index|
+        Card.create! :name=>cardnames[index], :type=>card_type[types[index]], :codename=>"script_#{name}"
+      end
+      
+      cardnames.pop # html5shiv_printshiv not in default list, only used for IE9 (handled in head.rb)
+      Wagn::Cache.reset_global
+      Card.create! :name=>"#{Card[:all].name}+*script", :content=>cardnames.map { |name| "[[#{ name }]]" }.join("\n")
     end
   end
 end

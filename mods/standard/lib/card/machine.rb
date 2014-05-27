8a07aa4d6bf8838a8fe30858c1abaafb3dfc56fa
xithan <philipp.kuehl@gmail.com>
2014-05-27 02:32:26 +0200

    improved documentation

--- mods/standard/lib/card/machine.rb
+++ mods/standard/lib/card/machine.rb
@@ -1,9 +1,37 @@
+# What is this?
+# The Machine module together with the MachineInput module implements a kind of observer pattern.  
+# It handles a collection of input cards to generate a output card (default is a file card). 
+# If one of the input cards is changed the output card will be updated.
+# The classic example: 
+# A style card observes a collection of css and sccs card to generate a file card with a css file containg the assembled compressed css.
+
+# How to use it?
+# Include the Machine module in the card set that is supposed to produce the output card. If the output card should be autmatically updated when a input card is changed the input card has to be in a set that includes the MachineInput module.
+# The default machine
+#  -  uses its item cards as input cards or the card itself if there are no item cards;
+#     can be changed by passing a block to collect_input_cards 
+#  -  takes the raw view of the input cards to generate the output;
+#     can be changed by passing a block to machine_input (in the input card set)
+#  -  stores the output as a .txt file in the "+machine output" card;
+#     can be cahnged by passing a filetype and/or a block to store_machine_output
+
+
+# How does it work?
+# Machine cards have a +machine input and a +machine output card. The +machine input card is a pointer to all input cards.
+# Including the MachineInput module creates an ":on => save" event that runs the machines of all cards that are linked to that card via the +machine input pointer. 
+
+
+
 
 class Card
   module Machine    
     module ClassMethods
       attr_accessor :output_config 
   
+      def collect_input_cards &block
+        define_method :engine_input, &block
+      end
+      
       def prepare_machine_input &block
         define_method :before_engine, &block
       end
@@ -24,11 +52,32 @@ class Card
       host_class.extend( ClassMethods )
       host_class.output_config = { :filetype => "txt" }
             
-      if Codename[:machine_output]
+      #if Codename[:machine_output]
         host_class.card_accessor :machine_output, :type=>:file
         host_class.card_accessor :machine_input, :type => :pointer
   
-        # default engine
+        # define default machine behaviour
+        host_class.collect_input_cards do 
+          # traverse through all levels of pointers and
+          # collect all item cards as input 
+          items = [self]
+          new_input = []
+          already_extended = [] # avoid loops
+          while items.size > 0
+            item = items.shift
+            if item.trash or already_extended.include? item 
+              next
+            elsif item.item_cards == [item]  # no pointer card
+              new_input << item
+              already_extended << item
+            else
+              items.insert(0, item.item_cards)
+              items.flatten!
+              already_extended << item
+            end
+          end
+          items
+        end
         host_class.prepare_machine_input {}
         host_class.machine_engine { |input| input }
         host_class.store_machine_output do |output|
@@ -50,7 +99,7 @@ class Card
         host_class.event "update_machine_output_#{host_class.name.gsub(':','_')}".to_sym, :after => :store, :on => :save do  
           update_machine_output
         end
-      end
+     # end
     end
 
     def run_machine joint="\n"
@@ -70,29 +119,9 @@ class Card
       run_machine
     end
 
-
-    # traverse through all levels of pointers/skins/factories
-    # collects all item cards (for pointers/skins) 
     def update_input_card
-      items = [self]
-      new_input = []
-      already_extended = [] # avoid loops
-      while items.size > 0
-        item = items.shift
-        if item.trash or already_extended.include? item 
-          next
-        elsif item.item_cards == [item]  # No pointer card
-          new_input << item
-          already_extended << item
-        else
-          items.insert(0, item.item_cards)
-          items.flatten!
-          already_extended << item
-        end
-      end
-  
       Card::Auth.as_bot do
-        machine_input_card.items = new_input
+        machine_input_card.items = engine_input
       end
     end
 

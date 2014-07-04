6528f9017d3aa3b641ef83516334d2bc1629c436
xithan <philipp.kuehl@gmail.com>
2014-07-04 13:13:56 +0200

    there is still the cache problem

--- mods/standard/sets/right/machine_output.rb
+++ mods/standard/sets/right/machine_output.rb
@@ -1,10 +1,10 @@
 format do
   view :not_found do |args|
     if card.left.kind_of? Machine
-      card.left.refresh.update_machine_output   #FIXME problems with cache; without refresh this produces a loop 
+      #card.left.refresh.update_machine_output   #FIXME problems with cache; without refresh this produces a loop 
                                                 #      (it creates a new file but returns the url to a "newer" version that doesn't exist )
-      #Wagn::Cache.reset_global
-      #card.left.update_machine_output
+      Wagn::Cache.reset_global
+      card.left.update_machine_output
       self.error_status = 302
       wagn_path card.left.machine_output_url
     else

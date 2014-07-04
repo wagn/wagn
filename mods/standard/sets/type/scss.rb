310185d7f6229149bca18f3f29f843c513e9929e
xithan <philipp.kuehl@gmail.com>
2014-07-04 13:22:47 +0200

    scss includes css

--- mods/standard/sets/type/scss.rb
+++ mods/standard/sets/type/scss.rb
@@ -1,4 +1,13 @@
 include Card::Set::Type::Css
+include Machine
+include MachineInput
+
+store_machine_output :filetype => "css"
+
+machine_input do 
+  compress_css format(:format => :css)._render_core
+end
+
 
 format do
   view :core do |args|

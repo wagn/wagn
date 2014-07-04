da2dcafaa46f94e4d9cb9fea0c0d47e51ec01e01
xithan <philipp.kuehl@gmail.com>
2014-07-04 14:53:42 +0200

    observer cleanup

--- mods/standard/lib/card/observer.rb
+++ mods/standard/lib/card/observer.rb
@@ -1,36 +1,36 @@
 # -*- encoding : utf-8 -*-
 
-class Card
-  class Observer
-
-    class << self
-    
-      def send_timer_mails interval   
-        Card.search( :right => Card[interval].name ).map(&:item_cards).flatten.each do |card|
-          deliver( card )
-        end
-      end
-
-      def send_event_mails card, args  
-        #byebug if args[:on] == :delete      
-        setting = "on #{args[:on]}"    
-        email_templates_for( card, setting ) do |card|
-          deliver( card )
-        end
-      end
-      
-      def deliver mailcard
-        mailcard.format(:format=>:email)._render_mail.deliver
-      end
-      
-      def email_templates_for card, setting
-        if event_card = Card.fetch("#{card.name}+*self+*#{setting}")   #FIXME
-        #if event_card = card.rule_card(setting)
-          event_card.extended_item_cards.each do |mailcard|
-            yield(mailcard)
-          end
-        end
-      end
-    end
-  end
-end
+# class Card
+#   class Observer
+#
+#     class << self
+#
+#       def send_timer_mails interval
+#         Card.search( :right => Card[interval].name ).map(&:item_cards).flatten.each do |card|
+#           deliver( card )
+#         end
+#       end
+#
+#       def send_event_mails card, args
+#         #byebug if args[:on] == :delete
+#         setting = "on #{args[:on]}"
+#         email_templates_for( card, setting ) do |card|
+#           deliver( card )
+#         end
+#       end
+#
+#       def deliver mailcard
+#         mailcard.format(:format=>:email)._render_mail.deliver
+#       end
+#
+#       def email_templates_for card, setting
+#         if event_card = Card.fetch("#{card.name}+*self+*#{setting}")   #FIXME
+#         #if event_card = card.rule_card(setting)
+#           event_card.extended_item_cards.each do |mailcard|
+#             yield(mailcard)
+#           end
+#         end
+#       end
+#     end
+#   end
+# end
--- mods/standard/sets/all/observer.rb
+++ mods/standard/sets/all/observer.rb
@@ -7,21 +7,17 @@ class << self
   end
 end
 
-def deliver
-  format(:format=>:email)._render_mail.deliver
-end
+
 
 def send_event_mails args  
-  #byebug if args[:on] == :delete      
-  setting = "on #{args[:on]}"    
-  email_templates_for( setting ) do |card|
-    card.deliver
+  setting = "on_#{args[:on]}".to_sym    
+  email_templates_for( setting ) do |mailcard|
+    mailcard.deliver( context: self)
   end
 end
 
 def email_templates_for setting
-  if event_card = Card.fetch("#{self.name}+*self+*#{setting}")   #FIXME
-  #if event_card = card.rule_card(setting)
+  if event_card = self.rule_card(setting)
     event_card.extended_item_cards.each do |mailcard|
       yield(mailcard)
     end
@@ -30,7 +26,6 @@ end
 
 [:create, :update, :delete, :save].each do |action|
   event "observer_#{action}".to_sym, :after=>:approve, :on=>action do 
-    #Card::Set::All::Observer.send_event_mails self, :on=>action
     self.send_event_mails :on=>action
   end
 end

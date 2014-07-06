7a14d88006d9c178869a9b1ca8305ec37dc8217c
Ethan McCutchen <ethan@grasscommons.org>
2014-07-05 22:09:24 -0600

    fixed broken rss feeds

--- lib/card.rb
+++ lib/card.rb
@@ -5,7 +5,7 @@ class Card < ActiveRecord::Base
   require_dependency 'card/active_record_ext'
   require_dependency 'card/codename'
   require_dependency 'card/query'
-  require_dependency 'card/set_pattern'  
+  require_dependency 'card/set_pattern'
   require_dependency 'card/set'
   require_dependency 'card/format'
   require_dependency 'card/exceptions'
@@ -16,7 +16,7 @@ class Card < ActiveRecord::Base
   has_many :references_from, :class_name => :Reference, :foreign_key => :referee_id
   has_many :references_to,   :class_name => :Reference, :foreign_key => :referer_id
 
-  cache_attributes 'name', 'type_id' #Review - still worth it in Rails 3?
+  cache_attributes 'name', 'type_id' # review - still worth it in Rails 3?
 
   cattr_accessor :set_patterns, :error_codes
   @@set_patterns, @@error_codes = [], {}

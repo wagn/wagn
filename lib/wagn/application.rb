e8780df173b04ff6c01388bf814edaced1297553
xithan <philipp.kuehl@gmail.com>
2014-08-21 16:41:29 +0200

    weird errors disappeared

--- lib/wagn/application.rb
+++ lib/wagn/application.rb
@@ -4,8 +4,7 @@ require 'wagn/all'
 require 'active_support/core_ext/numeric/time'
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  #Bundler.require *Rails.groups(:assets => %w(development test))   
-  Bundler.require *Rails.groups(:assets => %w(development))   
+  Bundler.require *Rails.groups(:assets => %w(development test))   
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end

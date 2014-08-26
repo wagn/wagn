wagn/wagn

commit 2b69126abe0aee2838c334aaba3a6f297e2ddefb

made machine input/output use lazy loading

mod/standard/lib/card/machine.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 # What is this?
 # The Machine module together with the MachineInput module implements a kind of observer pattern.  
 # It handles a collection of input cards to generate a output card (default is a file card). 
@@ -100,8 +102,8 @@ class Card
           end
         end
   
-        host_class.event "update_machine_output_#{host_class.name.gsub(':','_')}".to_sym, :after => :store_subcards, :on => :save do  
-          update_machine_output
+        host_class.event "reset_machine_output_#{host_class.name.gsub(':','_')}".to_sym, :after => :store_subcards, :on => :save do  
+          reset_machine_output!
         end
       end
     end
@@ -119,10 +121,25 @@ class Card
       end.select(&:present?).join( joint )
       after_engine output
     end
+    
+    def reset_machine_output!
+      Auth.as_bot do
+        moc = machine_output_card and moc.real? and moc.delete!
+        mic = machine_input_card  and mic.real? and mic.delete!
+      end
+    end
+    
  
     def update_machine_output updated=[]
-      update_input_card
-      run_machine
+      cache_key = "UPDATE-LOCK:#{key}"
+      already_locked = Card.cache.read cache_key
+      unless already_locked
+        Card.cache.write cache_key, true
+        update_input_card
+        run_machine
+      end
+    ensure
+      Card.cache.write cache_key, false unless already_locked
     end
 
     def update_input_card

mod/standard/lib/card/machine_input.rb
@@ -21,7 +21,7 @@ class Card
       host_class.event "after_machine_input_updated_#{host_class.name.gsub(':','_')}".to_sym, :after=>:extend, :on => :save do
         machines = Card.search( {:right_plus => [{:codename => "machine_input"}, {:link_to => name}]}.merge(host_class.machines_wql) )  
         machines.each do |item|
-          item.update_machine_output if item.kind_of? Machine
+          item.reset_machine_output! if item.kind_of? Machine
         end
       end
     
@@ -31,7 +31,7 @@ class Card
     
       host_class.event "after_machine_input_deleted_#{host_class.name.gsub(':','_')}".to_sym, :after=>:store_subcards, :on => :delete do
         @involved_machines.each do |item|
-          item.update_machine_output if item.kind_of? Machine
+          item.reset_machine_output! if item.kind_of? Machine
         end
       end
     end

spec/controllers/card_controller_spec.rb
@@ -239,10 +239,7 @@ describe CardController do
     context 'css' do
       before do
         @all_style = Card[ "#{ Card[:all].name }+#{ Card[:style].name }" ]
-        @all_style.update_machine_output
-        Card::Auth.as_bot do
-          @all_style.fetch(:trait => :machine_output).delete!
-        end
+        @all_style.reset_machine_output!
       end
       
       it 'should create missing machine output file' do

spec/mod/standard/lib/machine_spec.rb
@@ -14,12 +14,16 @@ end
 
 
 shared_examples_for 'machine' do |filetype|
-  context "when created" do
+  context "machine is run" do
+    before do
+      machine.update_machine_output
+    end
+    
     it 'has +machine_input card' do
-      machine.machine_input_card.should_not be_nil
+      machine.machine_input_card.real?.should be_true 
     end
     it 'has +machine_output card' do
-      machine.machine_output_card.should_not be_nil
+      machine.machine_output_card.real?.should be_true
     end
     it "generates #{filetype} file" do 
       expect(machine.machine_output_path).to match(/\.#{filetype}$/)
@@ -32,8 +36,10 @@ shared_examples_for 'content machine' do |filetype|
     let(:machine) { machine_card }
   end
   
+  
   context '+machine_input card' do
     it "points to self" do
+      machine_card.update_input_card      
       expect(machine_card.input_item_cards).to eq([machine_card])
     end
   end
@@ -112,6 +118,10 @@ We build the following structure:
   end
   
   describe '+machine_input card' do
+    before do
+      subject.update_input_card
+    end
+    
     it "contains items of all levels" do
       subject.machine_input_card.item_cards.map(&:id).sort.should == @expected_items.map(&:id).sort
     end

commit 52eebf467f99b417b8d3576991e92a67d06aefc5

weird machine bug (introduced in https://github.com/wagn/wagn/commit/2b69126abe0aee2838c334aaba3a6f297e2ddefb)

mod/03_machines/lib/card/machine.rb
@@ -123,7 +123,8 @@ class Card
     def reset_machine_output!
       Auth.as_bot do
         moc = machine_output_card and moc.real? and moc.delete!
-        mic = machine_input_card  and mic.real? and mic.delete!
+        #mic = machine_input_card  and mic.real? and mic.delete!
+        update_input_card
       end
     end
     

mod/03_machines/lib/card/machine_input.rb
@@ -24,7 +24,7 @@ class Card
           item.reset_machine_output! if item.kind_of? Machine
         end
       end
-    
+      
       host_class.event "before_machine_input_deleted_#{host_class.name.gsub(':','_')}".to_sym, :after=>:approve, :on => :delete do
         @involved_machines = Card.search( {:right_plus => [{:codename => "machine_input"}, {:link_to => name}]}.merge(host_class.machines_wql) )  
       end

mod/03_machines/spec/set/type/skin_spec.rb
@@ -45,4 +45,26 @@ describe Card::Set::Type::Skin do
         expect(File.read path).to eq( compressed_css )
     end
   end
+  
+  context 'when item changed' do
+    it 'updates output of related machine card' do
+      skin = Card.gimme! "test skin supplier", :type => :skin, :content => ''
+      machine = Card.gimme! "style with skin machine+*style", :type => :pointer, :content=>'[[test skin supplier]]'
+      item = Card.gimme! "skin item", :type => :css, :content => css 
+      skin << item
+      skin.putty
+      Card::Auth.as_bot do
+        item.update_attributes :content=>changed_css
+        { :get => machine.machine_output_url }
+        item.update_attributes :content=>new_css
+      end
+      updated_machine  = Card.gimme machine.cardname
+      path = updated_machine.machine_output_path
+      expect(File.read path).to eq( compressed_new_css )
+      expect(updated_machine.machine_input_card.content).to include("[[skin item]]")
+      expect(Card.search(:link_to => "skin item")).to include(updated_machine.machine_input_card)
+    end
+  end
+  
+  
 end
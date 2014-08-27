133e4eb46d8211913a0445aa2afc3417a030119d
xithan <philipp.kuehl@gmail.com>
2014-08-27 15:37:39 +0200

    updated migration

--- db/migrate/20140822073704_create_new_revision_tables.rb
+++ db/migrate/20140822073704_create_new_revision_tables.rb
@@ -1,6 +1,11 @@
 # -*- encoding : utf-8 -*-
 class CreateNewRevisionTables < ActiveRecord::Migration
   def up
+    remove_column :cards, :db_content
+    drop_table :card_acts
+    drop_table :card_actions
+    drop_table :card_changes
+    
     add_column :cards, :db_content, :text
     
     create_table :card_acts do |t|
@@ -15,6 +20,7 @@ class CreateNewRevisionTables < ActiveRecord::Migration
       t.integer :card_act_id
       t.integer :super_action_id
       t.integer :action_type
+      t.boolean :draft
     end
     
     create_table :card_changes do |t|
@@ -22,6 +28,18 @@ class CreateNewRevisionTables < ActiveRecord::Migration
       t.integer :field
       t.text    :value 
     end
+    
+    Card::Revision.order(:created_at).each do |rev|
+      act = Card::Act.create(:card_id=>rev.card_id, :actor_id=>rev.creator_id, :acted_at=>rev.created_at)
+      action = Card::Action.create(:card_id=>rev.card_id, :card_act_id=>act.id, :action_type=>:create)
+      Card::Change.create(:card_action_id=>action.id, :field=>:db_content, :value=>rev.content )
+    end 
+    
+    Card.all.each do |card|
+      card.update_column(:db_content,card.current_revision.content)
+    end
+    #drop_table :card_revisions
+    #remove_column :cards, :current_revision
   end
 
   def down

beab49f1ab9848bbe675200415746a592121dedf
xithan <philipp.kuehl@gmail.com>
2014-08-27 15:39:18 +0200

    polished act.rb

--- lib/card/act.rb
+++ lib/card/act.rb
@@ -3,17 +3,7 @@ class Card
   class Act < ActiveRecord::Base
     before_save :set_actor
     has_many :actions, :order => :id, :foreign_key=>:card_act_id
-    
-    def create_with_ip(request)
-      self.ip_address = request.remote_ip
-      save
-    end
-    
-    def save_with_ip(request)
-      self.ip_address = request.remote_ip
-      save
-    end
-    
+        
     def set_actor
       self.actor_id = Auth.current_id
     end

4870cd1c66439fd8d688836b957e323b0d95ec8f
Ethan McCutchen <ethan@grasscommons.org>
2014-09-11 21:35:37 -0600

    Rails.root -> Wagn.root

--- lib/wagn/config/initializers/airbrake.rb
+++ lib/wagn/config/initializers/airbrake.rb
@@ -1,5 +1,5 @@
 # -*- encoding : utf-8 -*-
-filename = File.join Rails.root, 'airbrake.key'
+filename = File.join Wagn.root, 'config/airbrake.key'
 if File.exists? filename
   key = File.read( filename ).strip
   Airbrake.configure do |config|

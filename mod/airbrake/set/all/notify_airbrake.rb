9ee132c7ca8aa28a7a6cc662a3b638526c5af446
xithan <philipp.kuehl@gmail.com>
2014-12-04 17:54:07 +0100

    fixed bug introduced with last commit

--- mod/airbrake/set/all/notify_airbrake.rb
+++ mod/airbrake/set/all/notify_airbrake.rb
@@ -1,3 +1,3 @@
-event :notify_airbrake :after=>:notable_exception_raised
+event :notify_airbrake, :after=>:notable_exception_raised do
   controller.send :notify_airbrake, @exception if Airbrake.configuration.api_key
 end

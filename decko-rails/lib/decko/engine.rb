310cc4f31eb57a96a5cca3030b7d79f76e602985
Gerry Gleason <ggleason@tribune.com>
2014-12-15 08:49:39 -0600

    Engine changes after mv

--- decko-rails/lib/decko/engine.rb
+++ decko-rails/lib/decko/engine.rb
@@ -13,68 +13,77 @@ require 'diff/lcs'
 require 'diffy'
 #require 'scheduler_daemon'
 
-CARD_GEM_ROOT = File.expand_path('../../..', __FILE__)
+DECKO_GEM_ROOT = File.expand_path('../../..', __FILE__)
 
-class Card < ActiveRecord::Base
+module Decko
+
+  class Decko::Engine < Rails::Engine
+
+    def paths
+      @paths ||= begin
+        paths = super
+
+        Decko.add_gem_path paths, "app/controllers",     :eager_load => true
+
+        paths
+      end
+    end
+
+    def approot_is_gemroot?
+      Decko.gem_root.to_s == config.root.to_s
+    end
+
+  end
 
   class << self
     def root
       Rails.root
     end
-  
+
     def gem_root
-      CARD_GEM_ROOT
+      DECKO_GEM_ROOT
     end
 
-    def config
-      Rails.application.config
-    end
-
-    def paths
-      config.paths
-    end
-    
     def future_stamp
       ## used in test data
       @@future_stamp ||= Time.local 2020,1,1,0,0,0
     end
-  end
-    
-  class Engine < Rails::Engine
 
-    initializer :load_card_config_initializers,  :before => :load_config_initializers do
+    def application
+      Rails.application
+    end
+
+    def config
+      application.config
+    end
+
+    def card_paths_and_config paths
+      wpaths = Wagn::Application.paths
+
+      Wagn::Application.card_config Card.config
+      Card.config.database =  Rails.application.config.database_configuration[Rails.env]['database']
+      path = wpaths['tmp/set'] and paths['tmp/set'] = path
+      path = wpaths['tmp/set_pattern'] and paths['tmp/set_pattern'] = path
+      path = wpaths['local-mod'] and paths['local-mod'] = path
+
+      add_gem_path paths, 'gem-mod',             :with => 'mod'
+      add_gem_path paths, "db"
+      add_gem_path paths, "db/migrate"
+      add_gem_path paths, "db/migrate_core_cards"
+      add_gem_path paths, 'db/migrate_deck_cards', :with=>'db/migrate_cards'
+      add_gem_path paths, "db/seeds",            :with => "db/seeds.rb"
+
       add_gem_path paths, 'lib/card/config/initializers', :glob => "**/*.rb"
-      config.paths['lib/card/config/initializers'].existent.sort.each do |initializer|
+      paths['lib/card/config/initializers'].existent.sort.each do |initializer|
         load(initializer)
       end
-    end
 
-    def paths
-      @paths ||= begin
-        paths = super
-        add_gem_path paths, 'gem-mod',             :with => 'mod'
-
-        add_gem_path paths, "app/controllers",     :eager_load => true
-        add_gem_path paths, "db"
-        add_gem_path paths, "db/migrate"
-        add_gem_path paths, "db/migrate_core_cards"
-        add_gem_path paths, "db/seeds",            :with => "db/seeds.rb"
-        add_gem_path paths, 'gem-mod',             :with => 'mod'
-        add_gem_path paths, 'gem-assets',          :with => 'public/assets'
-
-        paths
-      end
-    end
-    
-    def approot_is_gemroot?
-      Card.gem_root.to_s == config.root.to_s
     end
 
     def add_gem_path paths, path, options={}
-      gem_path = File.join( Card.gem_root, path )
+      gem_path = File.join( paths.path, path )
       with = options.delete(:with)
-      with = with ? File.join(Card.gem_root, with) : gem_path
-      #warn "add gem path #{path}, #{with}, #{gem_path}, #{options.inspect}"
+      with = with ? File.join(paths.path, with) : gem_path
       paths[path] = Rails::Paths::Path.new(paths, gem_path, with, options)
     end
 

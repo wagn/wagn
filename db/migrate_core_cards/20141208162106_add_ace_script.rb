2f1150b35dfb6a6c8de5328d2cfc2c81b87009d5
chuenlok <clt58@cam.ac.uk>
2015-01-07 16:27:53 +0000

    remove file content type.

--- db/migrate_core_cards/20141208162106_add_ace_script.rb
+++ db/migrate_core_cards/20141208162106_add_ace_script.rb
@@ -5,13 +5,8 @@ class AddAceScript < Wagn::CoreMigration
     all_script = Card.fetch "*all+*script"
     all_script.add_item "script: ace"
     all_script.save!
-    js_path = "WagnGem:mod/03_machines/lib/javascript"
-    Card.create! :name=>"FileContent",:type=>"Cardtype",:codename=>"file_content"
-    Card.create! :name=>"FileContent+*type+*create",:type=>"Pointer",:content=>"[[Administrator]]"
-    Card.create! :name=>"FileContent+*type+*update",:type=>"Pointer",:content=>"[[Administrator]]"
-    Card.create! :name=>"FileContent+*type+*delete",:type=>"Pointer",:content=>"[[Administrator]]"
-      
-    Card.create! :name=>"script: ace",:type=>"FileContent",:content=>"#{js_path}/ace.js"
+    
+    Card.create! :name=>"script: ace",:codename=>"script_ace",:type=>"JavaScript"
 
   end
 end

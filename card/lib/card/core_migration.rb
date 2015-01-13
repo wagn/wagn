5a597345f4e16c40fae118a0ed0ab3030b227290
Gerry Gleason <ggleason@tribune.com>
2015-01-13 07:17:35 -0600

    Wrong name for action_mailer

--- card/lib/card/core_migration.rb
+++ card/lib/card/core_migration.rb
@@ -10,7 +10,7 @@ class Card::CoreMigration < Card::Migration
   end
   
   def import_json filename
-    Cardio.config.active_mailer.perform_deliveries = false
+    Cardio.config.action_mailer.perform_deliveries = false
     raw_json = File.read( data_path filename ) 
     json = JSON.parse raw_json
     Card.merge_list json["card"]["value"], :output_file=>File.join(data_path,"unmerged_#{ filename }")

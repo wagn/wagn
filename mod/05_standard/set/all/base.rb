2f4e8622049ed0c4b22c3798efce769f78df6dc4
xithan <philipp.kuehl@gmail.com>
2015-01-17 12:40:06 +0100

    reorganized rich_html.rb and html_format.rb

--- mod/05_standard/set/all/base.rb
+++ mod/05_standard/set/all/base.rb
@@ -81,39 +81,9 @@ format do
   end
 
 
-
-  # ERROR VIEWS
-
-
   view :blank,          :perms=>:none do |args| '' end
-  view :closed_missing, :perms=>:none do |args| '' end
-  view :missing,        :perms=>:none do |args| '' end
-
-  view :not_found, :perms=>:none, :error_code=>404 do |args|
-    %{ Could not find #{card.name.present? ? %{"#{card.name}"} : 'the card requested'}. }
-  end
-
-  view :server_error, :perms=>:none, :error_code=>500 do |args|
-    %{ Wagn Hitch!  Server Error. Yuck, sorry about that.\n}+
-    %{ To tell us more and follow the fix, add a support ticket at http://wagn.org/new/Support_Ticket }
-  end
-
-  view :denial, :perms=>:none, :error_code=>403 do |args|
-    focal? ? 'Permission Denied' : ''
-  end
-
-  view :bad_address, :perms=>:none, :error_code=>404 do |args|
-    %{ 404: Bad Address }
-  end
-
-  view :too_deep, :perms=>:none do |args|
-    %{ Man, you're too deep.  (Too many levels of inclusions at a time) }
-  end
-
-  view :too_slow, :perms=>:none do |args|
-    %{ Timed out! #{ showname } took too long to load. }
-  end
-
+    
+  
 
   #none of the below belongs here!!
 
---
+++ mod/05_standard/set/all/header.rb
@@ -0,0 +1,62 @@
+format :html do
+  
+  view :toggle do |args|
+    verb, adjective, direction = ( args[:toggle_mode] == :close ? %w{ open open e } : %w{ close closed s } )
+    
+    link_to '', path( :view=>adjective ), 
+      :remote => true,
+      :title => "#{verb} #{card.name}",
+      :class => "#{verb}-icon ui-icon ui-icon-circle-triangle-#{direction} toggler slotter nodblclick"
+  end
+    
+
+  view :header do |args|
+    %{
+      <h1 class="card-header">
+        #{ _optional_render :toggle, args, :hide }
+        #{ _optional_render :title, args }
+        #{ _optional_render :menu, args }
+      </h1>
+    }
+  end
+  
+  view :menu, :tags=>:unknown_ok do |args|
+    return _render_template_closer if args[:menu_hack] == :template_closer
+    disc_tagname = Card.fetch(:discussion, :skip_modules=>true).cardname
+    disc_card = unless card.new_card? or card.junction? && card.cardname.tag_name.key == disc_tagname.key
+      Card.fetch "#{card.name}+#{disc_tagname}", :skip_virtual=>true, :skip_modules=>true, :new=>{}
+    end
+
+    @menu_vars = {
+      :self         => card.name,
+      :linkname     => card.cardname.url_key,
+      :type         => card.type_name,
+      :structure    => card.structure && card.template.ok?(:update) && card.template.name,
+      :discuss      => disc_card && disc_card.ok?( disc_card.new_card? ? :comment : :read ),
+      :piecenames   => card.junction? && card.cardname.piece_names[0..-2].map { |n| { :item=>n.to_s } },
+      :related_sets => card.related_sets.map { |name,label| { :text=>label, :path_opts=>{ :current_set => name } } }
+    }
+    if card.real?
+      @menu_vars.merge!({
+        :edit      => card.ok?(:update),
+        :account   => card.account && card.ok?(:update),
+        :watch     => Auth.signed_in? && render_watch,
+        :creator   => card.creator.name,
+        :updater   => card.updater.name,
+        :delete    => card.ok?(:delete) && link_to( 'delete', path(:action=>:delete),
+          :class => 'slotter standard-delete', :remote => true, :'data-confirm' => "Are you sure you want to delete #{card.name}?"
+        )
+      })
+    end
+
+    json = html_escape_except_quotes JSON( @menu_vars )
+    %{<span class="card-menu-link" data-menu-vars='#{json}'>#{_render_menu_link}</span>}
+  end
+
+  view :menu_link do |args|
+    '<a class="ui-icon ui-icon-gear"></a>'
+  end
+  
+
+  
+end

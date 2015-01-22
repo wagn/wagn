0c8f66de2e869b25c7a8c67d24840da84b5dbf91
xithan <philipp.kuehl@gmail.com>
2015-01-22 13:05:34 +0100

    moved specs

--- mod/01_core/spec/format/html_format_spec.rb
+++ mod/01_core/spec/format/html_format_spec.rb
@@ -29,31 +29,6 @@ describe Card::HtmlFormat do
       end
     end
 
-    context "full wrapping" do
-      before do
-        @ocslot = Card['A'].format
-      end
-
-      it "should have the appropriate attributes on open" do
-        assert_view_select @ocslot.render(:open), 'div[class="card-slot open-view card-frame ALL TYPE-basic SELF-a"]' do
-          assert_select 'h1[class="card-header"]' do
-            assert_select 'span[class="card-title"]'
-          end
-          assert_select 'div[class~="card-body"]'
-        end
-      end
-
-      it "should have the appropriate attributes on closed" do
-        v = @ocslot.render(:closed)
-        assert_view_select v, 'div[class="card-slot closed-view card-frame ALL TYPE-basic SELF-a"]' do
-          assert_select 'h1[class="card-header"]' do
-            assert_select 'span[class="card-title"]'
-          end
-          assert_select 'div[class~="closed-content card-content"]'
-        end
-      end
-    end
-
     context "Cards with special views" do
 
     end

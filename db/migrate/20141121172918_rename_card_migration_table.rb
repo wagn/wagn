88bab2b73f240061025a3e9bc21a5f936a8ce1f7
xithan <philipp.kuehl@gmail.com>
2015-02-09 14:30:59 +0100

    fixed migrations

--- db/migrate/20141121172918_rename_card_migration_table.rb
+++ db/migrate/20141121172918_rename_card_migration_table.rb
@@ -1,7 +1,14 @@
 class RenameCardMigrationTable < ActiveRecord::Migration
   def self.up
-    rename_table :schema_migrations_cards, :schema_migrations_core_cards
+    if ActiveRecord::Base.connection.table_exists?  :schema_migrations_cards
+      rename_table :schema_migrations_cards, :schema_migrations_core_cards
+    else
+      create_table  :schema_migrations_core_cards do |t|
+        t.integer :version
+      end
+    end
   end 
+  
   def self.down
     rename_table :schema_migrations_core_cards, :schema_migrations_cards
   end

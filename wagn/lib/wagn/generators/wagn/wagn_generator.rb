91c9573bafaa34279eaa87533ac4107583f10e84
Gerry Gleason <ggleason@tribune.com>
2015-02-14 17:30:43 -0600

    Missing generated config/routes.rb (mounts the engine)

--- wagn/lib/wagn/generators/wagn/wagn_generator.rb
+++ wagn/lib/wagn/generators/wagn/wagn_generator.rb
@@ -5,10 +5,10 @@ class WagnGenerator < Rails::Generators::AppBase
 
 #class WagnGenerator < Rails::Generators::AppGenerator
 
-  
-
   source_root File.expand_path('../templates', __FILE__)
   
+  argument :mount_point, :required=>false
+
   class_option :database, :type => :string, :aliases => "-d", :default => "mysql",
     :desc => "Preconfigure for selected database (options: #{DATABASES.join('/')})"
     
@@ -16,7 +16,7 @@ class WagnGenerator < Rails::Generators::AppBase
     desc: "Prepare deck for wagn core testing"
     
   class_option 'gem-path', :type => :string, aliases: '-g', :default => false, :group => :runtime, 
-    desc: "Path to local gem installation"
+    desc: "Path to local gem installation (Use env WAGN_DEV_GEM_PATH)"
     
   class_option 'mod-dev', :type => :boolean, aliases: '-m', :default => false, :group => :runtime, 
     desc: "Prepare deck for mod testing"
@@ -28,10 +28,10 @@ class WagnGenerator < Rails::Generators::AppBase
   
 ## should probably eventually use rails-like AppBuilder approach, but this is a first step.  
   def dev_setup
-    @gem_path = options['gem-path']
+    @gem_path = ENV['WAGN_DEV_GEM_PATH'] || options['gem-path']
     if options['core-dev']
       # TODO: rename or split, gem_path points to the source repo, card and wagn gems are subdirs
-      @gem_path = options['gem-path'] || ask("Enter the path to your local wagn gem installation: ")
+      @gem_path = ask("Enter the path to your local wagn gem installation: ") unless @gem_path
       @spec_path = @gem_path
       @spec_helper_path = File.join @spec_path, 'card', 'spec', 'spec_helper'
       @features_path = File.join @gem_path, 'wagn/features/'  # ending slash is important in order to load support and step folders
@@ -41,7 +41,7 @@ class WagnGenerator < Rails::Generators::AppBase
       @spec_path = 'mod/'
       @spec_helper_path = './spec/spec_helper'
       @simplecov_config = "card_simplecov_filters"
-      @gem_path = options['gem-path'] || ask("Enter the path to your local wagn gem installation: ")
+      @gem_path = ask("Enter the path to your local wagn gem installation: ") unless @gem_path
       @spec_path = @gem_path
       @spec_helper_path = File.join @spec_path, 'spec', 'spec_helper'
       empty_directory 'spec'
@@ -73,7 +73,6 @@ class WagnGenerator < Rails::Generators::AppBase
     end
   end
 
-  
   def rakefile
     template "Rakefile"
   end
@@ -126,6 +125,7 @@ class WagnGenerator < Rails::Generators::AppBase
 
     inside "config" do
       template "application.rb"
+      template 'routes.erb', "routes.rb"
       template "environment.rb"
       template "boot.rb"
       template "databases/#{options[:database]}.yml", "database.yml"  

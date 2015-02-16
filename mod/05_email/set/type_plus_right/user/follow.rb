fa92bae0604a86810dcee7cf13d93ce9c4878c82
xithan <philipp.kuehl@gmail.com>
2015-02-16 01:05:08 +0100

    bootstrap css

---
+++ mod/05_email/set/type_plus_right/user/follow.rb
@@ -0,0 +1,163 @@
+
+include Card::Set::Type::Pointer
+
+def raw_content
+  @raw_content ||= if left
+      items = if left.type_id == Card::UserID
+         user = left
+         follow_rules = Card.user_rule_cards left.name, 'follow'
+         follow_rules.map {|card| "[[#{card.name}]]" }
+      end.join "\n"
+    else
+      ''
+    end
+end
+
+def ruled_user
+  if left.type_id == Card::UserID
+    left
+  elsif Auth.signed_in?
+    Auth.current
+  else
+    Card[:all]  # dangerous
+  end
+end
+
+def virtual?; true end
+
+
+
+format() { include Card::Set::Type::Pointer::Format   }
+
+ 
+format :html do
+   include Card::Set::Type::Pointer::HtmlFormat
+
+
+   view :closed_content do |args|
+     ''
+   end
+   
+   
+   view :open_content do |args|
+     %{
+      <div role="tabpanel">
+       <ul class="nav nav-tabs" role="tablist" id="myTab">
+         <li role="presentation" class="active"><a href="#following" aria-controls="following" role="tab" data-toggle="tab">Follow</a></li>
+         <li role="presentation"><a href="#ignoring" aria-controls="ignoring" role="tab" data-toggle="tab">Ignore</a></li>
+       </ul>
+
+       <div class="tab-content">
+         <div role="tabpanel" class="tab-pane active" id="following">#{render_following_list args}</div>
+         <div role="tabpanel" class="tab-pane" id="ignoring">#{render_ignoring_list}</div>
+       </div>
+      </div>
+     }
+   end
+ 
+ 
+   def followed_by_option
+     hash = Hash.new { |h,k| h[k] = [] }
+     card.item_cards.each do |follow_rule|
+       follow_rule.item_cards.each do |follow_option|
+         hash[follow_option.codename.to_sym] << follow_rule
+       end
+     end
+   end
+   
+   def each_suggestion
+      if (suggestions = Card["follow suggestions"])
+        suggestions.item_names.each do |sug|
+          if ((set_card = Card.fetch sug.to_name.left) && set_card.type_code == :set) 
+            option_card = Card.fetch(sug.to_name.right) || Card[sug.to_name.right.to_sym]
+            option = option_card.codename
+            yield(set_card, option)
+          elsif ((set_card = Card.fetch sug) && set_card.type_code == :set) 
+            yield(set_card, 'always')
+          end
+        end
+      end
+   end
+   
+   # returns hashes with existing and suggested follow options
+   # structure:
+   # set_pattern_class => [ {:card=>rule_card, :options=>['always', 'created_by_me'] },.... ] 
+   def followed_by_set
+     res = Hash.new { |h,k| h[k] = [] }
+     card.item_cards.each do |follow_rule|
+       res[follow_rule.rule_set.subclass_for_set] << {:card=>follow_rule, :options=>follow_rule.item_names}
+     end
+     
+     if Auth.signed_in? && Auth.current_id == card.left.id 
+       each_suggestion do |set_card, option|
+         suggested_rule_name = set_card.follow_rule_name(card.trunk)
+         if (rule = res[set_card.subclass_for_set].find { |rule| rule[:card].name == suggested_rule_name })
+           if !rule[:options].include? option
+             rule[:options] << option
+           end
+         else
+           rule_card =  Card.new(:name=>suggested_rule_name)
+           res[set_card.subclass_for_set] << {:card=>rule_card, :options=>[option]}
+         end          
+       end
+     end
+     res
+   end
+   
+   
+   view :following_list do |args|
+     if !Auth.signed_in? || Auth.current_id != card.left.id 
+       hide_buttons = [:delete_button, :add_button]
+     end
+     
+     sets = followed_by_set
+     wrap_with :div, :class=>'pointer-list-editor' do
+       wrap_with :ul, :class=>'delete-list' do 
+         
+         Card.set_patterns.select{|p| sets[p]}.reverse.map do |set_pattern|
+           sets[set_pattern].map do |rule|
+             rule[:options].map do |option|
+      
+                 content_tag :li do
+                   subformat(rule[:card]).render_follow_item :condition=>option, :hide=>hide_buttons
+                 end
+                
+             end.join("\n")
+           end.join("\n")
+         end.join("\n")
+         
+       end
+     end
+   end
+   
+   
+   view :ignoring_list do |args|
+     ignore_list = []
+     card.item_cards.each do |follow_rule|
+       follow_rule.item_cards.each do |follow_option|
+         if follow_option.codename.to_sym == :never
+           ignore_list << follow_rule
+         end
+       end
+     end
+     editable = Auth.signed_in? && Auth.current_id == card.left.id 
+     wrap_with :div, :class=>'pointer-list-editor' do
+       wrap_with :ul, :class=>'delete-list' do
+         
+         ignore_list.map do |rule_card|
+           content_tag :li do
+             subformat(rule_card).render_item_delete :condition=>'ignore', :editable=>editable
+           end
+         end.join "\n"
+         
+       end
+     end
+   end
+   
+   view :pointer_items, :tags=>:unknown_ok do |args|
+     super(args.merge(:item=>:link))
+   end
+   
+end
+
+format(:css ) { include Card::Set::Type::Pointer::CssFormat  }

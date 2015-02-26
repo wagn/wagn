cd514801fafdf12304a05ca97d3db254978217b0
xithan <philipp.kuehl@gmail.com>
2015-02-26 15:24:40 +0100

    input-group-addon instead of button for dragable pointer items

--- card/mod/06_bootstrap/set/type/pointer/bootstrap.rb
+++ card/mod/06_bootstrap/set/type/pointer/bootstrap.rb
@@ -3,11 +3,9 @@ format :html do
   view :list_item do |args|
     %{
       <li class="pointer-li input-group">
-        <span class="input-group-btn">
-          <button class="handle btn btn-default" type="button">
-            #{ glyphicon 'option-vertical left' }
-            #{ glyphicon 'option-vertical right'}
-          </button>
+        <span class="input-group-addon handle">
+          #{ glyphicon 'option-vertical left' }
+          #{ glyphicon 'option-vertical right'}
         </span>
         #{ text_field_tag 'pointer_item', args[:pointer_item], :class=>'pointer-item-text form-control' }
         <span class="input-group-btn">

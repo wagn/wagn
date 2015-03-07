7312227d4c94e6cf6ac5aff6e765e393eea61793
Gerry Gleason <ggleason@tribune.com>
2015-03-07 07:41:05 -0600

    Card generator namespace, test scripts, README to markdown with badge

---
+++ card/lib/generators/card/migration/migration_generator.rb
@@ -0,0 +1,38 @@
+# -*- encoding : utf-8 -*-
+
+require 'generators/card'
+
+class Card
+  module Generators
+    class MigrationGenerator < MigrationBase
+      source_root File.expand_path('../templates', __FILE__)
+  
+      class_option 'core', :type => :boolean, aliases: '-c', :default => false, :group => :runtime, 
+        desc: "Create card migration for card core"
+
+      def create_migration_file
+        migration_type = options['core'] ? :core_cards : :deck_cards
+        mig_paths = Cardio.migration_paths(migration_type)
+        raise "No migration directory for #{migration_type}" if mig_paths.blank?
+        set_local_assigns!
+        migration_template @migration_template, File.join( mig_paths.first, "#{file_name}.rb")
+      end
+  
+    protected
+  
+      # sets the default migration template that is being used for the generation of the migration
+      # depending on the arguments which would be sent out in the command line, the migration template 
+      # and the table name instance variables are setup.
+
+      def set_local_assigns!
+        @migration_template = "card_migration.erb"
+        @migration_parent_class = options['core'] ? 'Card::CoreMigration' : 'Card::Migration'
+        case file_name
+        when /^(import)_(.*)(?:\.json)?/
+          @migration_action = $1
+          @json_filename    = "#{$2}.json"
+        end
+      end
+    end
+  end
+end

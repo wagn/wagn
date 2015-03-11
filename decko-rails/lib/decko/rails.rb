0c061f73f1e3926cddbed7151f97611be2e10872
Gerry Gleason <ggleason@tribune.com>
2015-03-11 18:01:30 -0500

    Compress generator branch to one commit

--- decko-rails/lib/decko/rails.rb
+++ decko-rails/lib/decko/rails.rb
@@ -1,11 +1,33 @@
 DECKO_RAILS_GEM_ROOT = File.expand_path('../../..', __FILE__)
 
+require 'rails/all'
+require 'decko/engine'
+
 module Decko
-  module Rails
+  module Rails # not sure we need this
     class << self
       def gem_root
         DECKO_RAILS_GEM_ROOT
       end
     end
   end
+
+  if defined? ::Rails::Railtie
+    class Railtie < ::Rails::Railtie
+
+      initializer 'decko-rails.load_task_path', :before => 'decko.engine.load_config_initializers' do
+        puts "dk rails #{::Rails.application.paths.inspect} #{defined? Decko::Engine}"
+        Cardio.set_config ::Rails.application.config
+        Cardio.set_paths ::Rails.application.paths
+      end
+
+      rake_tasks do |app|
+        begin
+          # for some reason this needs the 'wagn/', can't get lib/tasks change right by this time?
+          load 'wagn/tasks/wagn.rake'
+        end
+      end
+    end
+  end
+
 end

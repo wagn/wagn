d96c90eeda34263abfed14e71c37095651241373
xithan <philipp.kuehl@gmail.com>
2015-03-18 20:41:13 +0100

    more themes

--- card/db/migrate_core_cards/20150317162412_bootstrap_themes.rb
+++ card/db/migrate_core_cards/20150317162412_bootstrap_themes.rb
@@ -3,7 +3,7 @@
 class BootstrapThemes < Card::CoreMigration
   def up
     Card.create! :name=>'raw bootstrap skin', :type_code=>:skin, :content=> "[[style: bootstrap]]\n[[style: jquery-ui-smoothness]]\n[[style: functional]]\n[[style: standard]]\n[[style: right sidebar]]\n[[style: bootstrap common]]\n[[style: bootstrap cards]]"
-    %w{amelia simpex cerulean lumen darkly readable holo superhero yeti cosmo cyborg spacelab google_plus facebook}.each do |theme|
+    %w{amelia simpex cerulean lumen darkly flatly journal paper readable sandstone slate holo superhero yeti cosmo cyborg spacelab united}.each do |theme|
       Card.create! :name=>"theme: #{theme}", :type_code=>:css, :codename=>"theme_#{theme}"
       Card.create! :name=>"#{theme} skin", :type_code=>:skin, :content=>"[[raw bootstrap skin]]\n[[theme: #{theme}]]"
     end

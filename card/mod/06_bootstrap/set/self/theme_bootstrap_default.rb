df88b3af323175b35d820c50001157b43560ce17
Ethan McCutchen <ethan@grasscommons.org>
2015-03-24 14:30:48 -0600

    got newly organized theming working

--- card/mod/06_bootstrap/set/self/theme_bootstrap_default.rb
+++ card/mod/06_bootstrap/set/self/theme_bootstrap_default.rb
@@ -1,9 +1,2 @@
-view :raw do |args|
-  File.read "#{Cardio.gem_root}/mod/06_bootstrap/lib/stylesheets/bootstrap_theme.css"
-end
-
-format :html do 
-  view :editor do |args|
-    "Content is stored in file and can't be edited."
-  end
-end
+format { include BootstrapCompatible::Format }
+format(:html) { include BootstrapCompatible::HtmlFormat }

d47e95265c813512fcd40e7b794551781d14834c
xithan <philipp.kuehl@gmail.com>
2015-04-10 18:13:32 +0200

    fix phantomjs error

--- card/mod/03_machines/set/self/script_card_menu.rb
+++ card/mod/03_machines/set/self/script_card_menu.rb
@@ -1,3 +1,11 @@
+view :raw do |args|
 
-format { include ScriptAce::Format }
-format(:html) { include ScriptAce::HtmlFormat }
+  Rails.logger.info "reading file: #{Cardio.gem_root}/mod/03_machines/lib/javascript/#{card.codename}.js.coffee"
+  File.read "#{Cardio.gem_root}/mod/03_machines/lib/javascript/#{card.codename}.js.coffee"
+end
+
+format :html do
+  view :editor do |args|
+    "Content is stored in file and can't be edited."
+  end
+end

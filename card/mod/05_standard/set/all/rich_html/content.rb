ed9a95fface2e39c32aa8f0c9cc0b2f51714338b
Ethan McCutchen <ethan@grasscommons.org>
2015-05-12 23:14:19 -0600

    add menu back into titled view

--- card/mod/05_standard/set/all/rich_html/content.rb
+++ card/mod/05_standard/set/all/rich_html/content.rb
@@ -40,6 +40,7 @@ format :html do
   view :titled, :tags=>:comment do |args|
     wrap args do
       [
+        _optional_render( :menu, args ),
         _render_header( args ),
         wrap_body( :content=>true ) { _render_core args },
         optional_render( :comment_box, args )

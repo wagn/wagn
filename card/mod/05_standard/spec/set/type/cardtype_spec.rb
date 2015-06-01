e1d5ed560c6eb978562ff69a379c49689add03c2
xithan <philipp.kuehl@gmail.com>
2015-06-01 12:38:01 +0200

    removed failing spec that belongs to #422

--- card/mod/05_standard/spec/set/type/cardtype_spec.rb
+++ card/mod/05_standard/spec/set/type/cardtype_spec.rb
@@ -1,12 +1,4 @@
 # -*- encoding : utf-8 -*-
 
 describe Card::Set::Type::Cardtype do
-  describe 'add_button view' do
-
-    it 'has the right path' do
-      button = render_content "{{Basic|add_button}}"
-      debug_assert_view_select button, 'a[href=/new/Basic]'
-    end
-
-  end
 end

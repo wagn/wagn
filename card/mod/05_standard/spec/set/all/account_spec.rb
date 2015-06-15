b7b1ce8d8bb60396e601395a2a9636646f92b643
xithan <philipp.kuehl@gmail.com>
2015-06-16 00:15:39 +0200

    adjust read rule count in account_spec

--- card/mod/05_standard/spec/set/all/account_spec.rb
+++ card/mod/05_standard/spec/set/all/account_spec.rb
@@ -128,7 +128,7 @@ describe Card::Set::All::Account do
       # includes lots of account rules...
       Card::Auth.as(:joe_admin) do
         ids = Card::Auth.as_card.read_rules
-        expect(ids.length).to eq(@read_rules.size + 12)
+        expect(ids.length).to eq(@read_rules.size + 15)
       end
     end
 

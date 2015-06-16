dc57bbe86801e7bdda24cf38fbe3144beeb4dbd3
xithan <philipp.kuehl@gmail.com>
2015-06-17 01:31:09 +0200

    use pointer interface for session cards

--- card/mod/04_settings/set/self/input.rb
+++ card/mod/04_settings/set/self/input.rb
@@ -1,2 +1,2 @@
 extend Card::Setting
-setting_opts :group=>:pointer, :position=>3, :restricted_to_type => :pointer, :rule_type_editable => false
+setting_opts :group=>:pointer, :position=>3, :restricted_to_type => [:pointer,:session], :rule_type_editable => false

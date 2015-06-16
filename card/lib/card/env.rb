3f758b537415b1aa71c03896a3e236eee97eaa1c
xithan <philipp.kuehl@gmail.com>
2015-06-16 19:19:38 +0200

    fix controller params handling

--- card/lib/card/env.rb
+++ card/lib/card/env.rb
@@ -5,40 +5,40 @@ class Card
     class << self
       def reset args={}
         @@env = { :main_name => nil }
-        
+
         if c = args[:controller]
           self[:controller] = c
           self[:session]    = c.request.session
-          self[:params]     = c.request.params
+          self[:params]     = c.params
           self[:ip]         = c.request.remote_ip
           self[:ajax]       = c.request.xhr? || c.request.params[:simulate_xhr]
           self[:host]       = Card.config.override_host     || c.request.env['HTTP_HOST']
           self[:protocol]   = Card.config.override_protocol || c.request.protocol
-        
+
         end
       end
-    
+
       def [] key
         @@env[key.to_sym]
       end
-    
+
       def []= key, value
         @@env[key.to_sym] = value
       end
-      
+
 
       def params
         self[:params] ||= {}
       end
-      
+
       def session
         self[:session] ||= {}
       end
-    
+
       def ajax?
         self[:ajax]
       end
-    
+
       def method_missing method_id, *args
         case args.length
         when 0 ; self[ method_id ]
@@ -47,6 +47,6 @@ class Card
         end
       end
     end
-  end  
+  end
   Env.reset
 end

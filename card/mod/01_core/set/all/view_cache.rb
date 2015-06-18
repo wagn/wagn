f0891cc9342db4d7b076bfa427344774df477081
Ethan McCutchen <ethan@grasscommons.org>
2015-06-18 14:15:36 -0600

    STILL trying to turn off ViewCache!

--- card/mod/01_core/set/all/view_cache.rb
+++ card/mod/01_core/set/all/view_cache.rb
@@ -1,6 +1,6 @@
-event :clear_view_cache, :after=>:store do
-  Card::ViewCache.reset
-end
+#event :clear_view_cache, :after=>:store do
+#  Card::ViewCache.reset
+#end
 
 format do
   def view_caching?

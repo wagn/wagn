de516674cedb488ecb4fa2e2b055992c907d860d
chuenlok <clt58@cam.ac.uk>
2015-06-28 13:34:56 +0100

    remove wrong syntax

--- card/mod/02_basic_types/set/type/pointer.rb
+++ card/mod/02_basic_types/set/type/pointer.rb
@@ -216,7 +216,7 @@ end
 # this event is only on pointer card. Other type cards do not have this event,
 # so it is not overridden and will be run while updating type and content in the same request. 
 event :standardize_items, :before=>:approve, :on=>:save, :when=>proc{  |c| c.type_id == Card::PointerID  } do
-  if db_content_changed? && 
+  if db_content_changed?
     self.content = item_names(:context=>:raw).map { |name| "[[#{name}]]" }.join "\n"
   end
 end

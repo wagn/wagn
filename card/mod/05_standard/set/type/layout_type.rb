eb5bf9925a221096f9a94ae98b666c83c2634a83
chuenlok <clt58@cam.ac.uk>
2015-07-30 19:06:57 +0100

    import format instead of repeat the code

--- card/mod/05_standard/set/type/layout_type.rb
+++ card/mod/05_standard/set/type/layout_type.rb
@@ -3,12 +3,10 @@ include Html
 
 format do
   include Html::Format
-  view :editor do |args|
-    text_area :content, :rows=>5, :class=>'card-content ace-editor-textarea', "data-card-type-code"=>card.type_code
-  end
 end
 
 format :html do
+  include Html::HtmlFormat
   view :core do |args|
     with_inclusion_mode :template do
       process_content ::CodeRay.scan( _render_raw, :html ).div

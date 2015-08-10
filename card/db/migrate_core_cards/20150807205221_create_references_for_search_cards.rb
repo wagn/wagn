92ab42acf6acc0466d973cffbc441dc879006386
Ethan McCutchen <ethan@grasscommons.org>
2015-08-10 11:29:05 -0600

    temporarily broke search migration;

--- card/db/migrate_core_cards/20150807205221_create_references_for_search_cards.rb
+++ card/db/migrate_core_cards/20150807205221_create_references_for_search_cards.rb
@@ -2,6 +2,7 @@
 
 class CreateReferencesForSearchCards < Card::CoreMigration
   def up
+    raise "not ready to run yet"
     Card.search(:type=>'Search').each do |card|
       card.update_references
     end

220c1f947a44a5443fde2f7e30f78ba6bf6cb98c
Ethan McCutchen <ethan@grasscommons.org>
2015-08-22 22:27:23 -0600

    got rid of direct params[:item] handling in pointer core views

--- card/mod/02_basic_types/set/type/pointer.rb
+++ card/mod/02_basic_types/set/type/pointer.rb
@@ -185,7 +185,7 @@ format :css do
 
   view :core do |args|
     card.item_cards.map do |item|
-      nest item, :view=>(params[:item] || args[:item] || :content)
+      nest item, :view=>(args[:item] || :content)
     end.join "\n\n"
   end
 
@@ -197,7 +197,7 @@ end
 format :js do
   view :core do |args|
     card.item_cards.map do |item|
-      nest item, :view=>( args[:item] || params[:item] || :core)
+      nest item, :view=>( args[:item] || :core)
     end.join "\n\n"
   end
 end

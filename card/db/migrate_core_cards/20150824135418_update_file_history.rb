01952b2e6fd2deb2d78d1399afb5c482d3a14169
xithan <philipp.kuehl@gmail.com>
2015-08-24 17:12:25 +0200

    add migration to update history of file and image card

---
+++ card/db/migrate_core_cards/20150824135418_update_file_history.rb
@@ -0,0 +1,17 @@
+# -*- encoding : utf-8 -*-
+
+class UpdateFileHistory < Card::CoreMigration
+  def up
+    Card.search(:type=>[:in, 'file', 'image']).each do |card|
+      card.actions.each do |action|
+        if (content_change = action.change_for(:db_content).first)
+          original_filename, file_type, action_id, mod  = content_change.value.split("\n")
+          if mod.present?
+            content_change.update_attributes! :value=>":#{card.codename}/#{mod}#{::File.extname(original_filename)}"
+          else
+            content_change.update_attributes! :value=>"~#{card.id}/#{action_id}#{::File.extname(original_filename)}"
+          end
+        end
+      end
+  end
+end

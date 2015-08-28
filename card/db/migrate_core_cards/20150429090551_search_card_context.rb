ece23635b61ebca642198155655874067ccd0adf
xithan <philipp.kuehl@gmail.com>
2015-08-28 17:16:31 +0200

    fix typo in migration

--- card/db/migrate_core_cards/20150429090551_search_card_context.rb
+++ card/db/migrate_core_cards/20150429090551_search_card_context.rb
@@ -23,6 +23,6 @@ class SearchCardContext < Card::CoreMigration
     end
     Card["*self+*right+*structure"].update_attributes! :content=>'{"name":"_left"}'
     Card["*type+*right+*structure"].update_attributes! :content=>'{"type":"_left"}'
-    Card["*type plus right+*right+*structure"].update_attributes! :content=>'{"left\":{"type":"_LL\"}, "right":"_LR"}'
+    Card["*type plus right+*right+*structure"].update_attributes! :content=>'{"left":{"type":"_LL"}, "right":"_LR"}'
   end
 end

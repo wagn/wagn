687db253585f12bf15f0977d2037bc8dffde7e1b
xithan <philipp.kuehl@gmail.com>
2015-08-28 19:04:06 +0200

    :checkered_flag:

--- card/db/migrate_core_cards/20150429090551_search_card_context.rb
+++ card/db/migrate_core_cards/20150429090551_search_card_context.rb
@@ -22,6 +22,7 @@ class SearchCardContext < Card::CoreMigration
       end
     end
     Card["*self+*right+*structure"].update_attributes! :content=>'{"name":"_left"}'
+    Card["*right+*right+*structure"].update_attributes! :content=>'{"right":"_left"}'
     Card["*type+*right+*structure"].update_attributes! :content=>'{"type":"_left"}'
     Card["*type plus right+*right+*structure"].update_attributes! :content=>'{"left":{"type":"_LL"}, "right":"_LR"}'
   end

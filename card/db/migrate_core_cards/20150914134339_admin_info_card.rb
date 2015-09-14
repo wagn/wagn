f60b685dc2a15b34c3895bf44a1b4a0900c09cd2
xithan <philipp.kuehl@gmail.com>
2015-09-14 21:21:56 +0200

    reorganize recaptcha setting cards

--- card/db/migrate_core_cards/20150914134339_admin_info_card.rb
+++ card/db/migrate_core_cards/20150914134339_admin_info_card.rb
@@ -5,9 +5,19 @@ class AdminInfoCard < Card::CoreMigration
     Card.create! :name=>"*admin info", :codename=>"admin_info", :subcards=>{'+*self+*read'=>'[[Administrator]]'}
 
     codenames = %w( default_html_view debugger recaptcha_settings )
-    content = codenames.map {|cn| "[[#{cn}]]"}.join "\n"
+    content = codenames.map {|cn| "[[#{Card[cn.to_sym].name}]]"}.join "\n"
     Card.create! :name=>"*admin settings", :codename=>"admin_settings",
       :type_id=>Card::PointerID, :content=>content,
       :subcards=>{'+*self+*read'=>'[[Administrator]]'}
+
+    Card[:recaptcha_settings].update_attributes! :type_id=>Card::PointerID,
+      :content=>"[[+public key]]\n[[+private key]]\n[[+proxy]]"
+
+    ['public_key', 'private_key', 'proxy'].each do |name|
+      card = Card["recaptcha_#{name}".to_sym]
+      card.update_attributes! :name=>"#{Card[:recaptcha_settings].name}+#{name.gsub('_',' ')}"
+    end
+    Card.fetch('*recaptcha settings+*self+*structure').delete
+
   end
 end

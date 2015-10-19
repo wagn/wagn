0c98dfe094db46b42c8f6080ec8493cfc5eb9308
xithan <philipp.kuehl@gmail.com>
2015-10-19 23:33:36 +0200

    refactor card_controller.rb

--- card/mod/01_core/set/all/content.rb
+++ card/mod/01_core/set/all/content.rb
@@ -1,10 +1,10 @@
 ::Card.error_codes[:conflict] = [:conflict, 409]
 
 def content
-  db_content or (new_card? && template.db_content)
+  db_content || (new_card? && template.db_content)
 end
 
-def content=(value)
+def content= value
   self.db_content = value
 end
 
@@ -13,7 +13,7 @@ def raw_content
 end
 
 format do
-  def chunk_list #override to customize by set
+  def chunk_list # override to customize by set
     :default
   end
 end
@@ -23,11 +23,11 @@ def label
 end
 
 def creator
-  Card[ creator_id ]
+  Card[creator_id]
 end
 
 def updater
-  Card[ updater_id ]
+  Card[updater_id]
 end
 
 def clean_html?
@@ -38,25 +38,27 @@ def history?
   false
 end
 
-def save_content_draft content
+def save_content_draft _content
   clear_drafts
 end
 
 def clear_drafts
-  drafts.created_by(Card::Auth.current_id).each do |draft|
-    draft.delete
-  end
+  drafts.created_by(Card::Auth.current_id).each(&:delete)
 end
 
-event :save_draft, before: :store, on: :update, when: proc{ |c| Env.params['draft'] == 'true' } do
+def last_draft_content
+  drafts.last.card_changes.last.value
+end
+
+event :save_draft,
+      before: :store, on: :update,
+      when: proc { Env.params['draft'] == 'true' } do
   save_content_draft content
   abort :success
 end
-
 
 event :set_default_content, on: :create, before: :approve do
   if !db_content_changed? && template && template.db_content.present?
     self.db_content = template.db_content
   end
 end
-

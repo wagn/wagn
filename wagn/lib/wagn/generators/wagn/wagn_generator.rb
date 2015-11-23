52497247b81370bb9f431d01925171f3aa3fa888
xithan <philipp.kuehl@gmail.com>
2015-11-23 17:45:08 +0100

    merge wagn/master

--- wagn/lib/wagn/generators/wagn/wagn_generator.rb
+++ wagn/lib/wagn/generators/wagn/wagn_generator.rb
@@ -223,14 +223,22 @@ class WagnGenerator < Rails::Generators::AppBase
   def database_gemfile_entry
     return [] if options[:skip_active_record]
     gem_name, gem_version = gem_for_database
-    # gem_version =  gem_name == 'mysql2' ? '0.3.20' : nil
-    GemfileEntry.version gem_name, gem_version,
-                        "Use #{options[:database]} as the database for Active Record"
+    if gem_name == 'mysql2'
+      # && Gem.loaded_specs['rails'].version < Gem::Version.new('4.2.5')
+      # Rails update from 4.2.4 to 4.2.5 didn't help.
+      # Current mysql2 gem (0.4.1) is still causing trouble.
+      # Maybe with the next Rails release?
+      # Could also be that ruby 1.9.3 is the problem.
+      gem_version = '0.3.20'
+    end
+    msg = "Use #{options[:database]} as the database for Active Record"
+    GemfileEntry.version gem_name, gem_version, msg
   end
 
   protected
+
   def self.banner
-     "wagn new #{self.arguments.map(&:usage).join(' ')} [options]"
+    "wagn new #{arguments.map(&:usage).join(' ')} [options]"
   end
 
   def mysql_socket

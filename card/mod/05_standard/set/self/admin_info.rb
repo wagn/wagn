824dd14a81d7b12265d0c11d278e9dd9fef0b2a1
xithan <philipp.kuehl@gmail.com>
2015-11-23 17:36:08 +0100

    refactor admin_info.rb

--- card/mod/05_standard/set/self/admin_info.rb
+++ card/mod/05_standard/set/self/admin_info.rb
@@ -1,26 +1,42 @@
-view :core do |args|
-  warning = ''
+view :core do
+  warnings = []
   if Card.config.action_mailer.perform_deliveries == false
-    warning += "<li class='list-group-item list-group-item-warning'>Email delivery is turned off. Change settings in config/application.rb to send sign up notifications.</li>"
+    warnings << email_warning
   end
-  if Card.config.recaptcha_public_key == Card::Auth::DEFAULT_RECAPTCHA_SETTINGS[:recaptcha_public_key]
-    warning +=  "<li class='list-group-item list-group-item-warning'>" + process_content(
-                   "You are using Wagn's default recaptcha key.
-                    That's fine for a local installation.
-                    If you want to use recaptcha for a publicly available Wagn installation
-                    you have to register your domain at http://google.com/recaptcha and
-                    add your keys to [[*recaptcha settings]]. To turn off captchas go to the
-                    captcha rule card [[*all+*captcha]]") + "</li>"
+  if Card.config.recaptcha_public_key ==
+     Card::Auth::DEFAULT_RECAPTCHA_SETTINGS[:recaptcha_public_key]
+    warnings << recaptcha_warning
   end
-  if warning.present?
-    content_tag :div, :class=>"alert alert-warning alert-dismissible", :role=>"alert" do
-      %{
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h6>WARNING</h6>
-        <ul class="list-group">#{warning}</ul>
-      }.html_safe
-    end
-  else
-    ''
+  return '' if warnings.empty?
+  alert :warning, dismissible: true do
+    render_warning_list warnings
   end
+end
+
+def render_warning_list warnings
+  '<h6>WARNING</h6>'.html_safe + list_tag(
+    warnings,
+    class: 'list-group',
+    items: {
+      class: 'list-group-item list-group-item-warning'
+    }
+  )
+end
+
+def email_warning
+  %{
+    Email delivery is turned off.
+    Change settings in config/application.rb to send sign up notifications.
+  }
+end
+
+def recaptcha_warning
+  process_content %{
+    You are using Wagn's default recaptcha key.
+    That's fine for a local installation.
+    If you want to use recaptcha for a publicly available Wagn installation
+    you have to register your domain at http://google.com/recaptcha and
+    add your keys to [[*recaptcha settings]]. To turn off captchas go to the
+    captcha rule card [[*all+*captcha]
+  }
 end

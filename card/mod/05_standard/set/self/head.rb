8ba03365f686fac6ce13e18c547247e94ea60189
xithan <philipp.kuehl@gmail.com>
2015-12-10 23:43:35 +0100

    :dog:

--- card/mod/05_standard/set/self/head.rb
+++ card/mod/05_standard/set/self/head.rb
@@ -113,19 +113,21 @@ format :html do
 
 
   def google_analytics_head_javascript
-    return unless (ga_key = Card.global_setting(:google_analytics_key)
-    %{
+    return unless (ga_key = Card.global_setting(:google_analytics_key))
+    <<-JAVASCRIPT
       <script type="text/javascript">
         var _gaq = _gaq || [];
         _gaq.push(['_setAccount', '#{ga_key}']);
         _gaq.push(['_trackPageview']);
         (function() {
-          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          var ga = document.createElement('script');
+          ga.type = 'text/javascript'; ga.async = true;
           ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            s.parentNode.insertBefore(ga, s);
+          var s = document.getElementsByTagName('script')[0];
+          s.parentNode.insertBefore(ga, s);
+          s.parentNode.insertBefore(ga, s);
         })();
       </script>
-    }
+    JAVASCRIPT
   end
 end

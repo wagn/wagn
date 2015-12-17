9183da4f15a3e0c1c0ee7ff459e245dd285fb33a
Ethan McCutchen <ethan@grasscommons.org>
2015-12-17 11:43:42 -0700

    attachment cleanup

--- card/mod/05_standard/set/abstract/attachment.rb
+++ card/mod/05_standard/set/abstract/attachment.rb
@@ -8,12 +8,16 @@ event :select_file_revision, after: :select_action do
   attachment.retrieve_from_store!(attachment.identifier)
 end
 
-event :upload_attachment, before: :validate_name, on: :save, when: proc { |c| c.preliminary_upload? } do
+event :upload_attachment, before: :validate_name, on: :save,
+                          when: proc { |c| c.preliminary_upload? } do
   save_original_filename  # save original filename as comment in action
-  write_identifier        # set db_content (needs original filename to determine extension)
+  write_identifier        # set db_content
+  # (needs original filename to determine extension)
   store_attachment!
   finalize_action         # create Card::Change entry for db_content
-  @current_action.update_attributes! draft: true, card_id: (new_card? ? upload_cache_card.id : id)
+
+  card_id = new_card? ? upload_cache_card.id : id
+  @current_action.update_attributes! draft: true, card_id: card_id
   success << {
     target: (new_card? ? upload_cache_card : self),
     type: type_name,
@@ -23,7 +27,10 @@ event :upload_attachment, before: :validate_name, on: :save, when: proc { |c| c.
   abort :success
 end
 
-event :assign_attachment_on_create, after: :prepare, on: :create, when: proc { |c| c.save_preliminary_upload? } do
+event :assign_attachment_on_create,
+      after: :prepare,
+      on: :create,
+      when: proc { |c| c.save_preliminary_upload? } do
   if (action = Card::Action.fetch(@action_id_of_cached_upload))
     upload_cache_card.selected_action_id = action.id
     upload_cache_card.select_file_revision
@@ -31,7 +38,9 @@ event :assign_attachment_on_create, after: :prepare, on: :create, when: proc { |
   end
 end
 
-event :assign_attachment_on_update, after: :prepare, on: :update,
+event :assign_attachment_on_update,
+      after: :prepare,
+      on: :update,
       when:  proc { |c| c.save_preliminary_upload? } do
   if (action = Card::Action.fetch(@action_id_of_cached_upload))
     uploaded_file =
@@ -68,7 +77,10 @@ event :save_original_filename, after: :validate_name,
   @current_action.update_attributes! comment: original_filename
 end
 
-event :delete_cached_upload_file_on_create, after: :extend, on: :create, when: proc { |c| c.save_preliminary_upload? } do
+event :delete_cached_upload_file_on_create,
+      after: :extend,
+      on: :create,
+      when: proc { |c| c.save_preliminary_upload? } do
   if (action = Card::Action.fetch(@action_id_of_cached_upload))
     upload_cache_card.delete_files_for_action action
     action.delete
@@ -76,7 +88,10 @@ event :delete_cached_upload_file_on_create, after: :extend, on: :create, when: p
   clear_upload_cache_dir_for_new_cards
 end
 
-event :delete_cached_upload_file_on_update, after: :extend, on: :update, when: proc { |c| c.save_preliminary_upload? } do
+event :delete_cached_upload_file_on_update,
+      after: :extend,
+      on: :update,
+      when: proc { |c| c.save_preliminary_upload? } do
   if (action = Card::Action.fetch(@action_id_of_cached_upload))
     delete_files_for_action action
     action.delete
@@ -93,9 +108,8 @@ event :write_identifier, after: :save_original_filename do
   self.content = attachment.db_content(mod: load_from_mod)
 end
 
-
-def item_names(args={})  # needed for flexmail attachments.  hacky.
-  [self.cardname]
+def item_names _args={} # needed for flexmail attachments.  hacky.
+  [cardname]
 end
 
 def original_filename
@@ -124,7 +138,7 @@ end
 
 # used for uploads for new cards until the new card is created
 def upload_cache_card
-  @upload_cache_card ||= Card["new_#{attachment_name}".to_sym ]
+  @upload_cache_card ||= Card["new_#{attachment_name}".to_sym]
 end
 
 # action id of the cached upload
@@ -139,9 +153,7 @@ end
 def load_from_mod= value
   @mod = value
   write_identifier
-  if value
-    @store_in_mod = true
-  end
+  @store_in_mod = true if value
 end
 
 def load_from_mod
@@ -167,22 +179,22 @@ end
 # place for files of regular file cards
 def upload_dir
   if id
-    "#{ Card.paths['files'].existent.first }/#{id}"
+    "#{Card.paths['files'].existent.first}/#{id}"
   else
     tmp_upload_dir
   end
 end
 
 # place for files if card doesn't have an id yet
-def tmp_upload_dir action_id=nil
-  "#{ Card.paths['files'].existent.first }/#{upload_cache_card.id}"
+def tmp_upload_dir _action_id=nil
+  "#{Card.paths['files'].existent.first}/#{upload_cache_card.id}"
 end
 
 # place for files of mod file cards
 def mod_dir
   mod = @mod || mod_file?
   Card.paths['mod'].to_a.each do |mod_path|
-    dir = File.join(mod_path, mod, 'file', codename )
+    dir = File.join(mod_path, mod, 'file', codename)
     if Dir.exist? dir
       return dir
     end
@@ -195,23 +207,20 @@ def mod_file?
   # when db_content was changed assume that it's no longer a mod file
   elsif !db_content_changed? && content.present?
     case content
-    when /^:[^\/]+\/([^.]+)/ ; $1     # current mod_file format
-    when /^\~/               ; false  # current id file format
+    when %r{^:[^/]+/([^.]+)} then $1     # current mod_file format
+    when /^\~/               then false  # current id file format
     else
-      if lines = content.split("\n") and lines.size == 4 # old format, still used in card_changes.
+      if (lines = content.split("\n")) && (lines.size == 4)
+        # old format, still used in card_changes.
         lines.last
       end
     end
   end
 end
 
-
 def assign_set_specific_attributes
-  # if the attachment is a empty string, just keep the content
-  attachment_name_str = attachment_name.to_s
-  if @set_specific && @set_specific.present? &&
-     (!@set_specific.has_key?(attachment_name_str) ||
-      !@set_specific[attachment_name_str].blank?)
+  # reset content if we really have something to upload
+  if @set_specific.present? && @set_specific[attachment_name.to_s].present?
     self.content = nil
   end
   super
@@ -219,8 +228,8 @@ end
 
 def clear_upload_cache_dir_for_new_cards
   Dir.entries(tmp_upload_dir).each do |filename|
-    if filename =~/^\d+/
-      path = File.join(tmp_upload_dir, filename )
+    if filename =~ /^\d+/
+      path = File.join(tmp_upload_dir, filename)
       if older_than_five_days? File.ctime(path)
         FileUtils.rm path
       end
@@ -241,34 +250,33 @@ def delete_files_for_action action
   end
 end
 
+# create filesystem links to files from prior action
+def symlink_to prior_action_id
+  return unless prior_action_id != last_action_id
+  save_action_id = selected_action_id
+  links = {}
 
-def symlink_to(prior_action_id) # create filesystem links to files from prior action
-  if prior_action_id != last_action_id
-    save_action_id = selected_action_id
-    links = {}
-
-    self.selected_action_id = prior_action_id
-    attachment.versions.each do |name, version|
-      links[name] = version.store_path
-    end
-    original = attachment.store_path
-
-    self.selected_action_id = last_action_id
-    attachment.versions.each do |name, version|
-      ::File.symlink links[name], version.store_path
-    end
-    ::File.symlink original, attachment.store_path
-
-    self.selected_action_id = save_action_id
+  self.selected_action_id = prior_action_id
+  attachment.versions.each do |name, version|
+    links[name] = version.store_path
   end
+  original = attachment.store_path
+
+  self.selected_action_id = last_action_id
+  attachment.versions.each do |name, version|
+    ::File.symlink links[name], version.store_path
+  end
+  ::File.symlink original, attachment.store_path
+
+  self.selected_action_id = save_action_id
 end
 
-def attachment_format(ext)
-  if ext.present? && attachment && original_ext=attachment.extension
+def attachment_format ext
+  if ext.present? && attachment && (original_ext = attachment.extension)
     if['file', original_ext].member? ext
       original_ext
-    elsif exts = MIME::Types[attachment.content_type]
-      if exts.find {|mt| mt.extensions.member? ext }
+    elsif (exts = MIME::Types[attachment.content_type])
+      if exts.find { |mt| mt.extensions.member? ext }
         ext
       else
         exts[0].extensions[0]
@@ -279,5 +287,3 @@ rescue => e
   Rails.logger.info "attachment_format issue: #{e.message}"
   nil
 end
-
-

dd3f8fd5010c4c8582c5a4cb89b2a657405a164c
xithan <philipp.kuehl@gmail.com>
2015-12-21 16:56:54 +0100

    spec_helper.rb: added comment

--- card/lib/card/spec_helper.rb
+++ card/lib/card/spec_helper.rb
@@ -1,26 +1,23 @@
 module SpecHelper
 end
 module Card::SpecHelper
-
   include Rails::Dom::Testing::Assertions::SelectorAssertions
-  #~~~~~~~~~  HELPER METHODS ~~~~~~~~~~~~~~~#
+  # ~~~~~~~~~  HELPER METHODS ~~~~~~~~~~~~~~~#
 
   def login_as user
-    Card::Auth.current_id = (uc=Card[user.to_s] and uc.id)
-    if @request
-      @request.session[:user] = Card::Auth.current_id
-    end
-    #warn "(ath)login_as #{user.inspect}, #{Card::Auth.current_id}, #{@request.session[:user]}"
+    Card::Auth.current_id = (uc = Card[user.to_s]) && uc.id
+    return unless @request
+    @request.session[:user] = Card::Auth.current_id
+    # warn "(ath)login_as #{user.inspect}, #{Card::Auth.current_id}, #{@request.session[:user]}"
   end
 
-  def newcard name, content=""
+  def newcard name, content=''
     #FIXME - misleading name; sounds like it doesn't save.
     Card.create! name: name, content: content
   end
 
-  def assert_view_select(view_html, *args, &block)
+  def assert_view_select view_html, *args, &block
     node = Nokogiri::HTML::Document.parse(view_html).root
-    #node = HTML::Document.new(view_html).root
     if block_given?
       assert_select node, *args, &block
     else
@@ -28,9 +25,9 @@ module Card::SpecHelper
     end
   end
 
-  def debug_assert_view_select(view_html, *args, &block)
+  def debug_assert_view_select view_html, *args, &block
     Rails.logger.rspec %(
-      #{CodeRay.scan(Nokogiri::XML(view_html,&:noblanks).to_s, :html).div}
+      #{CodeRay.scan(Nokogiri::XML(view_html, &:noblanks).to_s, :html).div}
       <style>
         .CodeRay {
           background-color: #FFF;
@@ -43,17 +40,17 @@ module Card::SpecHelper
     assert_view_select view_html, *args, &block
   end
 
-  def render_editor(type)
+  def render_editor type
     card = Card.create(name: "my favority #{type} + #{rand(4)}", type: type)
     card.format.render(:edit)
   end
 
   def render_content content, format_args={}
-    render_content_with_args( content, format_args )
+    render_content_with_args content, format_args
   end
 
   def render_content_with_args content, format_args={}, view_args={}
-    @card ||= Card.new name: "Tempo Rary 2"
+    @card ||= Card.new name: 'Tempo Rary 2'
     @card.content = content
     @card.format(format_args)._render :core, view_args
   end
@@ -67,7 +64,7 @@ module Card::SpecHelper
       if card_args[:name]
         Card.fetch card_args[:name], new: card_args
       else
-        Card.new card_args.merge( name: 'Tempo Rary' )
+        Card.new card_args.merge(name: 'Tempo Rary')
       end
     end
     card.format(format_args)._render(view, view_args)
@@ -77,13 +74,24 @@ module Card::SpecHelper
     SharedData::USERS.sort
   end
 
+  # Make expectations in the event phase.
+  # Takes the usual event options :on and :before/:after/:around
+  # and registers the event_block with these options as an event.
+  # Unknown methods in the event_block are executed in the rspec context
+  # instead of the card's context.
+  # An additionaly :trigger block in opts is expected that is called
+  # to start the event phase.
+  # Example:
+  # in_phase before: :approve, on: :save,
+  #          trigger: ->{ test_card.update_attributes! content: '' } do
+  #            expect(item_names).to eq []
+  #          end
   def in_phase opts, &event_block
     $rspec_binding = binding
     Card.class_eval do
-      alias_method :original_method_missing, :method_missing
-      def method_missing(m, *args)
+      def method_missing m, *args
         begin
-          method = eval("method(%s)" % m.inspect, $rspec_binding)
+          method = eval('method(%s)' % m.inspect, $rspec_binding)
         rescue NameError
         else
           return method.call(*args)
@@ -104,5 +112,4 @@ module Card::SpecHelper
     opts[:trigger].call
     Card.skip_callback name, kind, :in_phase_test
   end
-
 end

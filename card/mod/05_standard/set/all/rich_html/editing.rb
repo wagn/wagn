030ecd43b81b8b608c02f45609a8dab037c1f431
xithan <philipp.kuehl@gmail.com>
2016-01-05 20:30:15 +0100

    :dog:

--- card/mod/05_standard/set/all/rich_html/editing.rb
+++ card/mod/05_standard/set/all/rich_html/editing.rb
@@ -1,5 +1,5 @@
 format :html do
-  ###---( TOP_LEVEL (used by menu) NEW / EDIT VIEWS )
+  # ##---( TOP_LEVEL (used by menu) NEW / EDIT VIEWS )
 
   view :new, perms: :create, tags: :unknown_ok do |args|
     frame_and_form :create, args, 'main-success' => 'REDIRECT' do
@@ -30,7 +30,7 @@ format :html do
                        else
                          "New #{card.type_name}"
                        end
-      # FIXME: - overrides nest args
+      # FIXME: overrides nest args
       unless card.rule_card :autoname
         # prompt for name
         hidden[:name_prompt] = true unless hidden.has_key? :name_prompt
@@ -77,44 +77,58 @@ format :html do
     end
   end
 
-
   def default_edit_args args
     args[:optional_help] ||= :show
     args[:optional_toolbar] ||= :show
 
     args[:buttons] ||= %{
-      #{ submit_button class: 'submit-button' }
-      #{ button_tag 'Cancel', class: 'cancel-button slotter', 'data-slot-selector'=>args[:cancel_slot_selector],
-       href: (args[:cancel_path] || path), type: 'button' }
+      #{submit_button class: 'submit-button'}
+      #{button_tag 'Cancel',
+                   class: 'cancel-button slotter',
+                   href: (args[:cancel_path] || path), type: 'button',
+                  'data-slot-selector' => args[:cancel_slot_selector]}
     }
   end
 
-
   view :edit_name, perms: :update do |args|
-    frame_and_form( { action: :update, id: card.id }, args, 'main-success'=>'REDIRECT' ) do
+    frame_and_form({ action: :update, id: card.id }, args, 'main-success'=>'REDIRECT') do
       [
-        _render_name_formgroup( args ),
-        _optional_render( :confirm_rename, args ),
-        _optional_render( :button_formgroup, args )
+        _render_name_formgroup(args),
+        _optional_render(:confirm_rename, args),
+        _optional_render(:button_formgroup, args)
       ]
     end
   end
 
-
   view :confirm_rename do |args|
     referers = args[:referers]
     descendants = card.descendants
-    alert 'warning' do
-      %{
-        <h5>Are you sure you want to rename <em>#{card.name}</em>?</h5>
-        #{ %{ <h6>This change will...</h6> } if referers.any? || descendants.any? }
-        <ul>
-          #{ %{<li>automatically alter #{ descendants.size } related name(s). </li>} if descendants.any? }
-          #{ %{<li>affect at least #{referers.size} reference(s) to "#{card.name}".</li>} if referers.any? }
-        </ul>
-        #{ %{<p>You may choose to <em>update or ignore</em> the references.</p>} if referers.any? }
-      }
+    msg = "<h5>Are you sure you want to rename <em>#{card.name}</em>?</h5>"
+    if referers.any? || descendants.any?
+      msg << rename_info(referers, descendants)
     end
+    alert 'warning' do
+      msg
+    end
+  end
+
+  def rename_info referers, descendants
+    msg = '<h6>This change will...</h6>'
+    msg << '<ul>'
+    if descendants.any?
+      msg << "<li>automatically alter #{descendants.size} related name(s)."\
+             '</li>'
+    end
+    if referers.any?
+      msg << "<li>affect at least #{referers.size} reference(s) to " \
+             "\"#{card.name}\".</li>"
+    end
+    msg << '</ul>'
+    if referers.any?
+      msg << '<p>You may choose to <em>update or ignore</em> the references.' \
+             '</p>'
+    end
+    msg
   end
 
   def default_edit_name_args args
@@ -127,61 +141,70 @@ format :html do
       card:     { update_referencers: false }
     )
     args[:optional_toolbar] ||= :show
-    args[:buttons] = %{
-      #{ submit_button text: 'Rename and Update', disable_with: 'Renaming',
-                       class: 'renamer-updater' }
-      #{ button_tag 'Rename',
-                    data: { disable_with: 'Renaming' },
-                    class: 'renamer' }
-      #{ button_tag 'Cancel', class: 'slotter',  type: 'button', href: path }
-    }
-
+    args[:buttons] =
+      %{
+        #{submit_button text: 'Rename and Update', disable_with: 'Renaming',
+                        class: 'renamer-updater'}
+        #{button_tag 'Rename',
+                     data: { disable_with: 'Renaming' },
+                     class: 'renamer'}
+        #{button_tag 'Cancel', class: 'slotter',  type: 'button', href: path}
+      }
   end
-
 
   view :edit_type, perms: :update do |args|
     frame_and_form :update, args do
-    #'main-success'=>'REDIRECT: _self', # adding this back in would make main cards redirect on cardtype changes
+    # 'main-success'=>'REDIRECT: _self', # adding this back in would make main cards redirect on cardtype changes
       [
-        _render_type_formgroup( args ),
-        optional_render( :button_formgroup, args )
+        _render_type_formgroup(args),
+        optional_render(:button_formgroup, args)
       ]
     end
   end
 
   def default_edit_type_args args
-    args[:variety] = :edit #YUCK!
+    args[:variety] = :edit # YUCK!
     args[:optional_toolbar] ||= :show
-    args[:hidden] ||= { success: {view: :edit} }
+    args[:hidden] ||= { success: { view: :edit } }
     args[:buttons] = %{
-      #{ submit_button }
-      #{ button_tag 'Cancel', href: path(view: :edit), type: 'button',
-                              class: 'slotter' }
+      #{submit_button}
+      #{button_tag 'Cancel', href: path(view: :edit), type: 'button',
+                             class: 'slotter'}
     }
   end
 
   view :edit_rules, tags: :unknown_ok do |args|
     view = args[:rule_view] || :common_rules
-    slot_args = {
-        rule_view: view, optional_set_navbar: :show, optional_set_label: :hide, optional_rule_navbar: :hide
+    slot_args =
+      {
+        rule_view: view,
+        optional_set_navbar: :show,
+        optional_set_label: :hide,
+        optional_rule_navbar: :hide
       }
-    _render_related args.merge(related: {card: current_set_card, view: :open, slot: slot_args})
+    _render_related args.merge(
+      related: { card: current_set_card, view: :open, slot: slot_args }
+    )
   end
 
   def default_edit_rules_args args
     args[:optional_toolbar] ||= :show
   end
 
-  view :options, {view: :edit_rules, mod: All::RichHtml::Editing::HtmlFormat} # for backwards compatibility
+  # for backwards compatibility
+  view :options, view: :edit_rules, mod: All::RichHtml::Editing::HtmlFormat
 
   view :edit_structure do |args|
-    slot_args = {
+    slot_args =
+      {
         cancel_slot_selector: '.card-slot.related-view',
         cancel_path: card.format.path(view: :edit),
         optional_edit_toolbar: :hide,
-        hidden: {success: {view: :open, 'slot[subframe]'=>true}}
+        hidden: { success: { view: :open, 'slot[subframe]' => true } }
       }
-    render_related args.merge(related: {card: card.structure, view: :edit, slot: slot_args})
+    render_related args.merge(
+      related: { card: card.structure, view: :edit, slot: slot_args }
+    )
   end
 
   def default_edit_structure_args args
@@ -202,10 +225,13 @@ format :html do
   view :edit_nest_rules do |args|
     view = args[:rule_view] || :field_related_rules
     frame args do
-#      with_inclusion_mode :edit do
-       nested_fields(args).map do |chunk|
-         nest Card.fetch("#{chunk.referee_name}+*self"), view: :titled, rule_view: view, optional_set_label: :hide, optional_rule_navbar: :show
-       end
+      # with_inclusion_mode :edit do
+      nested_fields(args).map do |chunk|
+        nest Card.fetch("#{chunk.referee_name}+*self"),
+             view: :titled, rule_view: view,
+             optional_set_label: :hide,
+             optional_rule_navbar: :show
+      end
     end
   end
 
@@ -213,6 +239,3 @@ format :html do
     args[:optional_toolbar] ||= :show
   end
 end
-
-
-

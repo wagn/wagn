1b86b81f3bbab1f8a44642cbe26106420ff2a73c
xithan <philipp.kuehl@gmail.com>
2016-01-16 02:30:22 +0000

    try to get rspec html matchers working again

--- card/mod/02_basic_types/spec/set/all/rss_spec.rb
+++ card/mod/02_basic_types/spec/set/all/rss_spec.rb
@@ -1,8 +1,6 @@
 # -*- encoding : utf-8 -*-
 
 describe Card::Set::All::Rss do
-  include RSpecHtmlMatchers
-
   it 'should render recent.rss' do
     rendered = Card[:recent].format(:rss).show(nil, {})
     expect(rendered).to match(/xml/)

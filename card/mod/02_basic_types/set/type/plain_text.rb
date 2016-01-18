0fe4c3397c6c2aa3a273b4c81e1bdd6e57fbece4
Ethan McCutchen <ethan@grasscommons.org>
2016-01-18 03:31:57 +0000

    fixed broken args in simple views

--- card/mod/02_basic_types/set/type/plain_text.rb
+++ card/mod/02_basic_types/set/type/plain_text.rb
@@ -1,10 +1,13 @@
 format :html do
 
   view :editor do |args|
-    text_area :content, rows: 5, class: 'card-content', "data-card-type-code"=>card.type_code
+    text_area :content,
+              rows: 5,
+              class: 'card-content',
+              'data-card-type-code' => card.type_code
   end
 
   view :core do |args|
-    process_content_object( CGI.escapeHTML _render_raw )
+    process_content_object(CGI.escapeHTML _render_raw)
   end
 end

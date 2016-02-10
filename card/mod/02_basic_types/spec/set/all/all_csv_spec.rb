8e33be1d6c0935a504c811e4d4ac439f1b3e4d5c
xithan <philipp.kuehl@gmail.com>
2016-02-10 20:45:15 +0100

    more :dog:

--- card/mod/02_basic_types/spec/set/all/all_csv_spec.rb
+++ card/mod/02_basic_types/spec/set/all/all_csv_spec.rb
@@ -1,9 +1,11 @@
 # -*- encoding : utf-8 -*-
 
-describe Card::Set::All::AllCsv, "CSV mod" do
-  context "csv_row view" do
-    it "should handle nests" do
-      expect(render_card( :csv_row, { content: '{{A+B}} {{T}}' }, format: :csv )).to eq("AlphaBeta,Theta")
+describe Card::Set::All::AllCsv, 'CSV mod' do
+  context 'csv_row view' do
+    it 'should handle nests' do
+      rendered = render_card :csv_row, { content: '{{A+B}} {{T}}' },
+                             format: :csv
+      expect(rendered).to eq('AlphaBeta,Theta')
     end
   end
 end

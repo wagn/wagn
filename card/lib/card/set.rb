81657ab6e86c14dd037f48dff05f97fecb76bc09
xithan <philipp.kuehl@gmail.com>
2016-02-11 13:24:09 +0100

    organize code

--- card/lib/card/set.rb
+++ card/lib/card/set.rb
@@ -3,6 +3,7 @@
 class Card
 
   module Set
+    include Event
     mattr_accessor :modules, :traits
     @@modules = { base: [], base_format: {}, nonbase: {}, nonbase_format: {} }
 
@@ -146,47 +147,6 @@ class Card
       end
     end
 
-    def process_stage_opts opts
-      case
-      when opts[:after] || opts[:before]
-        # ignore :in options
-      when opts[:in]
-        opts[:after] = :"#{opts.delete(:in)}_stage" if opts[:in]
-      end
-    end
-
-    def event event, stage_or_opts={}, opts={}, &final
-      if stage_or_opts.is_a? Symbol
-        opts[:in] = stage_or_opts
-      else
-        opts = stage_or_opts
-      end
-      process_stage_opts opts
-
-      perform_later = (opts[:before] == :integrate_with_delay) ||
-                      (opts[:after] == :integrate_with_delay)
-      final_method = "#{event}_without_callbacks" # should be private?
-      opts[:on] = [:create, :update] if opts[:on] == :save
-
-      Card.define_callbacks event
-
-      class_eval do
-        define_method final_method, &final
-      end
-
-      # TODO: refactor and adapt to stages changes
-      if perform_later
-        defer_method = "#{event}_perform_later"
-        define_event_perform_later_method event, defer_method
-        define_active_job event, final_method, opts[:queue_as]
-        define_event_method event, defer_method, opts
-      else
-        define_event_method event, final_method, opts
-      end
-      set_event_callbacks event, opts
-    end
-
-    # TODO: check if this is used
     def stage_method method, opts={}, &block
       class_eval do
         define_method "_#{method}", &block
@@ -207,66 +167,6 @@ class Card
             send "_#{method}", *args
           end
         end
-      end
-    end
-
-    def define_event_perform_later_method event, method_name
-      class_eval do
-        define_method method_name, proc {
-          s_attr =
-            serializable_attributes.each_with_object({}) do |name, hash|
-              value = instance_variable_get("@#{name}")
-              hash[name] =
-                # ActiveJob doesn't accept symbols as arguments
-                if Symbol === value
-                  { value: value.to_s, symbol: true }
-                else
-                  { value: value }
-                end
-            end
-          Object.const_get(event.to_s.camelize).perform_later(self, s_attr)
-        }
-      end
-    end
-
-    def define_event_method event, call_method, _opts
-      class_eval do
-        define_method event do
-          # puts "#{event}".green
-          run_callbacks event do
-            send call_method
-          end
-        end
-      end
-    end
-
-    # creates an Active Job.
-    # The scheduled job gets the card object as argument and all serializable
-    # attributes of the card.
-    # (when the job is executed ActiveJob fetches the card from the database so
-    #  all attributes get lost)
-    # @param name [String] the name for the ActiveJob child class
-    # @param final_method [String] the name of the card instance method to be
-    #   queued
-    # @option queue [Symbol] (:default) the name of the queue8
-    def define_active_job name, final_method, queue=:default
-      class_name = name.to_s.camelize
-      eval %{
-        class ::#{class_name} < ActiveJob::Base
-          queue_as #{queue}
-        end
-      }
-      Object.const_get(class_name).class_eval do
-        define_method :perform, proc { |card, attributes|
-          attributes.each do |attname, args|
-            # symbols are not allowed so all symbols arrive here as strings
-            # convert strings that were symbols before back to symbols
-            value = args[:symbol] ? args[:value].to_sym : args[:value]
-            card.instance_variable_set("@#{attname}", value)
-          end
-          card.include_set_modules
-          card.send final_method
-        }
       end
     end
 
@@ -428,23 +328,6 @@ EOF
     end
 
     private
-
-    def set_event_callbacks event, opts
-      [:before, :after, :around].each do |kind|
-        if (object_method = opts.delete(kind))
-          this_set_module = self
-          Card.class_eval do
-            set_callback(
-              object_method, kind, event,
-              prepend: true, if: proc do |c|
-                c.singleton_class.include?(this_set_module) &&
-                  c.event_applies?(opts)
-              end
-            )
-          end
-        end
-      end
-    end
 
     def get_traits mod
       Card::Set.traits ||= {}

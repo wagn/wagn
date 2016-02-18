cf2834baa09157f1ffa019511272637a98e19272
xithan <philipp.kuehl@gmail.com>
2016-02-18 10:04:55 +0100

    refactor Card::Content

--- card/lib/card/content.rb
+++ card/lib/card/content.rb
@@ -2,6 +2,7 @@
 
 # TODO: move Card::Chunk to Card::Content::Chunk...
 require_dependency 'card/chunk'
+require_dependency 'card/content/chunk_parser'
 
 class Card
   class Content < SimpleDelegator
@@ -16,8 +17,8 @@ class Card
         end
       @opts = opts || {}
 
-      content = parse_content content unless content.is_a?(Array)
-      super content
+      @chunks = ChunkParser.parse(content, chunk_list)
+      super(@chunks.any? : @chunks : content)
     end
 
     def card
@@ -60,75 +61,6 @@ class Card
     def process_each_chunk &block
       each_chunk { |chunk| chunk.process_chunk(&block) }
       self
-    end
-
-    def parse_content content
-      @chunks = []
-
-      if content.is_a? String
-        position = last_position = 0
-        prefix_regexp = Chunk.get_prefix_regexp chunk_list
-        interval_string = ''
-
-        while (prefix_match = content[position..-1].match(prefix_regexp))
-          prefix = prefix_match[0]
-          # prefix of matched chunk
-          chunk_start = prefix_match.begin(0) + position
-          # content index of beginning of chunk
-          if prefix_match.begin(0) > 0
-            # if matched chunk is not beginning of test string
-            interval_string += content[position..chunk_start - 1]
-            # hold onto the non-chunk part of the string
-          end
-
-          chunk_class = Chunk.find_class_by_prefix prefix
-          # get the chunk class from the prefix
-          match, offset =
-            chunk_class.full_match content[chunk_start..-1], prefix
-          # see whether the full chunk actually matches
-          # (as opposed to bogus prefix)
-          context_ok = chunk_class.context_ok? content, chunk_start
-          # make sure there aren't contextual reasons for ignoring this chunk
-          position = chunk_start
-          # move scanning position up to beginning of chunk
-
-          if match
-            # we have a chunk match
-            position += (match.end(0) - offset.to_i)
-            # move scanning position up to end of chunk
-            if context_ok
-              @chunks << interval_string unless interval_string.empty?
-              # add the nonchunk string to the chunk list
-              @chunks << chunk_class.new(match, self)
-              # add the chunk to the chunk list
-              interval_string = ''
-              # reset interval string for next go-round
-              last_position = position
-              # note that the end of the chunk was the last place where a
-              # chunk was found (so far)
-            end
-          else
-            position += 1
-            # no match.  look at the next character
-          end
-
-          next unless !match || !context_ok
-          interval_string += content[chunk_start..position - 1]
-          # moving beyond the alleged chunk.
-          # append failed string to "nonchunk" string
-        end
-      end
-
-      if chunks.any?
-        if last_position < content.size
-          remainder = content[last_position..-1]
-          # handle any leftover nonchunk string at the end of content
-          @chunks << remainder
-        end
-        chunks
-      else
-        content
-      end
     end
 
     ALLOWED_TAGS = {}

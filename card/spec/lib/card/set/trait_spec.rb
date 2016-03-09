83a1e5f6cf4097d385036874d748b5da04d7f97f
xithan <philipp.kuehl@gmail.com>
2016-03-09 14:57:50 +0100

    :dog:

--- card/spec/lib/card/set/trait_spec.rb
+++ card/spec/lib/card/set/trait_spec.rb
@@ -1,17 +1,27 @@
 
 describe Card::Set::Trait do
-  class Card; module Set; module Type; module Phrase
-    extend Card::Set
-    card_accessor :write, type: 'pointer'
-    card_accessor :read, type_id: Card::PointerID
-  end; end; end; end
+  class Card
+    module Set
+      module Type
+        module Phrase
+          extend Card::Set
+          card_accessor :write, type: :pointer
+          card_accessor :read, type_id: Card::PointerID
+        end
+      end
 
-  class Card; module Set; module TypePlusRight; module Phrase; module Write
-    extend Card::Set
-    def type_plus_right_module_loaded
-      true
+      module TypePlusRight
+        module Phrase
+          module Write
+            extend Card::Set
+            def type_plus_right_module_loaded
+              true
+            end
+          end
+        end
+      end
     end
-  end; end; end; end; end
+  end
 
   subject do
     Card::Auth.as_bot do
@@ -25,7 +35,7 @@ describe Card::Set::Trait do
   context 'if accessor type is defined by a string' do
     it 'has left' do
       in_stage :prepare_to_validate, on: :create,
-               trigger: -> { subject } do
+                                     trigger: -> { subject } do
         # test API doesn't support sets for event
         # so we check the name
         return unless name == 'joke'
@@ -34,7 +44,7 @@ describe Card::Set::Trait do
     end
     it 'loads *type plus right set module' do
       in_stage :prepare_to_validate, on: :create,
-               trigger: -> { subject } do
+                                     trigger: -> { subject } do
         return unless name == 'joke'
         expect(write_card).to respond_to?(:type_plus_right_module_loaded)
       end
@@ -43,7 +53,7 @@ describe Card::Set::Trait do
   context 'if accessor type is defined by an id' do
     it 'has left' do
       in_stage :prepare_to_validate, on: :create,
-               trigger: -> { subject } do
+                                     trigger: -> { subject } do
         return unless name == 'joke'
         expect(read_card.left.class).to eq Card
       end

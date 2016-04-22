410191512c94281c062c1894f612b570f74dabf2
xithan <philipp.kuehl@gmail.com>
2016-04-22 22:39:53 +0100

    fix toolbar icons

--- card/db/migrate_core_cards/20130927191728_account_events.rb
+++ card/db/migrate_core_cards/20130927191728_account_events.rb
@@ -2,18 +2,18 @@
 
 class AccountEvents < Card::CoreMigration
   def up
-    aa = Card.fetch "#{Card[:signup].name}+#{Card[:type].name}+#{Card[:accountable].name}", new: {}
+    aa = Card.fetch :signup, :type, :accountable, new: {}
     aa.content = '1'
     aa.save!
 
     role_right = "#{Card[:roles].name}+#{Card[:right].name}"
 
-    r_options = Card.fetch "#{role_right}+#{Card[:options].name}", new: {}
+    r_options = Card.fetch role_right, :options, new: {}
     r_options.type_id = Card::SearchTypeID
     r_options.content = %({"type":"role", "not":{"codename":["in","anyone","anyone_signed_in"]}})
     r_options.save!
 
-    r_input = Card.fetch "#{role_right}+#{Card[:input].name}", new: {}
+    r_input = Card.fetch role_right, :input, new: {}
     r_input.content = '[[checkbox]]'
     r_input.save!
   end

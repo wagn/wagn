be388dfb4c570a8778464d335d3677c65dd14a6a
Robert Wentworth <rhwentworth@users.noreply.github.com>
2016-05-13 21:04:51 -0700

    Infrastructure to support use of I18n; plus time localization and a few text extractions

--- card/lib/card/format.rb
+++ card/lib/card/format.rb
@@ -85,7 +85,10 @@ class Card
     # ~~~~~ INSTANCE METHODS
 
     def initialize card, opts={}
-      raise Card::Error, 'format initialized without card' unless (@card = card)
+      raise Card::Error, #'format initialized without card'
+        I18n.t(:exception_init_without_card, 
+               scope: 'lib.card.format')       unless (@card = card)
+               
       opts.each do |key, value|
         instance_variable_set "@#{key}", value
       end
@@ -221,11 +224,12 @@ class Card
     def format_date date, include_time=true
       # using DateTime because Time doesn't support %e on some platforms
       if include_time
-        DateTime.new(
+        I18n.localize(DateTime.new(
           date.year, date.mon, date.day, date.hour, date.min, date.sec
-        ).strftime('%B %e, %Y %H:%M:%S')
+        ), format: :card_date_seconds) #.strftime('%B %e, %Y %H:%M:%S')
       else
-        DateTime.new(date.year, date.mon, date.day).strftime('%B %e, %Y')
+        I18n.localize(DateTime.new(date.year, date.mon, date.day),
+                      format: :card_date_only)  #.strftime('%B %e, %Y')
       end
     end
 
--- card/mod/05_standard/set/right/when_last_edited.rb
+++ card/mod/05_standard/set/right/when_last_edited.rb
@@ -1,5 +1,7 @@
 view :raw do |_args|
-  card.left.new_card? ? '' : card.left.updated_at.strftime('%A, %B %d, %Y %I:%M %p %Z')
+  card.left.new_card? ? '' : I18n.localize(card.left.updated_at, 
+                                           format: :card_dayofwk_min_tz)
+  #.strftime('%A, %B %d, %Y %I:%M %p %Z')
 end
 
 view :core, :raw

7cd4062809c28f34465ec959a7ab705f777c9a2d
xithan <philipp.kuehl@gmail.com>
2016-05-19 01:00:27 +0200

    repair rspec command

--- card/spec/lib/card/chunk_spec.rb
+++ card/spec/lib/card/chunk_spec.rb
@@ -4,8 +4,9 @@ require 'card/content/chunk'
 describe Card::Content::Chunk, 'Chunk' do
   context 'Class' do
     it 'should populate prefix map on load' do
-      expect(Card::Content::Chunk.prefix_map.keys.size).to be > 0
-      expect(Card::Content::Chunk.prefix_map['{'][:class])
+      expect(Card::Content::Chunk.prefix_map_by_list[:default].keys.size)
+        .to be > 0
+      expect(Card::Content::Chunk.prefix_map_list[:default]['{'][:class])
         .to eq(Card::Content::Chunk::Include)
     end
 

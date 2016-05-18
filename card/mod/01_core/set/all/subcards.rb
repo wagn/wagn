5a677c92d870acffa889515e11e23487ef45dbf8
xithan <philipp.kuehl@gmail.com>
2016-05-18 23:16:35 +0200

    rename add_subcard to attach_subcard

--- card/mod/01_core/set/all/subcards.rb
+++ card/mod/01_core/set/all/subcards.rb
@@ -10,25 +10,29 @@ def subfield field_name
   subcards.field field_name
 end
 
-# phase_method :add_subcard, before: :store do |name_or_card, args=nil|
+# phase_method :attach_subcard, before: :store do |name_or_card, args=nil|
 # TODO: handle differently in different stages
-def add_subcard name_or_card, args=nil
+def attach_subcard name_or_card, args=nil
   subcards.add name_or_card, args
 end
 
-# phase_method :add_subfield, before: :approve do |name_or_card, args=nil|
-def add_subfield name_or_card, args=nil
+# phase_method :attach_subfield, before: :approve do |name_or_card, args=nil|
+def attach_subfield name_or_card, args=nil
   subcards.add_field name_or_card, args
 end
 
 def detach_subcard name_or_card
   subcards.remove name_or_card
 end
-alias_method :remove_subcard, :detach_subcard
+
 
 def detach_subfield name_or_card
   subcards.remove_field name_or_card
 end
+
+alias_method :add_subcard, :attach_subcard
+alias_method :remove_subcard, :detach_subcard
+alias_method :add_field, :attach_subfield
 alias_method :remove_subfield, :detach_subfield
 
 def clear_subcards

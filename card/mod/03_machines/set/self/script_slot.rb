2c2fc7dc3ee425b3af99a3e4b2650642cff067a8
xithan <philipp.kuehl@gmail.com>
2016-05-21 01:00:57 +0200

    add script_prosemirror.js to 'script: slot' card

--- card/mod/03_machines/set/self/script_slot.rb
+++ card/mod/03_machines/set/self/script_slot.rb
@@ -3,5 +3,5 @@ include_set Abstract::CodeFile
 def source_files
   %w( wagn_mod wagn_editor wagn_layout wagn_navbox wagn_upload wagn ).map do |n|
     "#{n}.js.coffee"
-  end
+  end << 'script_prosemirror.js'
 end

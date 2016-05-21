b3eb822c68a1565de1ee32c588c35714abf212c9
xithan <philipp.kuehl@gmail.com>
2016-05-21 12:24:46 +0200

    get specs passings

--- card/mod/05_standard/set/all/rich_html/form.rb
+++ card/mod/05_standard/set/all/rich_html/form.rb
@@ -238,7 +238,7 @@ format :html do
 
   view :editor do |_args|
     wrap_with(:div, id: unique_id, class: 'prosemirror-editor') do
-      hidden_field(:content, class: 'card-content', value: render_raw)
+      hidden_field(:content, class: 'card-content', value: card.raw_content)
     end
   end
 

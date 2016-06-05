5e35a2b4272d8f965435d6c5d99f2d63989ce712
xithan <philipp.kuehl@gmail.com>
2016-06-06 00:01:13 +0200

    use truncation strategy for @javascript features

--- wagn/features/support/env.rb
+++ wagn/features/support/env.rb
@@ -58,9 +58,9 @@ end
 #  certain features and scenarios.
 # See the DatabaseCleaner documentation for details. Example:
 #
-# Before('@no-txn,@selenium,@culerity,@celerity,@javascript') do
-#   DatabaseCleaner.strategy = :truncation #, {except: %w[widgets]}
-# end
+Before('@no-txn,@selenium,@culerity,@celerity,@javascript') do
+  DatabaseCleaner.strategy = :truncation #, {except: %w[widgets]}
+end
 #
 # Before('~@no-txn', '~@selenium', '~@culerity', '~@celerity',
 #        '~@javascript') do
@@ -71,4 +71,4 @@ end
 # Possible values are :truncation and :transaction
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
-Cucumber::Rails::Database.javascript_strategy = :transaction
+Cucumber::Rails::Database.javascript_strategy = :truncation

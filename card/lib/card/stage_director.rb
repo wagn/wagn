713734cb572a9e9a0dcb35e92a61b7a313bba3fc
xithan <philipp.kuehl@gmail.com>
2016-07-29 14:42:50 +0200

    refactor Set::Event

--- card/lib/card/stage_director.rb
+++ card/lib/card/stage_director.rb
@@ -8,6 +8,11 @@ class Card
     end
   end
 
+  def clean_after_stage_fail
+    @action = nil
+    expire_pieces
+    subcards.each(&:expire_pieces)
+  end
   # A 'StageDirector' executes the stages of a card when the card gets created,
   # updated or deleted.
   # For subcards, i.e. other cards that are changed in the same act, a
@@ -204,7 +209,8 @@ class Card
         run_subdirector_stages stage
       end
     rescue => e
-      @card.rescue_event e
+      @card.clean_after_stage_fail
+      raise e
     end
 
     def run_stage_callbacks stage

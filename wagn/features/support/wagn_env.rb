c036c1765e2ae5b23ee368463abf2ac007f4a555
xithan <philipp.kuehl@gmail.com>
2016-07-29 15:26:48 +0200

    clean up cucumber config

--- wagn/features/support/wagn_env.rb
+++ wagn/features/support/wagn_env.rb
@@ -6,25 +6,6 @@ Capybara.configure do |config|
   config.match = :prefer_exact
 end
 
-
-Before('@no-txn,@selenium,@culerity,@celerity,@javascript') do
-  # if ENV['RAILS_ROOT']
-  #   require File.join(ENV['RAILS_ROOT'], '/config/environment')
-  # else
-  #   require File.expand_path('../../config/environment', __FILE__)
-  # end
-#  Cucumber::Rails::World.use_transactional_fixtures = false
-  DatabaseCleaner.strategy = :transaction #, {except: %w[widgets]}
-  Card::Cache.restore
-end
-#
-Before('~@no-txn', '~@selenium', '~@culerity', '~@celerity',
-       '~@javascript') do
-  # if ENV['RAILS_ROOT']
-  #   require File.join(ENV['RAILS_ROOT'], '/config/environment')
-  # else
-  #   require File.expand_path('../../config/environment', __FILE__)
-  # end
-  DatabaseCleaner.strategy = :transaction
+Before do
   Card::Cache.restore
 end

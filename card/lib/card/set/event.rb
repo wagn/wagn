d9a964a8af4931c1f23b7064e3654c3b735d2635
xithan <philipp.kuehl@gmail.com>
2016-08-01 10:24:54 +0200

    move log_event_call

--- card/lib/card/set/event.rb
+++ card/lib/card/set/event.rb
@@ -31,6 +31,12 @@ class Card
     end
   end
 
+  def log_event_call event
+    Rails.logger.debug "#{name}: #{event}"
+    # puts "#{Card::DirectorRegister.to_s}".green
+  end
+
+
   module Set
     # Implements the event API for card sets
     module Event
@@ -99,11 +105,6 @@ class Card
             end
           end
         end
-      end
-
-      def log_event_call event
-        Rails.logger.debug "#{name}: #{event}"
-        # puts "#{Card::DirectorRegister.to_s}".green
       end
 
       # creates an Active Job.

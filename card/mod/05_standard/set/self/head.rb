37eccd387340e6263f9605b4cae2b1f1d51b5ce1
xithan <philipp.kuehl@gmail.com>
2016-08-03 13:14:49 +0200

    refactor Set::Self::Head (cherry picked from commit 0cac336)

--- card/mod/05_standard/set/self/head.rb
+++ card/mod/05_standard/set/self/head.rb
@@ -1,13 +1,12 @@
 format :html do
   view :raw do
-    %(
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      #{head_title}
-      #{head_buttons}
-      #{head_stylesheets}
-      #{head_javascript}
-    )
+    output [
+      head_meta,
+      head_title,
+      head_buttons,
+      head_stylesheets,
+      head_javascript
+    ]
   end
 
   view :core do |args|
@@ -16,6 +15,13 @@ format :html do
     when @mainline then '(*head)'
     else _render_raw(args)
     end
+  end
+
+  def head_meta
+    <<-HTML
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    HTML
   end
 
   def head_title
@@ -33,6 +39,31 @@ format :html do
       bits << rss_link if root.card.type_id == SearchTypeID
     end
     bits.compact.join "\n      "
+  end
+
+  def head_stylesheets
+    manual_style = params[:style]
+    style_card = Card[manual_style] if manual_style
+    style_card ||= root.card.rule_card :style
+    @css_path =
+      if params[:debug] == 'style'
+        page_path(style_card.cardname, item: :import, format: :css)
+      elsif style_card
+        card_path style_card.machine_output_url
+      end
+    return unless @css_path
+    %(<link href="#{@css_path}" media="all" rel="stylesheet" type="text/css" />)
+  end
+
+  def head_javascript
+    output [
+      wagn_variables,
+      script_rule,
+      ie9,
+      prose_mirror_config,
+      trigger_slot_ready,
+      google_analytics
+    ]
   end
 
   def favicon
@@ -55,76 +86,5 @@ format :html do
     root.search_params[:vars].each { |key, val| opts["_#{key}"] = val }
     href = page_path root.card.cardname, opts
     %(<link rel="alternate" type="application/rss+xml" title="RSS" href="#{href}" />)
-  end
-
-  def head_stylesheets
-    manual_style = params[:style]
-    style_card = Card[manual_style] if manual_style
-    style_card ||= root.card.rule_card :style
-    @css_path =
-      if params[:debug] == 'style'
-        page_path(style_card.cardname, item: :import, format: :css)
-      elsif style_card
-        card_path style_card.machine_output_url
-      end
-    return unless @css_path
-    %(<link href="#{@css_path}" media="all" rel="stylesheet" type="text/css" />)
-  end
-
-  def head_javascript
-    varvals = [
-      "window.wagn={rootPath:'#{Card.config.relative_url_root}'}",
-      # tinyMCE doesn't load on non-root wagns w/o preinit line
-      "window.tinyMCEPreInit={base:\"#{card_path 'assets/tinymce'}\",query:'3.5.9',suffix:''}"
-    ]
-    card.have_recaptcha_keys? &&
-      varvals << "wagn.recaptchaKey='#{Card.config.recaptcha_public_key}'"
-    (c = Card[:double_click]) && !Card.toggle(c.content) &&
-      varvals << 'wagn.noDoubleClick=true'
-    @css_path &&
-      varvals << "wagn.cssPath='#{@css_path}'"
-
-    manual_script = params[:script]
-    script_card   = Card[manual_script] if manual_script
-    script_card ||= root.card.rule_card :script
-
-    @js_tag =
-      if params[:debug] == 'script'
-        script_card.format(:js).render_core item: :include_tag
-      elsif script_card
-        javascript_include_tag script_card.machine_output_url
-      end
-    ie9_card = Card[:script_html5shiv_printshiv]
-    <<-HTML
-      #{javascript_tag { varvals * ';' }}
-      #{@js_tag if @js_tag}
-      <!--[if lt IE 9]>#{javascript_include_tag ie9_card.machine_output_url if ie9_card}<![endif]-->
-      #{javascript_tag { "wagn.setTinyMCEConfig('#{escape_javascript Card.global_setting(:tiny_mce).to_s}')" }}
-      #{google_analytics_head_javascript}
-      <script type="text/javascript">
-        $('document').ready(function() {
-          $('.card-slot').trigger('slotReady');
-        })
-      </script>
-    HTML
-  end
-
-  def google_analytics_head_javascript
-    return unless (ga_key = Card.global_setting(:google_analytics_key))
-    <<-JAVASCRIPT
-      <script type="text/javascript">
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', '#{ga_key}']);
-        _gaq.push(['_trackPageview']);
-        (function() {
-          var ga = document.createElement('script');
-          ga.type = 'text/javascript'; ga.async = true;
-          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-          var s = document.getElementsByTagName('script')[0];
-          s.parentNode.insertBefore(ga, s);
-          s.parentNode.insertBefore(ga, s);
-        })();
-      </script>
-    JAVASCRIPT
   end
 end

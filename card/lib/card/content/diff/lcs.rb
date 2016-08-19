aa20ac103bf39b426eb6840d09828d7414c7c35c
Ethan McCutchen <ethan@grasscommons.org>
2016-08-18 20:29:35 -0400

    minor lcs refactor

--- card/lib/card/content/diff/lcs.rb
+++ card/lib/card/content/diff/lcs.rb
@@ -1,3 +1,5 @@
+require_dependency "card/content/diff/lcs_mechanic"
+
 class Card
   class Content
     class Diff
@@ -28,7 +30,7 @@ class Card
           if old_text
             old_words, old_ex = separate_comparables_from_excludees old_text
             new_words, new_ex = separate_comparables_from_excludees new_text
-            processor = ChunkProcessor.new old_words, new_words, old_ex, new_ex
+            processor = Processor.new old_words, new_words, old_ex, new_ex
             processor.run @result
           else
             list = split_and_preprocess(new_text)
@@ -88,7 +90,9 @@ class Card
         end
 
         # Compares two lists of chunks and generates a diff
-        class ChunkProcessor
+        class Processor
+          include ProcessorMechanic
+
           attr_reader :result, :summary, :dels_cnt, :adds_cnt
           def initialize old_words, new_words, old_excludees, new_excludees
             @adds = []
@@ -109,115 +113,15 @@ class Card
               else
                 write_excludees
               end
-              process_word word
+              process_element word.old_element, word.new_element, word.action
               prev_action = word.action
             end
             write_all
             @result
           end
-
-          private
-
-          def interpret_action prev_action, word
-            if (prev_action == word.action) ||
-               (prev_action == "-" && word.action == "!") ||
-               (prev_action == "!" && word.action == "+")
-              handle_action word.action
-            else
-              write_all
-            end
-          end
-
-          def handle_action action
-            case action
-            when "-" then del_old_excludees
-            when "+" then add_new_excludees
-            when "!" then
-              del_old_excludees
-              add_new_excludees
-            else
-              write_excludees
-            end
-          end
-
-          def write_all
-            write_dels
-            write_adds
-            write_excludees
-          end
-
-          def write_unchanged text
-            @result.write_unchanged_chunk text
-          end
-
-          def write_dels
-            return if @dels.empty?
-            @result.write_deleted_chunk @dels.join
-            @dels = []
-          end
-
-          def write_adds
-            return if @adds.empty?
-            @result.write_added_chunk @adds.join
-            @adds = []
-          end
-
-          def write_excludees
-            while (ex = @excludees[:new].next)
-              @result.write_excluded_chunk ex[:element]
-            end
-          end
-
-          def del_old_excludees
-            while (ex = @excludees[:old].next)
-              if ex[:type] == :disjunction
-                @dels << ex[:element]
-              else
-                write_dels
-                @result.write_excluded_chunk ex[:element]
-              end
-            end
-          end
-
-          def add_new_excludees
-            while (ex = @excludees[:new].next)
-              if ex[:type] == :disjunction
-                @adds << ex[:element]
-              else
-                write_adds
-                @result.complete << ex[:element]
-              end
-            end
-          end
-
-          def process_word word
-            process_element word.old_element, word.new_element, word.action
-          end
-
-          def process_element old_element, new_element, action
-            case action
-            when "-" then minus old_element
-            when "+" then plus new_element
-            when "!"
-              minus old_element
-              plus new_element
-            else
-              write_unchanged new_element
-              @excludees[:new].word_step
-            end
-          end
-
-          def plus new_element
-            @adds << new_element
-            @excludees[:new].word_step
-          end
-
-          def minus old_element
-            @dels << old_element
-            @excludees[:old].word_step
-          end
         end
 
+        # support class for LCS::Processor
         class ExcludeeIterator
           def initialize list
             @list = list

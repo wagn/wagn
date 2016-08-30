3e1501e3949b6e35013e71036009dc6f48a15c1a
xithan <philipp.kuehl@gmail.com>
2016-08-30 19:13:09 +0200

    refactor admin tasks move "empty trash" task to All::Trash

--- card/mod/core/set/all/utils.rb
+++ card/mod/core/set/all/utils.rb
@@ -1,43 +1,10 @@
 module ClassMethods
-  def empty_trash
-    Card.delete_trashed_files
-    Card.where(trash: true).delete_all
-    Card::Action.delete_cardless
-    Card::Reference.unmap_if_referee_missing
-    Card::Reference.delete_if_referer_missing
-  end
-
-  # deletes any file not associated with a real card.
-  def delete_trashed_files
-    trashed_card_ids = all_trashed_card_ids
-    file_ids = all_file_ids
-    dir = Cardio.paths["files"].existent.first
-    file_ids.each do |file_id|
-      next unless trashed_card_ids.member?(file_id)
-      if Card.exists?(file_id) # double check!
-        raise Card::Error, "Narrowly averted deleting current file"
-      end
-      FileUtils.rm_rf "#{dir}/#{file_id}", secure: true
-    end
-  end
-
   def delete_tmp_files id=nil
     dir = Cardio.paths["files"].existent.first + "/tmp"
     dir += "/#{id}" if id
     FileUtils.rm_rf dir, secure: true
   rescue
     Rails.logger.info "failed to remove tmp files"
-  end
-
-  def all_file_ids
-    dir = Card.paths["files"].existent.first
-    Dir.entries(dir)[2..-1].map(&:to_i)
-  end
-
-  def all_trashed_card_ids
-    trashed_card_sql = %( select id from cards where trash is true )
-    sql_results = Card.connection.select_all(trashed_card_sql)
-    sql_results.map(&:values).flatten.map(&:to_i)
   end
 
   def merge_list attribs, opts={}

f8b3c546fbaeaab7e4f3cbdf8bb0d5c1f2b30501
xithan <philipp.kuehl@gmail.com>
2016-09-05 13:20:48 +0200

    minor fixes

--- card/mod/machines/spec/set/abstract/machine_spec.rb
+++ card/mod/machines/spec/set/abstract/machine_spec.rb
@@ -2,7 +2,7 @@ describe Card::Set::Abstract::Machine do
   describe "#make_machine_output_coded" do
     it "creates coded file" do
       Card[:all, :script].make_machine_output_coded
-      mod_path = Card::Mod::Loader.mod_dirs.path "core"
+      mod_path = Card::Mod::Loader.mod_dirs.path "machines"
       path = File.join mod_path, "file", "all_script_machine_output", "file.js"
       expect(File.exist?(path)).to be_truthy
     end

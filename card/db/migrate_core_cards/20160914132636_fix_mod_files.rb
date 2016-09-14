45d7a17d665d4fc8f941f45e80df63b2d434358b
xithan <philipp.kuehl@gmail.com>
2016-09-14 17:33:55 +0200

    update changes

--- card/db/migrate_core_cards/20160914132636_fix_mod_files.rb
+++ card/db/migrate_core_cards/20160914132636_fix_mod_files.rb
@@ -6,9 +6,20 @@ class FixModFiles < Card::Migration::Core
       next unless card.coded?
       next unless card.content.include?("05_standard") ||
                   card.content.include?("06_bootstrap")
-      new_content = card.content.sub("05_standard","standard")
-                                .sub("06_bootstrap","bootstrap")
+      new_content = card.content.sub("05_standard", "standard")
+                        .sub("06_bootstrap", "bootstrap")
       card.update_column :db_content, new_content
+
+      update_history card
+    end
+  end
+
+  def update_history card
+    card.actions.each do |action|
+      next unless (content_change = action.change(:db_content))
+      new_value = content_change.value.gsub("05_standard", "standard")
+                                .gsub("06_bootstrap", "bootstrap")
+      content_change.update_column :value, new_value
     end
   end
 end

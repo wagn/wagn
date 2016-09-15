5f34892467bd325c706933c190d8d71864a07e1c
xithan <philipp.kuehl@gmail.com>
2016-09-15 16:17:59 +0200

    :dog2:

--- card/spec/support/card_spec_loader.rb
+++ card/spec/support/card_spec_loader.rb
@@ -1,86 +1,84 @@
-  class CardSpecLoader
-    class << self
-      def init
-        require "spork"
-        ENV["RAILS_ENV"] = "test"
-        require "timecop"
-      end
+class CardSpecLoader
+  class << self
+    def init
+      require "spork"
+      ENV["RAILS_ENV"] = "test"
+      require "timecop"
+    end
 
-      def load_shared_examples
-        Card::Mod::Loader.mod_dirs.each "spec/shared_examples" do |shared_ex_dir|
-          Dir["#{shared_ex_dir}/**/*.rb"].sort.each { |f| require f }
+    def prefork
+      Spork.prefork do
+        unless ENV["RAILS_ROOT"]
+          raise Card::Error, "No RAILS_ROOT given. Can't load environment."
         end
-      end
-
-      def prefork
-        Spork.prefork do
-          if ENV["RAILS_ROOT"]
-            require File.join(ENV["RAILS_ROOT"], "/config/environment")
-          else
-            require File.expand_path("../../config/environment", __FILE__)
-          end
-          @@joe_user_id = Card["joe_user"].id
-          load_shared_examples
-          require File.expand_path("../simplecov_helper.rb", __FILE__)
-          yield if block_given?
-        end
-      end
-
-      def each_run
-        Spork.each_run do
-          # This code will be run each time you run your specs.
-        end
-      end
-
-      def rspec_config
-        require "rspec/rails"
+        require File.join ENV["RAILS_ROOT"], "config/environment"
+        load_shared_examples
+        require File.expand_path("../simplecov_helper.rb", __FILE__)
 
         # Requires supporting ruby files with custom matchers and macros, etc,
         # in spec/support/ and its subdirectories.
         #  Dir[File.join(Cardio.gem_root, "spec/support/**/*.rb")].each do |f|
         #    require f
         #  end
-
-        #@joe_user_id = Card["joe_user"].id
-        RSpec.configure do |config|
-          config.include RSpec::Rails::Matchers::RoutingMatchers,
-                         file_path: %r{\bspec/controllers/}
-          config.include RSpecHtmlMatchers
-          # format_index = ARGV.find_index {|arg| arg =~ /--format|-f/ }
-          # formatter = format_index ? ARGV[ format_index + 1 ] : 'documentation'
-          # config.default_formatter=formatter
-
-          config.infer_spec_type_from_file_location!
-          # config.include CustomMatchers
-          # config.include ControllerMacros, type: :controllers
-
-          # == Mock Framework
-          # If you prefer to mock with mocha, flexmock or RR,
-          # uncomment the appropriate symbol:
-          # :mocha, :flexmock, :rr
-
-          config.use_transactional_fixtures = true
-          config.use_instantiated_fixtures  = false
-
-          config.before(:each) do
-            Delayed::Worker.delay_jobs = false
-            Card::Auth.current_id = @@joe_user_id
-            Card::Cache.restore
-            Card::Env.reset
-          end
-
-          config.after(:each) do
-            Timecop.return
-          end
-          yield config if block_given?
-        end
+        yield if block_given?
       end
+    end
 
-      def helper
-        require File.expand_path "../card_spec_helper.rb", __FILE__
-        RSpec::Core::ExampleGroup.send :include, Card::SpecHelper
-        Card.send :include, Card::SpecHelper::CardHelper
-        Card.send :extend, Card::SpecHelper::CardHelper::ClassMethods
+    def each_run
+      Spork.each_run do
+        # This code will be run each time you run your specs.
+      end
+    end
+
+    def rspec_config
+      require "rspec/rails"
+
+      @@joe_user_id = Card["joe_user"].id
+      RSpec.configure do |config|
+        config.include RSpec::Rails::Matchers::RoutingMatchers,
+                       file_path: %r{\bspec/controllers/}
+        config.include RSpecHtmlMatchers
+        # format_index = ARGV.find_index {|arg| arg =~ /--format|-f/ }
+        # formatter = format_index ? ARGV[ format_index + 1 ] : 'documentation'
+        # config.default_formatter=formatter
+
+        config.infer_spec_type_from_file_location!
+        # config.include CustomMatchers
+        # config.include ControllerMacros, type: :controllers
+
+        # == Mock Framework
+        # If you prefer to mock with mocha, flexmock or RR,
+        # uncomment the appropriate symbol:
+        # :mocha, :flexmock, :rr
+
+        config.use_transactional_fixtures = true
+        config.use_instantiated_fixtures  = false
+
+        config.before(:each) do
+          Delayed::Worker.delay_jobs = false
+          Card::Auth.current_id = @@joe_user_id
+          Card::Cache.restore
+          Card::Env.reset
+        end
+
+        config.after(:each) do
+          Timecop.return
+        end
+        yield config if block_given?
+      end
+    end
+
+    def helper
+      require File.expand_path "../card_spec_helper.rb", __FILE__
+      RSpec::Core::ExampleGroup.send :include, Card::SpecHelper
+      Card.send :include, Card::SpecHelper::CardHelper
+      Card.send :extend, Card::SpecHelper::CardHelper::ClassMethods
+    end
+
+    def load_shared_examples
+      Card::Mod::Loader.mod_dirs.each "spec/shared_examples" do |shared_ex_dir|
+        Dir["#{shared_ex_dir}/**/*.rb"].sort.each { |f| require f }
       end
     end
   end
+end

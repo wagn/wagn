c73762f30bc822335e50b3e9f22cedb09cd18b73
xithan <philipp.kuehl@gmail.com>
2016-09-20 00:06:01 +0200

    improve save helper

--- card/lib/card/model/save_helper.rb
+++ card/lib/card/model/save_helper.rb
@@ -1,48 +1,101 @@
 class Card
   module Model
+    # Helper methods to create and update cards.
+    # The general pattern is:
+    # All methods use the ActiveRecord !-methods that throw exceptions if
+    # somethings fails.
+    # All !-methods in this module rename existing cards
+    # to resolve name conflicts)
     module SaveHelper
-      def create_card args
+      def create_card name_or_args, content_or_args=nil
+        args = standardize_args name_or_args, content_or_args
         resolve_name_conflict args
         Card.create! args
       end
 
-      # if card with same name exists move it out of the way
-      def create_card! args
-        create_card args.reverse_merge(rename_if_conflict: :old)
-      end
-
-      def update_card name, args
+      def update_card name, content_or_args
+        args = standardize_update_args content_or_args
         resolve_name_conflict args
         Card[name].update_attributes! args
       end
 
-      def update_card! args
-        update_card args.reverse_merge(rename_if_conflict: :new)
-      end
-
-      def create_or_update name_or_args, args={}
-        if name_or_args.is_a?(Hash)
-          name = name_or_args.delete :name
-          args = name_or_args
+      def create_or_update_card name_or_args, content_or_args=nil
+        args = standardize_args name_or_args, content_or_args
+        if Card[args[:name]]
+          update_card(args.delete(:name), args)
         else
-          name = name_or_args
-        end
-        if Card[name]
-          update_card name, args
-        else
-          create_card args.merge(name: name)
+          create_card(args)
         end
       end
 
-      def create_or_update! name_or_args, args=nil
-        if name_or_args.is_a?(Hash)
-          name = name_or_args.delete(:name)
-          args = name_or_args
+      # create if card doesn't exist
+      # updates existing card only if given attributes are different except the
+      # name
+      # For example if a card with name "under_score" exists
+      # then `ensure_card "Under Score"` doesn't change anything
+      def ensure_card name_or_args, content_or_args=nil
+        args = standardize_args name_or_args, content_or_args
+        name = args.delete(:name)
+        if (card = Card[name])
+          ensure_attributes card, args
         else
-          name = name_or_args
-          args ||= {}
+          Card.create! args.merge(name: name)
         end
-        create_or_update name, args.reverse_merge(rename_if_conflict: :new)
+      end
+
+      # create if card doesn't exist
+      # updates existing card only if given attributes are different including
+      # the name
+      # For example if a card with name "under_score" exists
+      # then `ensure_card "Under Score"` renames it to "Under Score"
+      def ensure_card! name_or_args, content_or_args=nil
+        args = standardize_args name_or_args, content_or_args
+        if (card = Card[args[:name]])
+          ensure_attributes card, args
+        else
+          Card.create! args
+        end
+      end
+
+      # if card with same name exists move it out of the way
+      def create_card! name_or_args, content_or_args=nil
+        args = standardize_args name_or_args, content_or_args
+        create_card args.reverse_merge(rename_if_conflict: :old)
+      end
+
+      def update_card! name, content_or_args
+        args = standardize_update_args content_or_args
+        update_card name, args.reverse_merge(rename_if_conflict: :new)
+      end
+
+      def create_or_update_card! name_or_args, content_or_args=nil
+        args = standardize_args name_or_args, content_or_args
+        create_or_update args.reverse_merge(rename_if_conflict: :new)
+      end
+
+      # @return args
+      def standardize_args name_or_args, content_or_args
+        if name_or_args.is_a?(Hash)
+          name_or_args
+        else
+          add_name name_or_args, content_or_args
+        end
+      end
+
+      def standardize_update_args content_or_args
+        if content_or_args.is_a?(String)
+          { content: content_or_args }
+        else
+          content_or_args
+        end
+      end
+
+      def add_name name, content_or_args
+        if content_or_args.is_a?(String)
+          { content: content_or_args, name: name }
+        else
+          content_or_args.merge name: name
+        end
       end
 
       def resolve_name_conflict args
@@ -51,11 +104,20 @@ class Card
         args[:name] = Card.uniquify_name args[:name], rename
       end
 
-      # create if it doesn't exist
-      def ensure_card args
-        return if Card[args[:name]]
-        Card.create! args
+      def ensure_attributes card, args
+        update_args = args.select { |key, value| card.send(key) != value }
+        return if update_args.empty?
+        card.update_attributes! update_args
       end
+
+      alias_method :create, :create_card
+      alias_method :update, :update_card
+      alias_method :create_or_update, :create_or_update_card
+      alias_method :create!, :create_card!
+      alias_method :update!, :update_card!
+      alias_method :create_or_update!, :create_or_update_card!
+      alias_method :ensure, :ensure_card
+      alias_method :ensure!, :ensure_card!
     end
   end
 end

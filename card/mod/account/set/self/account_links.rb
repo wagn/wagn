89776b29a1286a573109ca5af2d3898e3ae674d0
xithan <philipp.kuehl@gmail.com>
2016-09-23 11:14:22 +0200

    make account links changeable

--- card/mod/account/set/self/account_links.rb
+++ card/mod/account/set/self/account_links.rb
@@ -12,35 +12,59 @@ format :html do
 
   view :sign_up, perms: ->(r) { r.show_signup_link? },
                  denial: :blank do |args|
-    link_text = args[:title] ||
-                I18n.t(:sign_up, scope: "mod.standard.set.self.account_links")
-    path_opts = { action: :new, type: :signup }
-    link_to_card :signup, link_text, path: path_opts, id: "signup-link"
+    link_to_card :signup, args[:link_text], args[:link_opts]
   end
 
   view :sign_in, perms: ->(_r) { !Auth.signed_in? },
-                 denial: :blank do |_args|
-    link_text = I18n.t :sign_in, scope: "mod.standard.set.self.account_links"
-    link_to_card :signin, link_text, id: "signin-link"
-  end
-
-  view :invite, perms: ->(r) { r.show_invite_link? },
-                denial: :blank do |_args|
-    link_text = I18n.t :invite, scope: "mod.standard.set.self.account_links"
-    path_opts = { action: :new, type: :signup }
-    link_to link_text, path: path_opts, id: "invite-a-friend-link"
+                 denial: :blank do |args|
+    link_to_card :signin, args[:link_text], args[:link_opts]
   end
 
   view :sign_out, perms: ->(_r) { Auth.signed_in? },
-                  denial: :blank do |_args|
-    link_text = I18n.t :sign_out, scope: "mod.standard.set.self.account_links"
-    path_opts = { action: :delete }
-    link_to_card :signin, link_text, path: path_opts, id: "signout-link"
+                  denial: :blank do |args|
+    link_to_card :signin, args[:link_text], args[:link_opts]
+  end
+
+  view :invite, perms: ->(r) { r.show_invite_link? },
+       denial: :blank do |args|
+    link_to args[:link_text], args[:link_opts]
   end
 
   view :my_card, perms: ->(_r) { Auth.signed_in? },
                  denial: :blank do |_args|
     link_to_card Auth.current.cardname, nil, id: "my-card-link"
+  end
+
+  def default_sign_up_args args
+    account_link_text :sign_up, args
+    account_link_opts "signup-link", args, action: :new, type: :signup
+  end
+
+  def default_sign_in_args args
+    account_link_text :invite, args
+    account_link_opts "signin-link", args
+  end
+
+  def default_invite_args args
+    account_link_text :invite, args
+    account_link_opts "invite-a-friend-link", args, action: :new, type: :signup
+  end
+
+  def default_sign_out_args args
+    account_link_text :sign_out, args
+    account_link_opts "signout-link", args, action: :delete
+  end
+
+  def account_link_text purpose, args
+    args[:link_text] =
+      args.delete(:title) ||
+      I18n.t(purpose, scope: "mod.standard.set.self.account_links")
+  end
+
+  def account_link_opts id, args, path=nil
+    args[:link_opts] ||= {}
+    args[:link_opts][:id] ||= id
+    args[:link_opts][:path] ||= path if path
   end
 
   view :raw do |args|

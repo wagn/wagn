af27aa7d76df485a0c751277e8e0f6fea3978865
xithan <philipp.kuehl@gmail.com>
2016-11-03 20:41:10 +0100

    remove unnecessary as_bot

--- card/db/migrate_core_cards/20161103154836_update_keys.rb
+++ card/db/migrate_core_cards/20161103154836_update_keys.rb
@@ -19,11 +19,9 @@ class UpdateKeys < Card::Migration::Core
 
   def resolve_conflict id, key, new_key
     if walking_dead? key
-      Card::Auth.as_bot do
-        Card.where(id: id).delete_all
-        # card wasn't reachable anyway
-        # (due to rails inflection update or smartname update)
-      end
+      Card.where(id: id).delete_all
+      # card wasn't reachable anyway
+      # (due to rails inflection update or smartname update)
     else
       puts "key conflict: can't change #{key} to #{new_key}"
     end

8d9ab96b2311c2e95ba44e236eec4386486f45ab
Ethan McCutchen <ethan@grasscommons.org>
2016-11-08 13:29:42 -0500

    polish prosemirror_editor

--- card/mod/prosemirror_editor/set/abstract/prosemirror_editor.rb
+++ card/mod/prosemirror_editor/set/abstract/prosemirror_editor.rb
@@ -1,7 +1,7 @@
 format :html do
   view :editor do
-    wrap_with(:div, id: unique_id, class: "prosemirror-editor") do
-      hidden_field(:content, class: "card-content", value: card.raw_content)
+    wrap_with :div, id: unique_id, class: "prosemirror-editor" do
+      hidden_field :content, class: "card-content", value: card.raw_content
     end
   end
 end

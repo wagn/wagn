6606a2b24f26a977fb29cb1fdaf4a367d72077fa
xithan <philipp.kuehl@gmail.com>
2016-11-14 12:33:09 +0100

    make view cache configurable defaults to off in development enviroment

--- card/lib/cardio.rb
+++ card/lib/cardio.rb
@@ -49,7 +49,7 @@ module Cardio
         closed_search_limit:    10,
 
         non_createable_types:   [%w(signup setting set)],
-        view_cache:             false,
+        view_cache:             (Rails.env == "production"),
 
         encoding:               "utf-8",
         request_logger:         false,

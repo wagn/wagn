8409e4dc4306ca1cc3242d8b77976440428450ac
Ethan McCutchen <ethan@grasscommons.org>
2016-12-12 15:03:15 -0500

    core extension inclusion workaround

--- card/config/initializers/core_extensions.rb
+++ card/config/initializers/core_extensions.rb
@@ -1,10 +1,17 @@
 # -*- encoding : utf-8 -*-
 
 # extend core Ruby object classes
+
+class Class
+  def self.include_extension extension
+    include extension
+  end
+end
+
 module CoreExtensions
-  ::Object.include Object
-  ::Module.include Module
-  ::Hash.include Hash::Merging
+  ::Object.include_extension Object
+  ::Module.include_extension Module
+  ::Array.include_extension Array
+  ::Hash.include_extension Hash::Merging
   ::Hash.extend Hash::ClassMethods::Nesting
-  ::Array.include Array
 end

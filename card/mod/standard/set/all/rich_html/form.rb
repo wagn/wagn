e2f7a650872c9ccaebc11508e52260d01c3868ba
xithan <philipp.kuehl@gmail.com>
2016-12-23 15:58:14 +0100

    don't break without voo

--- card/mod/standard/set/all/rich_html/form.rb
+++ card/mod/standard/set/all/rich_html/form.rb
@@ -116,7 +116,7 @@ format :html do
   end
 
   def formgroup_label editor_type, title
-    return unless voo.show? :title
+    return if voo && voo.hide?(:title)
     label_type = editor_type || :content
     form.label label_type, title
   end

b57202f59073e24bf1798da1cd87523893b3dd76
xithan <philipp.kuehl@gmail.com>
2017-01-31 12:37:20 +0100

    cucumber args

--- wagn/lib/wagn/commands/cucumber_command/parser.rb
+++ wagn/lib/wagn/commands/cucumber_command/parser.rb
@@ -3,34 +3,27 @@ require "optparse"
 
 module Wagn
   module Commands
-    class RspecCommand
+    class CucumberCommand
       class Parser < OptionParser
         def initialize opts
           super() do |parser|
             parser.banner = "Usage: wagn cucumber [WAGN ARGS] -- [CUCUMBER ARGS]\n\n"
-            # parser.separator <<-EOT
-            #
-            # WAGN ARGS
-            #
-            # You don't have to give a full path for FILENAME, the basename is enough
-            #   If FILENAME does not include '_spec' rspec searches for the
-            #   corresponding spec file.
-            #   The line number always referes to example in the (corresponding) spec
-            #   file.
-            #
-            # EOT
+            parser.separator <<-EOT.strip_heredoc
 
+               WAGN ARGS
+            EOT
+            opts[:env] = ["RAILS_ROOT=."]
             parser.on("-d", "--debug", "Drop into debugger on failure") do |a|
-              opts[:debug] = a ? "DEBUG=1" : ""
+              opts[:env] << "DEBUG=1" if a
             end
             parser.on("-f", "--fast", "Stop on first failure") do |a|
-              opts[:fast] = a ? "FAST=1" : ""
+              opts[:env] << "FAST=1" if a
             end
             parser.on("-l", "--launchy", "Open page on failure") do |a|
-              opts[:launchy] = a ? "LAUNCHY=1" : ""
+              opts[:env] << "LAUNCHY=1" if a
             end
             parser.on("-s", "--step", "Pause after each step") do |a|
-              opts[:step] = a ? "STEP=1" : ""
+              opts[:env] << "STEP=1" if a
             end
           end
         end

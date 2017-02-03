268b8a4f4efff7b17c1e12599a24552a99f9d372
xithan <philipp.kuehl@gmail.com>
2017-02-03 12:19:00 +0100

    render_card -> render_view

--- card/mod/standard/spec/set/type/toggle_spec.rb
+++ card/mod/standard/spec/set/type/toggle_spec.rb
@@ -6,13 +6,13 @@ describe Card::Set::Type::Toggle do
   end
 
   it "has yes/no as processed content" do
-    expect(render_card(:core, type: "Toggle", content: "0")).to eq("no")
-    expect(render_card(:closed_content, type: "Toggle", content: "1"))
+    expect(render_view(:core, type: "Toggle", content: "0")).to eq("no")
+    expect(render_view(:closed_content, type: "Toggle", content: "1"))
       .to eq("yes")
   end
 
   describe "view :labeled_editor" do
-    subject { render_card :labeled_editor, type: :toggle, name: "A+toggle" }
+    subject { render_view :labeled_editor, type: :toggle, name: "A+toggle" }
     it "has checkbox label" do
       is_expected.to have_tag("label", with: { for: "content_toggle" }) do
         with_text "Toggle"

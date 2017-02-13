1f7f8b5f5cf18141fc8b3d346185796903ebad8c
xithan <philipp.kuehl@gmail.com>
2017-02-13 13:18:23 +0100

    html5 doctype for email layout to fix features

--- card/lib/card/mailer.rb
+++ card/lib/card/mailer.rb
@@ -21,7 +21,7 @@ class Card
 
       def layout message
         <<-HTML
-          <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+          <!DOCTYPE html>
           <html>
             <head>
               <meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>

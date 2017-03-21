4fdf516fd39c03aa779f4122098be54d67149e2f
xithan <philipp.kuehl@gmail.com>
2017-03-21 11:13:44 +0100

    fix flash bug

--- card/mod/notifications/set/all/show_notifications.rb
+++ card/mod/notifications/set/all/show_notifications.rb
@@ -1,7 +1,7 @@
 format :html do
   view :flash do
-    flash_notice = Env.success.flash || params[:flash]
-    return "" unless flash_notice && focal?
+    flash_notice = params[:flash] || Env.success.flash
+    return "" unless flash_notice.present? && focal?
     Array(flash_notice).join "\n"
   end
 end

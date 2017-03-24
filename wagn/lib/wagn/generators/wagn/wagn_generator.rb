4aa0f222e391c61e4120793377aaf96737928621
xithan <philipp.kuehl@gmail.com>
2017-03-24 14:59:09 +0100

    two columns for gerry

--- wagn/lib/wagn/generators/wagn/wagn_generator.rb
+++ wagn/lib/wagn/generators/wagn/wagn_generator.rb
@@ -203,15 +203,15 @@ class WagnGenerator < Rails::Generators::AppBase
 
   def mysql_socket
     @mysql_socket ||= [
-      "/tmp/mysql.sock", # default
-      "/var/run/mysqld/mysqld.sock", # debian/gentoo
-      "/var/tmp/mysql.sock", # freebsd
-      "/var/lib/mysql/mysql.sock", # fedora
-      "/opt/local/lib/mysql/mysql.sock", # fedora
-      "/opt/local/var/run/mysqld/mysqld.sock", # mac + darwinports + mysql
-      "/opt/local/var/run/mysql4/mysqld.sock", # mac + darwinports + mysql4
-      "/opt/local/var/run/mysql5/mysqld.sock", # mac + darwinports + mysql5
-      "/opt/lampp/var/mysql/mysql.sock" # xampp for linux
+      "/tmp/mysql.sock",                        # default
+      "/var/run/mysqld/mysqld.sock",            # debian/gentoo
+      "/var/tmp/mysql.sock",                    # freebsd
+      "/var/lib/mysql/mysql.sock",              # fedora
+      "/opt/local/lib/mysql/mysql.sock",        # fedora
+      "/opt/local/var/run/mysqld/mysqld.sock",  # mac + darwinports + mysql
+      "/opt/local/var/run/mysql4/mysqld.sock",  # mac + darwinports + mysql4
+      "/opt/local/var/run/mysql5/mysqld.sock",  # mac + darwinports + mysql5
+      "/opt/lampp/var/mysql/mysql.sock"         # xampp for linux
     ].find { |f| File.exist?(f) } unless RbConfig::CONFIG["host_os"] =~ /mswin|mingw/
   end
 

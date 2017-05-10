fbaf9d10da78fcee7478c541881642f46ca94264
Srivigneshwar <prasadsrivi@gmail.com>
2017-05-10 14:38:21 +0200

    Help block bug fix

--- card/mod/standard/set/all/rich_html/content.rb
+++ card/mod/standard/set/all/rich_html/content.rb
@@ -186,7 +186,7 @@ format :html do
   view :help, tags: :unknown_ok do
     help_text = voo.help || rule_based_help
     return "" unless help_text.present?
-    wrap_with :div, help_text, class: classy("help-text #{args[:help_class]}")
+    wrap_with :div, help_text, class: classy("help-text")
   end
 
   def rule_based_help

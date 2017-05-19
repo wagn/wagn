fb38a7ba6f28be7ef872e1ebc68ac0d7520a7943
xithan <philipp.kuehl@gmail.com>
2017-05-19 15:11:43 +0200

    add json "essentials" view

--- card/spec/support/matchers.rb
+++ card/spec/support/matchers.rb
@@ -1,12 +1,29 @@
 RSpec::Matchers.define :be_invalid do
   match do |card|
-    valid = card.valid?
-    values_match?(false, valid) &&
-      values_match?(@error_msg, card.errors[@error_key])
+    @valid = card.valid?
+    # card.errors returns an array, hence we need an extra include matcher
+    values_match?(false, @valid) &&
+      values_match?(include(@error_msg), card.errors[@error_key])
+  end
+
+  description do
+    "be invalid #{"because of #{@error_key} #{@error_msg}" if @error_key}"
   end
 
   chain(:because_of) do |reason|
     @error_key, @error_msg = reason.to_a.first
+  end
+
+  failure_message do |actual|
+    super() + fail_reason(actual)
+  end
+
+  def fail_reason actual
+    if @valid
+      "but file is valid"
+    else
+      "but it is invalid because of #{actual.errors.messages}"
+    end
   end
 end
 
